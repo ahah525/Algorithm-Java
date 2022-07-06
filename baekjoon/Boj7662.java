969fe993cc9b1c3dc8700693d8169f3980eaa84e
ahah525 <ahah525@naver.com>
2022-07-06 21:25:36 +0900

    Data Structure2-7662 수정

--- baekjoon/Boj7662.java
+++ baekjoon/Boj7662.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 public class Boj7662 {
-    static HashMap<Integer, Integer> map;
+    static HashMap<Integer, Integer> map;   // 이중 우선순위 큐
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,6 +22,7 @@ public class Boj7662 {
                 int n = Integer.parseInt(st.nextToken());   // 정수
                 switch(mode) {
                     case "D":
+                        // 이중 우선순위 큐가 비었으면 삭제 연산 패스
                         if(map.isEmpty()) break;
                         if(n == 1) {
                             // 최댓값 삭제
@@ -38,12 +39,14 @@ public class Boj7662 {
                         map.put(n, map.getOrDefault(n, 0) + 1); // 원래 값에 + 1
                         break;
                 }
+                //System.out.println(map.keySet());
             }
+            // 이중 우선순위 큐의 최댓값, 최솟값 출력
             if(map.isEmpty()) {
                 System.out.println("EMPTY");
             } else {
                 int max = delete(maxHeap);  // 최댓값
-                int min = (map.size() == 0) ? max : delete(minHeap);    // 최솟값
+                int min = (map.isEmpty()) ? max : delete(minHeap);    // 최솟값
                 System.out.println(max + " " + min);
             }
         }
@@ -54,13 +57,14 @@ public class Boj7662 {
             // 일단 삭제
             cur = q.poll(); // 꺼낸 값
             int cnt = map.getOrDefault(cur, 0); // 꺼낸 값의 개수
-            // 1. 원래 삭제되었어야할 값이 남아있으면 삭제
+            // 1. 꺼낸 값이 이중 우선순위 큐에 없으면(과거에 삭제되었어야할 값이었으면), 한번 더 꺼내기
             if (cnt == 0) continue;
+            // 2. 꺼낸 값이 이중 우선순위 큐에 있으면, 이중 우선순위 큐의 값 변경하기
             if (cnt == 1) {
-                // 1개 있으면 key, value 삭제
+                // 2-1. 1개 있으면 key, value 삭제
                 map.remove(cur);
             } else {
-                // 2개 이상이면 value - 1 하기
+                // 2-2. 2개 이상이면 value - 1 로 수정
                 map.put(cur, cnt - 1);
             }
             break;
