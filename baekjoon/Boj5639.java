6af803ba341c274e6fcad2f937dbfb7a63676e70
ahah525 <ahah525@naver.com>
2022-07-11 17:39:47 +0900

    Tree - 5639

--- baekjoon/Boj5639.java
+++ baekjoon/Boj5639.java
@@ -15,7 +15,8 @@ public class Boj5639 {
         // 전위 순회 결과 저장
         while(true) {
             // 입력값 널이면 종료
-            if((s = br.readLine()) == null)
+            s = br.readLine();
+            if(s == null)
                 break;
             nodeList.add(Integer.parseInt(s));
         }
@@ -24,11 +25,11 @@ public class Boj5639 {
         bw.flush();
         bw.close();
     }
-    // 후위 순회
+    // 후위 순회(탐색 시작 인덱스, 탐색 끝 인덱스)
     public static void postOrder(int start, int end) throws IOException {
         // 엇갈리면 종료
         if(start > end) return;
-        int pivot = start + 1;  // 기준값
+        int pivot = end + 1;            // 오른쪽 서브트리의 루트 노드
         int cur = nodeList.get(start);  // 현재 노드
         for(int i = start; i <= end; i++) {
             // 현재 노드보다 큰 값을 가진 노드가 나타나면
@@ -39,6 +40,6 @@ public class Boj5639 {
         }
         postOrder(start + 1, pivot - 1);    // 왼쪽 서브트리
         postOrder(pivot, end);              // 오른쪽 서브트리
-        bw.write(cur + "\n");
+        bw.write(cur + "\n");           // 현재 노드값 출력
     }
 }
