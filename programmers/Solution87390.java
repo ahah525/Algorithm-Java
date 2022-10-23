b1ac82a3536417c8ab48e32089057451d3ab5dab
ahah525 <ahah525@naver.com>
2022-10-23 17:13:56 +0900

    87390-n^2 배열 자르기(v2.정답)

--- programmers/Solution87390.java
+++ programmers/Solution87390.java
@@ -1,48 +1,25 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution87390 {
     public static void main(String[] args) {
         int n = 1000000;
         long left = 100000;
         long right = 199999;
-        int len = (int) (right - left + 1);
 
-        int[] answer = solution(n, left, right);
+        List<Integer> answer = solution(n, left, right);
         // [3,2,2,3]
-        for(int i = 0; i < len; i++) {
-            System.out.print(answer[i] + " ");
-        }
+        System.out.println(answer);
     }
 
-    public static int[] solution(int n, long left, long right) {
-        int len = (int) (right - left + 1);
-        int[] answer = new int[len];
-        int cnt = 0;
-        int lx = (int) left / n;
-        int ly = (int) left % n;
-        int rx = (int) right / n;
-        int ry = (int) right % n;
+    public static List<Integer> solution(int n, long left, long right) {
+        List<Integer> answer = new ArrayList<>();
 
-        if(lx == rx) {
-            for(int j = ly; j <= ry; j++) {
-                answer[cnt++] = Math.max(lx + 1, j + 1);
-            }
-        } else {
-            // 맨 처음
-            for(int j = ly; j < n; j++) {
-                answer[cnt++] = Math.max(lx + 1, j + 1);
-            }
-            // 중간
-            for(int i = lx + 1; i <= rx - 1; i++) {
-                for(int j = 0; j < n; j++) {
-                    answer[cnt++] = Math.max(i + 1, j + 1);
-                }
-            }
-            // 맨 마지막
-            for(int j = 0; j <= ry; j++) {
-                answer[cnt++] = Math.max(rx + 1, j + 1);
-            }
-
+        for(long i = left; i <= right; i++) {
+            // 행, 열 값 중 최댓값 + 1
+            answer.add((int) (Math.max(i / n, i % n) + 1));
         }
 
         return answer;
