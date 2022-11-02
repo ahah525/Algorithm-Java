bd6a257e54756b9028572f80dcc7cdc141e681d8
ahah525 <ahah525@naver.com>
2022-11-02 18:01:26 +0900

    42577-전화번호 목록(v2.시간초과 해결)

--- programmers/Solution42577.java
+++ programmers/Solution42577.java
@@ -1,5 +1,7 @@
 package programmers;
 
+import java.util.Arrays;
+
 /**
  * phoneBook: 전화번호 리스트
  * answer: 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false, 그렇지 않으면 true
@@ -19,15 +21,16 @@ class Solution42577 {
     }
 
     public static boolean solution(String[] phoneBook) {
+        // 오름차순 정렬
+        Arrays.sort(phoneBook);
+
         for(int i = 0; i < phoneBook.length - 1; i++) {
-            for(int j = i + 1; j < phoneBook.length; j++) {
-                // A 번호가 B 번호로 시작하는지 검증
-                if(phoneBook[i].length() == phoneBook[j].length()) {
-                    continue;
-                }
-                if(phoneBook[i].startsWith(phoneBook[j]) || phoneBook[j].startsWith(phoneBook[i])) {
-                    return false;
-                }
+            // A 번호가 B 번호로 시작하는지 검증
+            if(phoneBook[i].length() == phoneBook[i + 1].length()) {
+                continue;
+            }
+            if(phoneBook[i + 1].startsWith(phoneBook[i])) {
+                return false;
             }
         }
         return true;
