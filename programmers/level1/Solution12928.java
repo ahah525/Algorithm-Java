132b7dab20fbcb563423f68dde9306746021c539
ahah525 <ahah525@naver.com>
2022-11-14 23:36:06 +0900

    12928-약수의 합(v2.성공)

--- programmers/level1/Solution12928.java
+++ programmers/level1/Solution12928.java
@@ -26,12 +26,12 @@ class Solution12928 {
     public static int solution(int n) {
         int answer = 0;
 
-        for(int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n / 2; i++) {
             if(n % i == 0) {
                 answer += i;
             }
         }
 
-        return answer;
+        return answer + n;
     }
 }
