e1f2aff8a0fd24ae6d209dc5d4333e70955404b2
ahah525 <ahah525@naver.com>
2022-11-18 12:02:39 +0900

    42584-주식가격(v2.성공)

--- programmers/level2/Solution42584.java
+++ programmers/level2/Solution42584.java
@@ -5,14 +5,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * v1. 2중 for 사용 (실패: 테스트 1 제외하고 모두 실패)
+ * [한줄평] 구현은 어렵지 않으나, 문제에 대한 이해가 부족하면 풀 수 없는 문제였다.
+ * - "가격이 떨어지지 않은 기간" 을 구하는 것이기 때문에, 가격이 떨어진 이후에 대해서는 카운팅할 필요가 없다는 것이 중요 포인트!
+ * - 5 -> 4로 가격이 떨어진다고 했을 때, 실제로 1초간 가격이 5로 유지된 것이므로 0이 아닌 1로 계산해야 한다
+ *
+ * v1. 2중 for 문 사용 (실패: 테스트 1 제외하고 모두 실패)
+ * - i : 0 ~ n - 1
+ * - j : i + 1 ~ n - 1
+ * v2. 2중 for 문 사용 (성공)
+ *  - i : 0 ~ n - 1
+ *  - j : i ~ n - 2
+ *
+ * @see <a href="https://school.programmers.co.kr/questions/39058">질문 힌트</a>
+ * @see <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12916/solution_groups?language=java">다른 풀이</a>
  */
 class Solution42584 {
     public static void main(String[] args) {
         // [4, 3, 1, 1, 0]
-        int[] prices = {1, 2, 3, 2, 3};
-        List<Integer> answer1 = solution(prices);
+        int[] prices1 = {1, 2, 3, 2, 3};
+        List<Integer> answer1 = solution(prices1);
         System.out.println(answer1);
+
+        // 반례) [1, 1, 1, 1, 0]
+        int[] prices2 = {5, 4, 3, 2, 5};
+        List<Integer> answer2 = solution(prices2);
+        System.out.println(answer2);
     }
 
     /**
@@ -24,13 +41,12 @@ class Solution42584 {
      */
     public static List<Integer> solution(int[] prices) {
         List<Integer> answer = new ArrayList<>();
-
-        for(int i = 0; i < prices.length; i++) {
+        int n = prices.length;
+        for(int i = 0; i < n; i++) {
             int t = 0;
-            for(int j = i + 1; j < prices.length; j++) {
-                if(prices[i] <= prices[j]) {
-                   t++;
-                }
+            for(int j = i; j < n - 1; j++) {
+                if(prices[i] <= prices[j]) t++;
+                else break;
             }
             answer.add(t);
         }
