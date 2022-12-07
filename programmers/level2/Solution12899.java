c74eabde486a78c33e7dcbf3a03dacd6e3a8b2e7
ahah525 <ahah525@naver.com>
2022-12-07 10:28:08 +0900

    12899-124 나라의 숫자(v1.성공)

---
+++ programmers/level2/Solution12899.java
@@ -0,0 +1,52 @@
+package programmers.level2;
+
+
+/**
+ * [문제명] 124 나라의 숫자
+ * [한줄평] BFS 가 아니라 수학적으로 접근해야겠다는 것은 빨리 알아차렸지만 수식을 세우는데 시간이 좀 걸렸던 문제였다.
+ * 나머지가 0인 경우에 대한 처리를 하는 부분에서 시간이 걸렸고 부모와 자식간의 관계를 수식으로 써보고 나서야 바로 깨달았다.
+ * v1. 수학적 접근(v1.성공)
+ * <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12899">문제</a>
+ * <a href="https://postimg.cc/hzTvgLnh">그림 풀이</a>
+ */
+class Solution12899 {
+    public static void main(String[] args) {
+        // 1
+        int n1 = 1;
+        System.out.println(solution(n1));
+
+        // 2
+        int n2 = 2;
+        System.out.println(solution(n2));
+
+        // 4
+        int n3 = 3;
+        System.out.println(solution(n3));
+
+        // 11
+        int n4 = 4;
+        System.out.println(solution(n4));
+    }
+
+    /**
+     * @param n 50,000,000이하의 자연수
+     * @return n을 124 나라에서 사용하는 숫자로 바꾼 값을 return
+     */
+    public static String solution(int n) {
+        // 1 = 나머지 1
+        // 2 = 나머지 2
+        // 4 = 나머지 0
+        String s = "412";
+        StringBuilder sb = new StringBuilder();
+        while(n != 0) {
+            int r = n % 3;  // 3으로 나눈 나머지
+            sb.append(s.charAt(r));
+            // 1. 나머지가 1 or 2 일 경우, 부모값 = 3으로 나눈 몫
+            n /= 3;
+            // 2. 나머지가 0 일 경우, 부모값 = 3으로 나눈 몫 - 1
+            if(r == 0)
+                n -= 1;
+        }
+        return sb.reverse().toString();
+    }
+}
