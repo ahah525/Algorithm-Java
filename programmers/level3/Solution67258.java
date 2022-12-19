9cfd16148ffec7b081c76bbf0ea472fd0215f20b
ahah525 <ahah525@naver.com>
2022-12-19 22:42:10 +0900

    67258 - [카카오 인턴] 보석 쇼핑(v2.성공)

--- programmers/level3/Solution67258.java
+++ programmers/level3/Solution67258.java
@@ -9,41 +9,38 @@ import java.util.Set;
 /**
  * [문제명] [카카오 인턴] 보석 쇼핑
  * [풀이시간] 1시간
- * [한줄평] 구현을 하면서도 시간초과가 날 것 같다고 생각했는데, 결국 시간초과가 났다.
- * v1. 2중 for 문 - 완전탐색(실패 - 효율성 테스트 1~5, 7~15 실패)
+ * [한줄평] 구현을 하면서도 시간초과가 날 것 같다고 생각했는데, 결국 시간초과가 났다. 결국 힌트를 얻어 투포인터로 해결했는데 투포인터는 무조건 복습이 필요하다!!
+ * 투포인터에서 잊지밀아야할 점은 end = n 이 되기전까지 반복한다는것(종료조건)이고, start++ 와 end++ 시점이 중요하다.
+ * v1. Set, Map, 2중 for 문 - 완전탐색(실패 - 효율성 테스트 1~5, 7~15 실패)
  * - 시작 인덱스, 끝 인덱스를 바꿔가며 완전 탐색 수행
+ * v2. Set, Map, 투포인터(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/15008/lessons/67258">문제</a>
  */
 class Solution67258 {
     public static void main(String[] args) {
         // [3, 7]
         String[] gems1 = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        System.out.println(solution1(gems1));
+        System.out.println(solution2(gems1));
 
         // [1, 3]
         String[] gems2 = {"AA", "AB", "AC", "AA", "AC"};
         int[] course2 = {2,3,5};
-        System.out.println(solution1(gems2));
+        System.out.println(solution2(gems2));
 
         // [1, 1]
         String[] gems3 = {"XYZ", "XYZ", "XYZ"};
-        System.out.println(solution1(gems3));
+        System.out.println(solution2(gems3));
 
         // [1, 5]
         String[] gems4 = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
-        System.out.println(solution1(gems4));
+        System.out.println(solution2(gems4));
     }
 
-    /**
-     *
-     * @param gems
-     * @return
-     */
     public static int[] solution1(String[] gems) {
         int[] answer = {1, 1};
-        int n = gems.length;
-        int minLen = n; // 최소 구간 길이
-        int minIdx = -1; // 최소 구간 시작 인덱스
+        int n = gems.length;   // 보석 개수
+        int minLen = n;     // 최소 구간 길이
+        int minIdx = -1;    // 최소 구간 시작 인덱스
         // 1. 보석 종류 구하기
         Set set = new HashSet();
         for(String s : gems) {
@@ -53,6 +50,7 @@ class Solution67258 {
         if(kind == 1) return answer;
         // 2. 시작점
         for(int i = 0; i < n; i++) {
+            // 현재 범위 = start ~ (end - 1)
             String start = gems[i]; // 시작값
             Map<String, Integer> map = new HashMap<>(); // (보석명, 개수)
             for(int j = i; j < n; j++) {
@@ -77,6 +75,56 @@ class Solution67258 {
         }
         answer[0] = minIdx + 1;
         answer[1] = answer[0] + minLen;
+        return answer;
+    }
+
+    /**
+     * @param gems 진열대 번호 순서대로 보석들의 이름이 저장된 배열
+     * @return 모든 보석을 하나 이상 포함하는 가장 짧은 구간의 시작 진열대 번호와 끝 진열대 번호를 담은 배열
+     */
+    public static int[] solution2(String[] gems) {
+        int[] answer = {1, 1};
+        int n = gems.length;    // 보석 개수
+        int minLen = Integer.MAX_VALUE;     // 최소 구간 길이
+        int minS = 0;    // 최소 구간 시작 인덱스
+        int minE = 0;    // 최소 구간 끝 인덱스
+        // 1. 보석 종류 구하기
+        Set set = new HashSet();
+        for(String s : gems) {
+            set.add(s);
+        }
+        int kind = set.size(); // 보석 종류수
+        // 2. 투포인터로 시작, 끝 인덱스 옮겨가며 끝 인덱스가 n 이 될 때까지 반복
+        int start = 0;  // 시작 인덱스
+        int end = 0;    // 끝 인덱스
+        Map<String, Integer> map = new HashMap<>(); // (보석명, 개수)
+        while(true) {
+            // 현재 범위 = start ~ (end - 1) -> end 를 포함하지 않음!!
+            if(map.size() == kind) {
+                // 1. 보석 종류를 모두 포함하고 있는 경우 -> start 값 빼기
+                String startName = gems[start++];
+                map.put(startName, map.get(startName) - 1);
+                if(map.get(startName) == 0) {
+                    map.remove(startName);
+                }
+                // 1-2. 최솟값 업데이트
+                if(minLen > end - start) {
+                    minLen = end - start;
+                    minS = start - 1;   // 위에서 start++ 을 해준 것을 고려해 (start - 1) 값이 최소 구간의 진짜 start 값이다.
+                    minE = end - 1;     // 해당 범위에서 실제로 end 값은 포함되지 않은 상태이므로 (end - 1) 값이 최소 구간의 end 값이다.
+                }
+            } else if(end == n) {
+                // 2. end 가 n 이 되면 종료 -> 위치 중요!!
+                break;
+            }else {
+                // 3. 보석 종류가 아직 부족한 경우 -> end 값 넣기
+                String endName = gems[end++];
+                map.put(endName, map.getOrDefault(endName, 0) + 1);
+            }
+        }
+        // 3. 실제 값은 +1 연산한 값으로 반환
+        answer[0] = minS + 1;
+        answer[1] = minE + 1;
         return answer;
     }
 }
