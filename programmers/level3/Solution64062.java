f0109d113a4635ebf8d1b6a0c78db8d7421390a8
ahah525 <ahah525@naver.com>
2022-12-21 14:50:19 +0900

    64062-징검다리 건너기(v2.성공)

--- programmers/level3/Solution64062.java
+++ programmers/level3/Solution64062.java
@@ -15,7 +15,7 @@ class Solution64062 {
         // 3
         int[] stones1 = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
         int k1 = 3;
-        System.out.println(solution1(stones1, k1));
+        System.out.println(solution2(stones1, k1));
     }
 
     //
@@ -47,5 +47,55 @@ class Solution64062 {
                     return answer;
             }
         }
+    }
+
+    /**
+     * @param stones 디딤돌에 적힌 숫자가 순서대로 담긴 배열
+     * @param k 한 번에 건너뛸 수 있는 디딤돌의 최대 칸수
+     * @return 최대 몇 명까지 징검다리를 건널 수 있는지
+     */
+    public static int solution2(int[] stones, int k) {
+        int min = 1;            // 건널 수 있는 최소 인원
+        int max = 200000000;    // 건널 수 있는 최대 인원
+        int answer = 0;
+        // min, max 가 엇갈릴 때까지 반복
+        while(min <= max) {
+            int mid = (min + max) / 2;  // 건너야 하는 인원
+            // 1.해당 인원이 모두 다리를 건널 수 있는지 검사
+            if(canPass(stones, k, mid)) {
+                // 1-1. 다리를 건널 수 있으면 오른쪽 탐색
+                min = mid + 1;
+                answer = Math.max(answer, mid); // 현재까지 최대 디딤돌 수 = mid
+            } else {
+                // 1-2. 다리를 건널 수 없으면 왼쪽 탐색
+                max = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    /**
+     * @param stones 디딤돌에 적힌 숫자가 순서대로 담긴 배열
+     * @param k 한 번에 건너뛸 수 있는 디딤돌의 최대 칸수
+     * @param n 다리를 건너야 하는 인원
+     * @return n 명이 모두 다리를 건널 수있는지 여부
+     */
+    public static boolean canPass(int[] stones, int k, int n) {
+        int cnt = 0;    // 연속되는 0 이하의 값
+        for(int i = 0; i < stones.length; i++) {
+            // 1. (n - 1) 명이 건너고 난 후 디딤돌 값 구하기
+            int num = stones[i] - (n - 1);
+            // 2. 디딤돌 값이 0 이하인 디딤돌 개수 구하기(연속적인 구간에 속한 디딤돌)
+            if (num <= 0) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            // 3. 해당 연속 구간에 속한 디딤돌이 k개 이상이면 한 번에 k칸을 점프해서 0이 아닌 디딤돌로 갈 수가 없다
+            if(cnt >= k) {
+                return false;
+            }
+        }
+        return true;
     }
 }
