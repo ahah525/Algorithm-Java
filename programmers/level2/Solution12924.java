4cf4e96aeb28016ebadadebde1d66eff1ae47d44
ahah525 <ahah525@naver.com>
2023-01-09 10:04:02 +0900

    12924-숫자의 표현(v1.성공)

--- programmers/level2/Solution12924.java
+++ programmers/level2/Solution12924.java
@@ -20,27 +20,28 @@ class Solution12924 {
      */
     public static int solution(int n) {
         int answer = 0;
-        int left = 1;
-        int right = 1;
         int sum = 0;
-
-        while(true) {
+        int l = 1;  // 왼쪽 포인터
+        int r = 1;  // 오른쪽 포인터
+        // 종료조건 명시(r = n + 2 이면, 끝구간값이 n + 1 이 되므로 종료)
+        while(r <= n + 1) {
+            // 1. 전처리 작업(누적합 계산) -> 포인터 이동(순서 중요!)
+            if(sum >= n) {
+                // 왼쪽 포인터를 오른쪽으로 1칸 이동
+                sum -= l;
+                l++;
+            } else {
+                // 오른쪽 포인터를 오른쪽으로 1칸 이동
+                sum += r;
+                r++;
+            }
+            // 2. 정답을 찾았을 때 처리
             if(sum == n) {
-//                System.out.println("left = " + left);
-//                System.out.println("right = " + right);
+                answer++;
+//                System.out.println("l = " + l);
+//                System.out.println("r = " + r);
 //                System.out.println("sum = " + sum);
 //                System.out.println("=========");
-                answer++;
-                sum -= left;
-                left++;
-            } else if(sum > n) {
-                sum -= left;
-                left++;
-            } else if(right == n + 1) {
-                break;
-            } else {
-                sum += right;
-                right++;
             }
         }
         return answer;
