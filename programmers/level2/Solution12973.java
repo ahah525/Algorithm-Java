40d2f0e7ca8e5638c77d3642960535e39ea042b9
ahah525 <ahah525@naver.com>
2023-01-11 11:44:08 +0900

    12973-짝지어 제거하기(v2.성공)

--- programmers/level2/Solution12973.java
+++ programmers/level2/Solution12973.java
@@ -1,24 +1,31 @@
 package programmers.level2;
 
 
+import java.util.Stack;
+
 /**
  * [문제명] 짝지어 제거하기
- * [풀이시간] 15분
- * [한줄평]
+ * [풀이시간] 45분(15분 + 30분)
+ * [한줄평] 이 문제는 StringBuilder 로 delete 해가면서 푸는 게 아니라, 스택을 이용해야만 시간초과를 해결할 수 있는 문제로 시간복잡도 계산이 필요하다!!
  * v1. StringBuilder(실패 - 효율성 테스트 모두 시간초과)
  * - 시간초과가 날 것을 고려해 StringBuilder 를 써서 포인터를 옮겨가는 방식으로 풀었는데 시간초과가 났다.
+ * - 제한시간 1초일 때, 약 1억개 연산이 가능하다. 즉, N이 1억(10^8) 이상이면 시간초과가 난다.
+ * - while 문 = O(N), StringBuilder 의 delete() = O(N) -> 대략 O(N^2) 인데, 문자열의 길이가 최대 10^6 이기때문에 O(10^12) 으로는 시간초과가 난다.
+ * v2. Stack(성공)
+ * -
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12973">문제</a>
  */
 class Solution12973 {
     public static void main(String[] args) {
         // 1
-        System.out.println(solution("baabaa"));
+        System.out.println(solution2("baabaa"));
 
         // 0
-        System.out.println(solution("cdcd"));
+        System.out.println(solution2("cdcd"));
     }
 
-    public static int solution(String s) {
+    // 실패
+    public static int solution1(String s) {
         int i = 0;  // 포인터
         StringBuilder sb = new StringBuilder(s);
         while(true) {
@@ -40,5 +47,28 @@ class Solution12973 {
                 i++;
             }
         }
+    }
+
+    /**
+     * @param s 알파벳 소문자로 이루어진 문자열(길이는 1,000,000이하)
+     * @return 짝지어 제거하기를 성공적으로 수행할 수 있는지(성공적으로 수행할 수 있으면 1을, 아닐 경우 0)
+     */
+    public static int solution2(String s) {
+        Stack<Character> stack = new Stack();
+        for(char c : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                // 1. 스택이 비었으면 넣기
+                stack.push(c);
+            } else {
+                if(c == stack.peek()) {
+                    // 2. 맨위에 값이랑 같으면 pop
+                    stack.pop();
+                } else {
+                    // 3. 맨위에 값이랑 다르면 넣기
+                    stack.push(c);
+                }
+            }
+        }
+        return stack.isEmpty() ? 1 : 0;
     }
 }
