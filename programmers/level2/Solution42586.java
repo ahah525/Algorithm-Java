572a0db77cf58283c98057744fe68c18ef791727
ahah525 <ahah525@naver.com>
2023-02-06 12:26:21 +0900

    42586-기능개발(2_v1.성공)

--- programmers/level2/Solution42586.java
+++ programmers/level2/Solution42586.java
@@ -7,9 +7,11 @@ import java.util.Queue;
 
 /**
  * [문제명] 기능개발
+ * [풀이시간] 25분
  * [한줄평] 큐를 이용해야겠다는 것은 쉽게 떠올렸지만 int 끼리 연산한 결과는 int 라는 것을 간과해서 틀렸던 문제였다.
- * v1. 소요일수 구할 때 / 연산 결과가 int -> 올림 적용 안됨(실패)
- * v2. 소요일수 구할 때 / 연산 결과가 double -> 올림 적용 성공(성공)
+ * 1_v1. 소요일수 구할 때 / 연산 결과가 int -> 올림 적용 안됨(실패)
+ * 1_v2. 소요일수 구할 때 / 연산 결과가 double -> 올림 적용 성공(성공)
+ * 2_v1. (성공)
  */
 class Solution42586 {
     public static void main(String[] args) {
@@ -36,36 +38,27 @@ class Solution42586 {
      */
     public static List<Integer> solution(int[] progresses, int[] speeds) {
         List<Integer> answer = new ArrayList();
-        int n = progresses.length;  // 작업 개수
-
-        // 1. 각 작업 끝내는데 걸리는 일 수 계산해서 큐에 넣기
+        // 1. 각 기능 소요일수 계산해서 큐에 넣기
         Queue<Integer> q = new LinkedList<>();
-        for(int i = 0; i < n; i++) {
-            // int days = (int) Math.ceil((100 - progresses[i]) / speeds[i]);   // / 결과가 이미 int 값이라 올림을 해도 소용이 없었던 오류
+        for(int i = 0; i < progresses.length; i++) {
             int days = (int) Math.ceil((double) (100 - progresses[i]) / speeds[i]);   // 소요 일수
             q.add(days);
         }
-
         // 2. 배포할 때마다 완성된 기능 개수 구하기
-        while(true) {
-            if(q.isEmpty()) {
-                break;
-            }
-            // 1. 맨 앞에서 배포해야하는 작업 한 개 빼기
-            int v = q.poll();
-            int cnt = 1;
-            // 2. 뒤에 작업이 이미 완료되었다면 같이 배포하기
-            while(true) {
-                if(q.isEmpty()) {
+        while(!q.isEmpty()) {
+            // 1. 맨 앞에서 배포해야하는 기능 한 개 빼기
+            int first = q.poll();   // 맨앞 기능의 소요일수
+            int cnt = 1;        // 배포한 기능 개수
+            // 2. 뒤에 기능이 이미 완료되었다면 같이 배포하기
+            while(!q.isEmpty()) {
+                // 2-1. 맨앞 기능 소요일 수 < 뒤에 있는 기능 소요일수 -> 같이 배포X
+                if(first < q.peek())
                     break;
-                }
-                int nv = q.peek();
-                if(v < nv) {
-                    break;
-                }
+                // 2-2. 맨앞 기능 소요일 수 >= 뒤에 있는 기능 소요일수 -> 같이 배포O
                 q.poll();
                 cnt++;
             }
+            // 3. 이번에 배포되는 기능 수 추가
             answer.add(cnt);
         }
         return answer;
