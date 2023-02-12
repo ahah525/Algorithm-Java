b1a04a798368e3223002a8464c178b213f25c79e
ahah525 <ahah525@naver.com>
2023-02-12 23:17:32 +0900

    42898-등굣길(1_v2.성공)

--- programmers/level3/Solution42898.java
+++ programmers/level3/Solution42898.java
@@ -1,30 +1,21 @@
 package programmers.level3;
 
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * [문제명] 등굣길
- * [풀이시간] (40분)
- * [한줄평]
+ * [풀이시간] 1시간(40분 + 20분)
+ * [한줄평] 최단 경로 개수를 찾기 위해 당연하게도 BFS 를 썼는데, 단순하게 DP 로 풀어야 하는 문제였다. 예전에 비슷한 풀어봤었는데 오랜만에 푸니까 어려웠다.
  * 1_v1. DP, BFS(실패- 1, 5 시간초과)
+ * 1_v2. DP(성공)
+ * [점화식] d[i][j] = d[i-1][j] + d[i][j-1]
+ * - d[i][j]: (1, 1)에서 (i, j)까지 가는 최단경로수
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42898">문제</a>
+ * @See <a href="https://moonsbeen.tistory.com/75">풀이 참고</a>
  */
 class Solution42898 {
     public static void main(String[] args) {
         //
         System.out.println();
-    }
-
-    public class P {
-        int x;
-        int y;
-
-        public P(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
     }
 
     /**
@@ -34,43 +25,33 @@ class Solution42898 {
      * @return 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수를 1,000,000,007로 나눈 나머지
      */
     public int solution(int m, int n, int[][] puddles) {
-        int[][] map = new int[n][m];
+        int mod = 1000000007;
+        int[][] map = new int[n + 1][m + 1];
         for(int[] p : puddles) {
-            map[p[1] - 1][p[0] - 1] = 1;
+            map[p[1]][p[0]] = -1;
         }
-        return bfs(n, m, map);
-    }
-
-    public int bfs(int n, int m, int[][] map) {
-        int[] dx = {1, 0};
-        int[] dy = {0, 1};
-        long[][] visited = new long[n][m];
-        Queue<P> q = new LinkedList<>();
-        q.add(new P(0, 0));
-        visited[0][0] = 1;
-        while(!q.isEmpty()) {
-            P p = q.poll();
-            int x = p.x;
-            int y = p.y;
-            for(int i = 0; i < 2; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if(0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if(map[nx][ny] == 0) {
-                        if(visited[nx][ny] == 0) {
-                            q.add(new P(nx, ny));
-                        }
-                        visited[nx][ny] = (visited[nx][ny] + visited[x][y]) % 1000000007 ;
-                    }
+        // 원래는 map[1][1] = 1 로 해야하지만 그렇게 하면 아래 반복문에서 map[1][1] = 0 + 0 = 0 으로 값이 덮어쓰여지는 문제가 발생한다.
+        map[1][0] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                // 현재 좌표가 웅덩이면 갈 수 있는 경로수 = 0 -> 패스
+                if(map[i][j] == -1) continue;
+                // 왼쪽 좌표가 웅덩이가 아닐 때만 경로수 더하기
+                if(map[i - 1][j] != -1) {
+                    map[i][j] += map[i - 1][j] % mod;
+                }
+                // 오른쪽 좌표가 웅덩이가 아닐 때만 경로수 더하기
+                if(map[i][j - 1] != -1) {
+                    map[i][j] += map[i][j - 1] % mod;
                 }
             }
         }
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < m; j++) {
-        //         System.out.print(visited[i][j] + " ");
+        // for(int i = 1; i <= n; i++) {
+        //     for(int j = 1; j <= m; j++) {
+        //         System.out.print(map[i][j] + " ");
         //     }
         //     System.out.println();
         // }
-        return (int) (visited[n - 1][m - 1] % 1000000007);
+        return map[n][m] % 1000000007;
     }
 }
