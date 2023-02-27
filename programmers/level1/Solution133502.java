d4598020799f05e000a4f070e70e19c053d577e8
ahah525 <ahah525@naver.com>
2023-02-27 22:00:18 +0900

    133502-햄버거 만들기(1_v4.성공)

--- programmers/level1/Solution133502.java
+++ programmers/level1/Solution133502.java
@@ -6,10 +6,11 @@ import java.util.Stack;
 /**
  * [문제명] 햄버거 만들기
  * [풀이시간] 40분(10분 + 10분 + 20분)
- * [한줄평]
+ * [한줄평] 어떻게 하면 효율적으로 풀 수 있을지 고민을 해야했던 문제였다. 다른 풀이를 보고 배열로도 풀수 있구나라고 깨달았다..
  * 1_v1. String.replace()(실패 - 3~12, 18 실패)
  * 1_v2. StringBuilder.indexOf() + delete() (실패 - 4, 5, 12 시간초과)
- * 1_v3. Stack (성공)
+ * 1_v3. Stack(성공)
+ * 1_v4. int[](성공) -> 추천
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/133502">문제</a>
  * @See <a href="https://leejams.github.io/%ED%96%84%EB%B2%84%EA%B1%B0-%EB%A7%8C%EB%93%A4%EA%B8%B0/">반례</a>
  */
@@ -42,6 +43,22 @@ class Solution133502 {
                     stack.push(b);
                     stack.push(a);
                 }
+            }
+        }
+        return answer;
+    }
+
+    public static int solution2(int[] ingredient) {
+        int answer = 0;
+        int[] stack = new int[ingredient.length];
+        int i = 0;
+        for(int n : ingredient) {
+            stack[i] = n;
+            if(i >= 3 && stack[i] == 1 && stack[i - 1] == 3 && stack[i - 2] == 2 && stack[i - 3] == 1) {
+                i -= 3;
+                answer++;
+            } else {
+                i++;
             }
         }
         return answer;
