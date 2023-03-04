eb32ed09b2df34d51ee237fdb2e74e50fe6f3e56
ahah525 <ahah525@naver.com>
2023-03-04 11:50:01 +0900

    17677-[1차] 뉴스 클러스터링(2_v1.성공)

--- programmers/level2/Solution17677.java
+++ programmers/level2/Solution17677.java
@@ -4,18 +4,13 @@ import java.util.*;
 
 /**
  * [문제명] [1차] 뉴스 클러스터링
- * [한줄평] 문제 설명대로 풀기만 하면 되는 구현문제였다. 실제로 집합을 구하는 것이 아니라 map 에 원소 개수를 저장해두고 수학적으로 풀었다.
- * v1. map 2개, 수학으로 풀이(성공)
- * str1, str2: 2이상 1,000 이하의 길이인 문자열
- * answer: 두 문자열 사이의 유사도
- * ---------------------------------------
- * 두 집합 A, B 사이의 자카드 유사도 J(A, B)
- * = 두 집합의 교집합 크기 / 두 집합의 합집합 크기
- * 집합 A와 집합 B가 모두 공집합일 경우, 나눗셈이 정의되지 않으니 따로 J(A, B) = 1
- * ---------------------------------------
- * 문자열 사이의 자카드 유사도 계산
- * - 입력으로 들어온 문자열은 두 글자씩 끊어서 다중집합의 원소로 만든다. (영문자로 된 글자 쌍만 유효하고, 기타 공백이나 숫자, 특수 문자가 들어있는 경우는 그 글자 쌍을 버린다.)
- * - 다중집합 원소 사이를 비교할 때, 대문자와 소문자의 차이는 무시한다.
+ * [풀이시간] / 32분
+ * [한줄평] 문제 설명대로 풀기만 하면 되는 구현문제였다. 실제로 집합을 구하는 것이 아니라 map 에 원소 개수를 저장해두고 수학적으로 풀었다. / 2번째 풀어서 쉽게 풀수 있었다.
+ * 1_v1. map 2개, 수학으로 풀이(성공)
+ * - substring() 으로 길이가 2인 문자열 추출
+ * 2_v1. map 2개(성공) -> 추천
+ * - charAt() 2개를 이어 붙여 문자열 추출
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17677">문제</a>
  */
 class Solution17677 {
     public static void main(String[] args) {
@@ -27,6 +22,11 @@ class Solution17677 {
         System.out.println(answer);
     }
 
+    /**
+     * @param str1 각 문자열의 길이는 2 이상, 1,000 이하
+     * @param str2 각 문자열의 길이는 2 이상, 1,000 이하
+     * @return 입력으로 들어온 두 문자열의 자카드 유사도(유사도 값은 0에서 1 사이의 실수이므로, 이를 다루기 쉽도록 65536을 곱한 후에 소수점 아래를 버리고 정수부만)
+     */
     public static int solution(String str1, String str2) {
         double res = 0;
         int n1 = 0; // 교집합 크기
@@ -76,5 +76,54 @@ class Solution17677 {
             }
         }
         return map;
+    }
+
+    public int solution2(String str1, String str2) {
+        Map<String, Integer> map1 = new HashMap<>();
+        Map<String, Integer> map2 = new HashMap<>();
+        // 1. 소문자 변환
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+        // 2. str1, str2 다중 집합 만들기
+        for(int i = 0; i < str1.length() - 1; i++) {
+            char c1 = str1.charAt(i);
+            char c2 = str1.charAt(i + 1);
+            if('a' <= c1 && c1 <= 'z' && 'a' <= c2 && c2 <= 'z') {
+                String s = String.valueOf(c1) + String.valueOf(c2);
+                map1.put(s, map1.getOrDefault(s, 0) + 1);
+            }
+        }
+        for(int i = 0; i < str2.length() - 1; i++) {
+            char c1 = str2.charAt(i);
+            char c2 = str2.charAt(i + 1);
+            if('a' <= c1 && c1 <= 'z' && 'a' <= c2 && c2 <= 'z') {
+                String s = String.valueOf(c1) + String.valueOf(c2);
+                map2.put(s, map2.getOrDefault(s, 0) + 1);
+            }
+        }
+        // 3. 둘 다 공집합일 경우 바로 리턴
+        if(map1.size() == 0 && map2.size() == 0) {
+            return 65536;
+        }
+        // 4. 자카드 유사도 계산
+        int a = 0;  // 교집합 크기
+        int b = 0;  // 합집합 크기
+        for(String key : map1.keySet()) {
+            if(map2.containsKey(key)) {
+                // O, O
+                a += Math.min(map1.get(key), map2.get(key));
+                b += Math.max(map1.get(key), map2.get(key));
+            } else {
+                // O, X
+                b += map1.get(key);
+            }
+        }
+        for(String key : map2.keySet()) {
+            if(!map1.containsKey(key)) {
+                // X, O
+                b += map2.get(key);
+            }
+        }
+        return (int) ((double) a / b * 65536);
     }
 }
