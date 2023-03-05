3c82d6f00a75a464e4ea848152390d6fac4f8a15
ahah525 <ahah525@naver.com>
2023-03-05 21:18:06 +0900

    42587-프린터(2_v2.성공)

--- programmers/level2/Solution42587.java
+++ programmers/level2/Solution42587.java
@@ -1,89 +1,33 @@
 package programmers.level2;
 
-import java.util.Iterator;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
  * [문제명] 프린터
  * [풀이시간] / 16분
- * [한줄평] 큐에서 값을 꺼내지 않고 탐색하기 위해 Iterator 사용법을 검색해서 풀었던 문제였다. / 2번쨰 풀다보니 쉽게 풀 수 있었다.
+ * [한줄평] Iterator 사용법을 검색해서 풀었던 문제였다. / 2번째라서 쉽게 풀었지만 우선순위큐를 이용한 풀이방법이 있다는 것도 알게되었다.
  * 1_v1. Queue, Iterator(성공)
- * 2_v1. Queue, 향상된 for 문(성공) -> 추천
+ * 2_v1. Queue, 향상된 for 문(성공)
+ * 2_v2. PriorityQueue(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42587">문제</a>
+ * @See <a href="https://fbtmdwhd33.tistory.com/223">다른 풀이 참고</a>
  */
 class Solution42587 {
     public static void main(String[] args) {
         // 1
         int[] priorities = {2, 1, 3, 2};
         int location = 2;
-        int answer = solution(priorities, location);
+        int answer = solution2(priorities, location);
         System.out.println(answer);
     }
 
-    public static class Docs {
-        int location;   // 원래 위치
-        int priority;   // 중요도
-
-        public Docs(int location, int priority) {
-            this.location = location;
-            this.priority = priority;
-        }
-
-        public int getLocation() {
-            return location;
-        }
-
-        public int getPriority() {
-            return priority;
-        }
-    }
-
     /**
-     * @param priorities 문서 중요도
-     * @param location 내가 인쇄를 요청한 문서 위치
-     * @return 내가 인쇄를 요청한 문서 순서의 실제 인쇄 순서
+     * 2_v1
      */
-    public static int solution(int[] priorities, int location) {
-        int answer = 0;
-        Queue<Docs> q = new LinkedList<>();
-
-        // Q 만들기
-        for(int i = 0; i < priorities.length; i++) {
-            q.add(new Docs(i, priorities[i]));
-        }
-
-        while(true) {
-            // 1. 맨앞 문서 1개 꺼내기
-            Docs now = q.poll();
-            // 2. 현재 문서보다 더 중요한 문서가 있는지 검사
-            // Iterator 로 큐에 담긴 모든 값 확인하기
-            boolean isAdd = false;  // 현재 문서를 다시 넣었는지 여부
-            Iterator<Docs> iterator = q.iterator();
-            while(iterator.hasNext()) {
-                Docs next = iterator.next();
-                // 현재 큐에 방금 꺼낸 문서보다 더 중요한 문서가 있으면 맨 뒤로 넣기
-                if(now.getPriority() < next.priority) {
-                    q.add(now);
-                    isAdd = true;
-                    break;
-                }
-            }
-            // 3. 현재 문서를 뒤에 다시 넣지 않았으면 빼낸 개수 카운팅
-            if(!isAdd) {
-                answer++;
-                // 몇 번째로 꺼냈는지 체킹해야되는 문서를 찾으면 바로 리턴
-                if(now.getLocation() == location) {
-                    return answer;
-                }
-            }
-        }
-    }
-
-    /**
-     * 2번 풀이
-     */
-    class Doc {
+    static class Doc {
         int index;   // 몇번째 문서인지
         int priority;// 중요도
 
@@ -93,7 +37,7 @@ class Solution42587 {
         }
     }
 
-    public int solution2(int[] priorities, int location) {
+    public static int solution2(int[] priorities, int location) {
         int answer = 0; // 인쇄한 문서 개수
         // 1. 대기큐 초기화
         Queue<Doc> q = new LinkedList<>();
@@ -122,7 +66,7 @@ class Solution42587 {
      * @param standard 가장 앞에 있는 문서의 중요도
      * @return 가장 앞에 있는 문서를 프린트할 수 있는지 여부
      */
-    public boolean canPrint(Queue<Doc> q, int standard) {
+    public static boolean canPrint(Queue<Doc> q, int standard) {
         for(Doc d : q) {
             // 대기목록에서 더 중요도가 높은 문서가 1개라도 있으면 false
             if(standard < d.priority) {
@@ -130,5 +74,33 @@ class Solution42587 {
             }
         }
         return true;
+    }
+
+    /**
+     * 2_v3
+     */
+    public int solution3(int[] priorities, int location) {
+        int answer = 0;
+        // 1. 문서 중요도 내림차순 정렬
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int p : priorities) {
+            pq.add(p);
+        }
+        l1:
+        while(!pq.isEmpty()) {
+            for(int i = 0; i < priorities.length; i++) {
+                // 2. 대기큐에서 꺼낸 중요도값과 같은 값을 가진 문서를 찾으면 큐에서 삭제, 카운팅
+                if(pq.peek() == priorities[i]) {
+                    // System.out.println(i);
+                    pq.poll();
+                    answer++;
+                    // 3. 꺼내서 삭제한 문서가 내가 앤쇄를 요청한 문서면 탈출
+                    if(i == location) {
+                        break l1;
+                    }
+                }
+            }
+        }
+        return answer;
     }
 }
