9cf90d03a76a0db63564f50a9166d02ea0189e5b
ahah525 <ahah525@naver.com>
2023-03-06 11:04:26 +0900

    43238-입국심사(1_v2.성공)

--- programmers/level3/Solution43238.java
+++ programmers/level3/Solution43238.java
@@ -1,15 +1,17 @@
 package programmers.level3;
 
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.Arrays;
 
 /**
  * [문제명] 입국심사
- * [풀이시간] (30분)
- * [한줄평]
+ * [풀이시간] 1시간(30분 + 30분)
+ * [한줄평] 이분탐색 문제라는 것을 알아도 어떻게 풀어야할지 감이 안잡혀서 결국 풀이를 보고 이해했던 문제다.
  * 1_v1. PriorityQueue(실패 - 6~9 시간초과)
+ * 2_v2. BinarySearch(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43238">문제</a>
+ * @See <a href="https://school.programmers.co.kr/questions/7919">풀이 힌트</a>
+ * @See <a href="https://swycha.tistory.com/122">풀이 참고</a>
  */
 class Solution43238 {
     public static void main(String[] args) {
@@ -17,28 +19,36 @@ class Solution43238 {
         System.out.println();
     }
 
-    class Test {
-        int play;// 심사 시간
-        long end; // 심사 끝나는 시각
-
-        Test(int play, long end) {
-            this.play = play;
-            this.end = end;
-        }
-    }
-
+    /**
+     * @param n 입국심사를 기다리는 사람 수
+     * @param times 각 심사관이 한 명을 심사하는데 걸리는 시간이 담긴 배열
+     * @return 모든 사람이 심사를 받는데 걸리는 시간의 최솟값
+     */
     public long solution(int n, int[] times) {
         long answer = 0;
-        // 1. (심사 끝나는 시각 + 심사 시간) 오름차순 정렬
-        Queue<Test> pq = new PriorityQueue<>((o1, o2) -> ((o1.end + o1.play) - (o2.end + o2.play)) > 0 ? 1 : -1);
-        for(int t : times) {
-            pq.add(new Test(t, 0));
-        }
-        // 2.
-        for(int i = 0; i < n; i++) {
-            Test t = pq.poll();
-            pq.add(new Test(t.play, t.end + t.play));
-            answer = Math.max(answer, t.end + t.play);
+        // 1. 심사 소요 시간 오름차순 정렬
+        Arrays.sort(times);
+        int m = times.length;   // 심사위원 수
+        // 2. 모든 사람이 심사를 받는데 걸리는 시간의 최솟값, 최댓값 초기화
+        long start = 1;
+        long end = n * (long) times[times.length - 1]; // 최댓값 = 심사 소요 시간이 가장 긴 심사위원이 n명을 심사하는 경우
+        // start, end 엇갈릴 때까지 반복
+        while(start <= end) {
+            long mid = (start + end) / 2;
+            // 3. mid 시간에 심사할 수 있는 사람수의 총합 구하기
+            long sum = 0;
+            for(int time : times) {
+                // 해당 심사위원이 mid 시간에 심사할 수 있는 사람수
+                sum += mid / time;
+            }
+            if(sum < n) {
+                // 4. mid 시간에서 n명을 모두 심사할 수 없으면, 오른쪽 범위 탐색
+                start = mid + 1;
+            } else {
+                // 5. mid 시간에서 n명 이상을 심사할 수 있으면, 왼쪽 범위 탐색
+                end = mid - 1;
+                answer = mid;   // mid 값 기록해두기
+            }
         }
         return answer;
     }
