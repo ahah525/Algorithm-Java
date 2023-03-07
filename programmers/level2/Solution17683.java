566184b2224b42a861277dc1ac14606fd2b9ceac
ahah525 <ahah525@naver.com>
2023-03-07 12:15:29 +0900

    17683-[3차] 방금그곡(2_v1.성공)

--- programmers/level2/Solution17683.java
+++ programmers/level2/Solution17683.java
@@ -1,9 +1,13 @@
 package programmers.level2;
 
+
 /**
  * [문제명] [3차] 방금그곡
- * [햔줄평] #이 붙은 음을 하나의 다른 음으로 치환해서 풀어야한다는 힌트를 받고 풀었던 문제였다. 이것만 알면 쉽게 풀 수 있는 구현 문제였다.
- * v1. replaceAll()로 #이 붙은 모든 음 -> 1문자로 치환(성공)
+ * [풀이시간] / 27분
+ * [한줄평] #이 붙은 음을 하나의 다른 음으로 치환해서 풀어야한다는 힌트를 받고 풀었던 문제였다. / 2번째 풀때 바로 치환을 해야겠다고 떠올려서 빨리 풀 수 있었던 것 같다.
+ * 1_v1. replaceAll()(성공)
+ * 2_v1. replace()(성공)
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17683">문제</a>
  */
 public class Solution17683 {
     public static void main(String[] args) {
@@ -26,8 +30,8 @@ public class Solution17683 {
         System.out.println(answer3);
     }
 
+    // 2_v1
     /**
-     *
      * @param m 멜로디를 담은 문자열
      * @param musicinfos 방송된 곡의 정보를 담고 있는 배열(시작 시각, 끝 시각, 음악 제목, 악보 정보)
      * - 멜로디와 악보에 사용되는 음은 C, C#, D, D#, E, F, F#, G, G#, A, A#, B 12개
@@ -37,59 +41,46 @@ public class Solution17683 {
      * - 조건이 일치하는 음악이 없을 때에는 “(None)”을 반환
      */
     public static String solution(String m, String[] musicinfos) {
+        int answerPlay = 0;
         String answerTitle = "";
-        int answerPlayTime = -1;
-        // 멜로디 변환
+        // 1. 멜로디 변환
         m = change(m);
-
-        for(String musicinfo : musicinfos) {
-            String[] info = musicinfo.split(",");
-            String[] start = info[0].split(":");    // 시작 시각
-            String[] end = info[1].split(":");      // 끝 시각
-            // 재생 시간
-            int playTime = (Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]))
+        for(String musicInfo : musicinfos) {
+            String[] info = musicInfo.split(",");
+            String[] start = info[0].split(":");    // 음악 시작 시각
+            String[] end = info[1].split(":");      // 음악 끝 시각
+            String title = info[2]; // 음악 제목
+            // 1. 재생 시간 계산
+            int play = (Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]))
                     - (Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]));
-
-            String title = info[2];         // 제목
-            String music = change(info[3]); // 악보 정보
-            int musicTime = music.length(); // 실제 음악 길이
-            String realMusic;               // 재생된 악보 정보
-
-            // 1. 실제 재생된 악보 정보 계산
-            if(playTime <= musicTime) {
-                realMusic = music.substring(0, playTime);
-            } else {
-                StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < playTime / musicTime; i++) {
-                    sb.append(music);
-                }
-                sb.append(music.substring(0, playTime % musicTime));
-                realMusic = sb.toString();
+            // 2. 악보 정보 변환
+            String sheet = change(info[3]);
+            // 3. 실제로 재생된 악보 정보 만들기
+            int len = sheet.length();   // 재생된 음악 길이
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < play / len; i++) {
+                sb.append(sheet);
             }
-            // 2. 기억한 멜로디가 해당 재생된 악보 정보에 있는지 확인
-            if(realMusic.contains(m)) {
-                if(answerPlayTime < playTime) {
-                    answerPlayTime = playTime;
+            sb.append(sheet.substring(0, play % len));
+            sheet = sb.toString();
+            // 4. 실제로 재생된 악보에 멜로디가 있는지 검사
+            if(sheet.contains(m)) {
+                if(play > answerPlay) {
+                    // 재생길이가 더 길 때만 답 수정
+                    answerPlay = play;
                     answerTitle = title;
                 }
             }
-
-//            System.out.println("title = " + title);
-//            System.out.println("musicTime = " + musicTime);
-//            System.out.println("realMusic = " + realMusic);
-//            System.out.println("======================================");
         }
-        if(answerTitle.equals("")) {
-            return "(None)";
-        }
-        return answerTitle;
+        return answerPlay == 0 ? "(None)" : answerTitle;
     }
 
-    public static String change(String m) {
-        return m.replaceAll("C#", "H")
-                .replaceAll("D#", "I")
-                .replaceAll("F#", "J")
-                .replaceAll("G#", "K")
-                .replaceAll("A#", "L");
+    // 악보 변환
+    public static String change(String s) {
+        return s.replace("C#", "H")
+                .replace("D#", "I")
+                .replace("F#", "J")
+                .replace("G#", "K")
+                .replace("A#", "L");
     }
 }
