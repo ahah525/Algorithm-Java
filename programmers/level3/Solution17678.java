c39b1c3618278509a05a070d9d44250eb84b2a1e
ahah525 <ahah525@naver.com>
2023-03-09 13:55:11 +0900

    17678-[1차] 셔틀버스(1_v2.성공)

--- programmers/level3/Solution17678.java
+++ programmers/level3/Solution17678.java
@@ -6,9 +6,11 @@ import java.util.Queue;
 
 /**
  * [문제명] [1차] 셔틀버스
- * [풀이시간] (1시간 8분)
- * [한줄평]
+ * [풀이시간] 1시간 20분(1시간 8분 + 12분)
+ * [한줄평] 이분탐색으로 풀 수 있다는 힌트를 얻긴했지만 혼자서 풀었던 문제. 다행히 반례도 빨리 찾았다.
  * 1_v1. PriorityQueue, 이분탐색(실패 - 19~22, 24 실패)
+ * 2_v1. PriorityQueue, 이분탐색(성공)
+ * [반례] 태울 수 있는 사람을 모두 태우고 대기열에 사람이 남아있을 경우, 콘이 첫번째에 있을 거라는 보장은 없음 => q.peek() 이 아닌 q.contains() 로 검사해야함!!
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17678">문제</a>
  */
 class Solution17678 {
@@ -40,53 +42,55 @@ class Solution17678 {
      * @return 콘이 무사히 셔틀을 타고 사무실로 갈 수 있는 제일 늦은 도착 시각
      */
     public static String solution(int n, int t, int m, String[] timetable) {
-        // 1. timetable 분 단위로 환산(오름차순 정렬)
+        // 1. timetable 분 단위로 환산해서 우선순위 큐에 넣기(오름차순 정렬)
         Queue<Integer> q = new PriorityQueue<>();
         for(String time : timetable) {
             String[] tt = time.split(":");
             q.add(Integer.parseInt(tt[0]) * 60 + Integer.parseInt(tt[1]));
         }
-        // 2.
-        int start = Math.min(q.peek() - 1, 540);    // 첫차시각
-//        System.out.println("start = " + start);
-        int end = 540 + t * (n - 1);    // 막차시각
-        int answer = 0;
+        // 2. 콘의 도착시각 최솟값 최댓값 초기화
+        int start = 0;  // 최솟값 = 00:00
+        int end = 540 + t * (n - 1);    // 최댓값 = 막차시각(막차시각보다 늦으면 탈 수 없음)
+        // 3. 제일 늦은 도착시각 구하기
+        int answer = 0; // 제일 늦은 도착시각
         while(start <= end) {
             int mid = (start + end) / 2;
             if(canTake(q, mid, n, t, m)) {
+                // 4. 콘이 mid 시점에 도착해서 셔틀을 탈 수 있으면 더 늦게 도착해도 됨 -> 오른쪽 탐색, 정답 갱신
                 start = mid + 1;
                 answer = Math.max(answer, mid);
             } else {
+                // 5. 콘이 mid 시점에 도착해서 셔틀을 탈 수 없으면 더 빨리 도착해야 함 -> 왼쪽 탐색
                 end = mid - 1;
             }
         }
-//        System.out.println("answer = " + answer);
+        // 6. 분 -> 시각으로 변환해 리턴
         return String.format("%02d:%02d", answer / 60, answer % 60);
     }
 
-    // 콘이 셔틀타고 갈 수 있는지
+    /**
+     * @param queue 대기열
+     * @param con 콘의 도착시각
+     * @return 콘이 con 시점에 도착했을 때 셔틀을 탈 수 있는지 여부
+     */
     public static boolean canTake(Queue<Integer> queue, int con, int n, int t, int m) {
-//        System.out.println("con = " + con);
-        int shuttle = 540;  // 셔틀 시각
+        int shuttle = 540;  // 셔틀 시각(첫차는 무조건 09:00)
+        // 1. 기존 대기열에 콘 도착시각을 포함한 우선순위 큐 생성
         Queue<Integer> q = new PriorityQueue<>(queue);
         q.add(con);
-//        System.out.println(q);
-
-        // 셔틀 태우기
         for(int i = 0; i < n; i++) {
-            // 해당 시각에 탈 수 있는 사람 태우기
-            int person = 0;
+            // 2. 해당 셔틀 도착 시점에 대기열에서 사람 태우기
+            int person = 0; // 셔틀에 탑승한 사람수
+            // 3. 대기열이 비었으면 모든 사람이 더 탔으므로 break
             if(q.isEmpty()) break;
+            // 4. 셔틀에 m 명 보다 적게 타있고 해당 사람을 현재 시점에 태울 수 있으면, 셔틀에 태우기
             while(!q.isEmpty() && shuttle >= q.peek() && person < m) {
                 q.poll();
                 person++;
             }
             shuttle += t;
         }
-        //
-//        System.out.println(q);
-
-//        System.out.println("------------------");
-        return (q.isEmpty() || q.peek() != con) ? true : false;
+        // 5. 큐가 비었거나 콘의 도착시각이 대기열에 없으면 콘이 탑승했다는 의미
+        return (q.isEmpty() || !q.contains(con)) ? true : false;
     }
 }
