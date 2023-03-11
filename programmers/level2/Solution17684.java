dd08c82c44bd5f83d14be551bc92d649402bc161
ahah525 <ahah525@naver.com>
2023-03-11 21:19:18 +0900

    17684-[3차] 압축(2_v1.성공)

--- programmers/level2/Solution17684.java
+++ programmers/level2/Solution17684.java
@@ -1,46 +1,34 @@
 package programmers.level2;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * [한줄평] map 을 사용해서 그대로 구현하기만 하면 쉽게 풀 수 있는 문제였다.
- * - 사전에 등록된 단어 중 가장 긴 문자열을 계속해서 찾아야하기 때문에, 현재 입력(msg)에서 substring() 으로 길이를 하나씩 줄여가며 사전에 해당 단어가 있는지 검사해야한다!!
- * 가정) 현재입력: ABCD, 사전: A,B,C,..., ABC
- * ABCD, ABC, AB, A 순서로 각 단어가 사전에 있는지 검사해야한다!
- *
+ * [문제명] [3차] 압축
+ * [풀이시간] / 40분
+ * [한줄평] map 을 사용해서 그대로 구현하기만 하면 쉽게 풀 수 있는 문제였다. / 가장 긴 문자열을 찾는데 시간을 줄이기 위해 고민하느라 오래 걸렸던 구현 문제였다.
  * v1. map(사전 관리용) 1개, list(정답 리턴용) 1개 사용 (성공)
- *
- * >> LZW 압축방법
- * 1. 길이가 1인 모든 단어를 포함하도록 사전을 초기화한다.
- * 2. 사전에서 현재 입력과 일치하는 가장 긴 문자열 w를 찾는다.
- * 3. w에 해당하는 사전의 색인 번호를 출력하고, 입력에서 w를 제거한다.
- * 4. 입력에서 처리되지 않은 다음 글자가 남아있다면(c), w+c에 해당하는 단어를 사전에 등록한다.
- * 5. 단계 2로 돌아간다.
+ * [접근법] 가장 긴 문자열을 찾기 위해 substring() 을 사용
+ * - 현재 입력(msg)에서 substring() 으로 길이를 하나씩 줄여가며 사전에 해당 단어가 있는지 검사한다.
+ * - 가정) 현재입력: ABCD, 사전: A,B,C,..., ABC => ABCD, ABC, AB, A 순서로 각 단어가 사전에 있는지 검사한다.
+ * 2_v1. HashMap(사전), LinkedList(문자열) (성공) -> 추천(빠름)
+ * [접근법] 가장 긴 문자열을 찾기 위해 LinkedList 를 사용
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17684">문제</a>
  */
 class Solution17684 {
     public static void main(String[] args) {
         // [11, 1, 27, 15]
-        String msg1 = "KAKAO";
-        List<Integer> answer1 = solution(msg1);
-        System.out.println(answer1);
+        System.out.println(solution2("KAKAO"));
 
         // [20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34]
-        String msg2 = "TOBEORNOTTOBEORTOBEORNOT";
-        List<Integer> answer2 = solution(msg2);
-        System.out.println(answer2);
+        System.out.println(solution2("TOBEORNOTTOBEORTOBEORNOT"));
 
         // [1, 2, 27, 29, 28, 31, 30]
-        String msg3 = "ABABABABABABABAB";
-        List<Integer> answer3 = solution(msg3);
-        System.out.println(answer3);
+        System.out.println(solution2("ABABABABABABABAB"));
     }
 
+    // 1_v1
     /**
-     *
      * @param msg 영문 대문자로만 이뤄진 문자열 msg(1 글자 이상, 1000 글자 이하)
      * @return 주어진 문자열을 압축한 후의 사전 색인 번호를 배열로 출력
      */
@@ -70,7 +58,40 @@ class Solution17684 {
                 }
             }
         }
-//        System.out.println(map);
+        return answer;
+    }
+
+    // 2_v1
+    public static List<Integer> solution2(String msg) {
+        List<Integer> answer = new ArrayList<>();   //
+        Queue<String> q = new LinkedList<>();   // 메시지의 각 문자가 들어있는 큐
+        Map<String, Integer> map = new HashMap<>(); // 사전(단어, 인덱스)
+        int i;  // 사전 인덱스
+        // 1. 사전 초기화
+        for(i = 1; i <= 26; i++) {
+            map.put(String.valueOf((char) ('A' + i - 1)), i);
+        }
+        for(char c : msg.toCharArray()) {
+            q.add(Character.toString(c));
+        }
+        // 2. 메시지 큐가 빌때까지 반복
+        while(!q.isEmpty()) {
+            // 3. w, c 찾기
+            StringBuilder w = new StringBuilder();  // 현재 입력과 일치하는 가장 긴 문자열
+            String c = "";  // 다음 글자
+            while(!q.isEmpty()) {
+                if(!map.containsKey(w + q.peek())) break;
+                // 큐에서 꺼낸 문자를 w 뒤에 붙임
+                w.append(q.poll());
+                // 다음 글자가 있을 때만 갱신
+                if(!q.isEmpty()) c = q.peek();
+            }
+            // 4. w 의 인덱스 리스트에 추가
+            answer.add(map.get(w.toString()));
+            // 5. 다음 글자가 있을 때만 사전에 등록
+            if(!c.equals(""))
+                map.put(w + c, i++);
+        }
         return answer;
     }
 }
