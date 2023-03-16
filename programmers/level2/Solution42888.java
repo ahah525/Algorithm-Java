694febe291d9bf44fd470110e28e1d5706703729
ahah525 <ahah525@naver.com>
2023-03-16 11:36:56 +0900

    42888-오픈채팅방(2_v2.성공)

--- programmers/level2/Solution42888.java
+++ programmers/level2/Solution42888.java
@@ -9,22 +9,17 @@ import java.util.Map;
 /**
  * [문제명] 오픈채팅방
  * [풀이시간] / 19분
- * [한줄평] 주어진 문제대로 풀기만하면 되는 쉬운 구현 문제였다. 다양한 풀이 방법이 있을 것 같다. / 쉬운 구현문제였다.
- * 1_v1. HashMap, ArrayList(성공) -> 더 빠름
- * 1) (아이디, 닉네임) 쌍 저장하기
- * 2) 바뀐 닉네임을 적용해서 결과 문자열 리스트 만들기
- * 1_v2. (구상만)
- * 1) (아이디, 닉네임) 쌍 저장하기, 결과 문자열 리스트 만들기
- * 2) 바뀐 닉네임 적용
+ * [한줄평] 주어진 문제대로 풀기만하면 되는 쉬운 구현 문제였다. 다양한 풀이 방법이 있을 것 같다. / 쉬운 구현문제였고 효율을 높이기 위한 방법만 조금 고민하면 된다.
+ * 1_v1. HashMap, ArrayList(성공)
  * 2_v1. HashMap, ArrayList 2, Info 클래스(성공)
+ * 2_v2. HashMap, ArrayList 2, Info 클래스(성공) -> 추천
+ * [개선] Info 클래스의 type 변수의 타입을 String -> char 로 바꿈(비교할 때 equals() 로 안해도 됨!)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42888">문제</a>
  */
 class Solution42888 {
     public static void main(String[] args) {
         // ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
-        String[] record1 = {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"};
-        List<String> answer1 = solution2(record1);
-        System.out.println(answer1);
+        System.out.println(solution2(new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"}));
     }
 
     // 1_v1
@@ -74,43 +69,45 @@ class Solution42888 {
         return answer;
     }
 
-    // 2_v1
+    // 2_v2
     static class Info {
-        String type;
+        char type;  // 시간 단축을 위해 char 로 관리
         String id;
 
-        Info(String type, String id) {
+        Info(char type, String id) {
             this.type = type;
             this.id = id;
         }
     }
     public static List<String> solution2(String[] record) {
-        // (id, nickname)
+        // 1. map, list 기록
         Map<String, String> map = new HashMap<>();
         List<Info> list = new ArrayList<>();
-        List<String> answer = new ArrayList<>();
         for(String r : record) {
             String[] info = r.split(" ");
             String type = info[0];
             String id = info[1];
             String nickname;
+            // Enter, Change -> map 에 nickname 기록
+            // Enter, Leave -> 들어오고 나간 정보 기록
             switch (type) {
                 case "Enter":
                     nickname = info[2];
                     map.put(id, nickname);
-                    list.add(new Info(type, id));
+                    list.add(new Info('E', id));
                     break;
                 case "Leave":
-                    list.add(new Info(type, id));
+                    list.add(new Info('L', id));
                     break;
                 default:
                     nickname = info[2];
                     map.put(id, nickname);
             }
         }
-        //
+        // 2. 문자열 만들어서 리스트에 넣고 리턴
+        List<String> answer = new ArrayList<>();
         for(Info info : list) {
-            if(info.type.equals("Enter"))
+            if(info.type == 'E')
                 answer.add(map.get(info.id) + "님이 들어왔습니다.");
             else
                 answer.add(map.get(info.id) + "님이 나갔습니다.");
