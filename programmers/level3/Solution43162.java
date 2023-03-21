651229dd840b369314a8a85cbd2c4842bc42aebb
ahah525 <ahah525@naver.com>
2023-03-21 12:00:45 +0900

    43162-네트워크(2_v1.성공)

--- programmers/level3/Solution43162.java
+++ programmers/level3/Solution43162.java
@@ -6,27 +6,24 @@ import java.util.Queue;
 
 /**
  * [문제명] 네트워크
- * [한줄평] 전형적인 DFS/BFS 로 푸는 쉬운 문제였고 더 익숙한 BFS 로 풀었다.
- * v1. BFS(성공)
- * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43162/solution_groups?language=java">다른 풀이</a>
+ * [풀이시간] / 10분
+ * [한줄평] 전형적인 DFS,BFS 로 푸는 쉬운 문제였고 더 익숙한 BFS 로 풀었다. / BFS 로 빨리 풀었던 기본 문제였다.
+ * 1_v1. BFS(성공)
+ * 2_v1. BFS(성공)
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43162">문제</a>
  */
 class Solution43162 {
-    static boolean[] visited;   // 방문 여부
 
     public static void main(String[] args) {
         // 2
-        int n1 = 3;
-        int[][] computers1 = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
-        System.out.println(solution(n1, computers1));
+        System.out.println(solution(3, new int[][]{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}}));
 
         // 1
-        int n2 = 3;
-        int[][] computers2 = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-        System.out.println(solution(n2, computers2));
+        System.out.println(solution(3, new int[][]{{1, 1, 0}, {1, 1, 1}, {0, 1, 1}}));
     }
 
+    // 1_v1, 2_v1
     /**
-     *
      * @param n 컴퓨터의 개수(1 이상 200 이하인 자연수)
      * @param computers 연결에 대한 정보가 담긴 2차원 배열
      * - 각 컴퓨터는 0부터 n-1인 정수로 표현
@@ -35,18 +32,18 @@ class Solution43162 {
      * @return 네트워크의 개수
      */
     public static int solution(int n, int[][] computers) {
-        visited = new boolean[n];
+        boolean[] visited = new boolean[n]; // 방문여부
         int answer = 0; // 네트워크 개수
         for(int i = 0; i < n; i++) {
             if(!visited[i]) {
-                bfs(i, n, computers);
+                bfs(i, n, computers, visited);
                 answer++;
             }
         }
         return answer;
     }
 
-    public static void bfs(int start, int n, int[][] computers) {
+    public static void bfs(int start, int n, int[][] map, boolean[] visited) {
         Queue<Integer> q = new LinkedList<>();
         // 시작노드 방문 처리
         q.add(start);
@@ -55,11 +52,11 @@ class Solution43162 {
         while(!q.isEmpty()) {
             int v = q.poll();  // 현재 노드
             // 인접 노드 검사
-            for(int nv = 0; nv < n; nv++) {
+            for(int i = 0; i < n; i++) {
                 // 방문한적이 없고 연결되어 있으면 방문처리
-                if(!visited[nv] && computers[v][nv] == 1) {
-                    q.add(nv);
-                    visited[nv] = true;
+                if(!visited[i] && map[v][i] == 1) {
+                    q.add(i);
+                    visited[i] = true;
                 }
             }
         }
