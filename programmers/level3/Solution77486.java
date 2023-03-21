f8199c23e167629c60623a04df7faf9d3f403d7a
ahah525 <ahah525@naver.com>
2023-03-21 15:01:35 +0900

    77486-다단계 칫솔 판매(1_v2.성공)

--- programmers/level3/Solution77486.java
+++ programmers/level3/Solution77486.java
@@ -7,10 +7,11 @@ import java.util.Map;
 
 /**
  * [문제명] 다단계 칫솔 판매
- * [풀이시간] (47분)
- * [한줄평]
+ * [풀이시간] 52분(47분 + 5분)
+ * [한줄평] Map 을 이용해서 푸는 구현 문제로 어렵지는 않았으나 반례를 찾지 못해 힌트를 보고 해결했다.
  * 1_v1. HashMap 2(실패-11~13 시간초과)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/77486">문제</a>
+ * @See <a href="https://school.programmers.co.kr/questions/29442">반례</a>
  */
 class Solution77486 {
     public static void main(String[] args) {
@@ -25,7 +26,7 @@ class Solution77486 {
         );
     }
 
-    // 1_v1
+    // 1_v2
     /**
      * @param enroll 각 판매원의 이름을 담은 배열
      * @param referral 각 판매원을 다단계 조직에 참여시킨 다른 판매원의 이름을 담은 배열
@@ -35,34 +36,33 @@ class Solution77486 {
      */
     public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
         int[] answer = new int[enroll.length];
-        // 1. 본인
-        Map<String, String> recommenders = new HashMap();
-        Map<String, Integer> prices = new HashMap<>();
+        // 1. 본인의 추천인, 이익금 map 초기화
+        Map<String, String> recommenders = new HashMap();   // (본인명, 추천인명)
+        Map<String, Integer> prices = new HashMap<>();  // (본인명, 이익금)
         for(int i = 0; i < enroll.length; i++) {
             recommenders.put(enroll[i], referral[i]);
             prices.put(enroll[i], 0);
         }
-
-        // 2. seller[i] 이익금 배분
+        // 2. 각 판매원의 이익금 배분
         for(int i = 0; i < seller.length; i++) {
-            int price = amount[i] * 100;
-            String me = seller[i];
-            while(recommenders.containsKey(me)) {
-                // 3. 추천인 배분 금액 계산(추천인이 있고 최소 1원을 받을 수 있을 때만)
-                int rp = 0; // 추천인 배분 금액
-                if(price >= 10) rp = (int) (price * 0.1);
-                // 4. 본인 배분 금액 계산 및 반영
+            String me = seller[i];  // 본인명
+            int price = amount[i] * 100;    // 본인이 획득한 이익금
+            while(true) {
+                // 3. 본인이 center(민호)이거나 이익금이 0원이면 종료
+                if(me.equals("-") || price == 0) break;
+                // 4. 추천인에게 배분할 이익금 금액 계산
+                int rp = (int) (price * 0.1);
+                // 5. 본인에게 배분할 이익금 계산 및 반영
                 int mp = price - rp;
                 prices.put(me, prices.get(me) + mp);
-                // 5.
+                // 6. 추천인을 본인으로 대입
                 me = recommenders.get(me);
                 price = rp;
             }
-//            System.out.println(prices);
         }
-        for(int j = 0; j < enroll.length; j++) {
+        // 7. 각 판매원의 이익금을 1차원 배열로 리턴
+        for(int j = 0; j < enroll.length; j++)
             answer[j] = prices.get(enroll[j]);
-        }
         return answer;
     }
 }
