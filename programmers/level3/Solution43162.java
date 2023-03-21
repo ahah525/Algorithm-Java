adf9a29ba67e3506871ba96f18cb44f9b690e6a0
ahah525 <ahah525@naver.com>
2023-03-21 12:11:39 +0900

    43162-네트워크(2_v2.성공)

--- programmers/level3/Solution43162.java
+++ programmers/level3/Solution43162.java
@@ -10,6 +10,7 @@ import java.util.Queue;
  * [한줄평] 전형적인 DFS,BFS 로 푸는 쉬운 문제였고 더 익숙한 BFS 로 풀었다. / BFS 로 빨리 풀었던 기본 문제였다.
  * 1_v1. BFS(성공)
  * 2_v1. BFS(성공)
+ * 2_v2. DFS(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43162">문제</a>
  */
 class Solution43162 {
@@ -54,10 +55,34 @@ class Solution43162 {
             // 인접 노드 검사
             for(int i = 0; i < n; i++) {
                 // 방문한적이 없고 연결되어 있으면 방문처리
-                if(!visited[i] && map[v][i] == 1) {
+                if(map[v][i] == 1 && !visited[i]) {
                     q.add(i);
                     visited[i] = true;
                 }
+            }
+        }
+    }
+
+    // 2_v2
+    public static int solution2(int n, int[][] computers) {
+        boolean[] visited = new boolean[n]; // 방문여부
+        int answer = 0; // 네트워크 개수
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                dfs(i, n, computers, visited);
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void dfs(int v, int n, int[][] map, boolean[] visited) {
+        // 현재 노드 방문 처리
+        visited[v] = true;
+        // 인접 노드 검사
+        for(int i = 0; i < n; i++) {
+            if(map[v][i] == 1 && !visited[i]) {
+                dfs(i, n, map, visited);
             }
         }
     }
