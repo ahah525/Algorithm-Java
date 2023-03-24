c1ff5988c87ffd69991f8a9f6e6a07c37403ee7c
ahah525 <ahah525@naver.com>
2023-03-24 14:25:49 +0900

    86971-전력망을 둘로 나누기(2_v1.성공)

--- programmers/level2/Solution86971.java
+++ programmers/level2/Solution86971.java
@@ -8,11 +8,12 @@ import java.util.Queue;
 
 /**
  * [문제명] 전력망을 둘로 나누기
- * [풀이시간] 30분
+ * [풀이시간] 30분 / 13분
  * [한줄평] n 의 크기를 보니 완전탐색으로 풀 수 있겠다는 생각이 들어서 BFS 를 반복문을 돌려 풀었다. BFS 로 풀었지만 DFS 로 풀어도 되는 문제다.
- * 이 문제에서는 간선정보를 계속 수정해줘야하기 때문에 인접행렬 방식으로 푸는 것도 나쁘지 않은 것 같다.
+ * / 간선정보를 계속 수정해줘야하기 때문에 인접행렬 방식이 더 나을 것 같아 인접행렬, BFS 로 풀었다.
  * 1_v1. 인접리스트, BFS(성공)
  * 1_v2. 인접행렬, BFS(성공)
+ * 2_v1. 인접행렬, BFS(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/86971">문제</a>
  */
 class Solution86971 {
@@ -22,6 +23,7 @@ class Solution86971 {
         System.out.println(solution2(9, wires1));
     }
 
+    // 1_v1
     /**
      * @param n 송전탑의 개수
      * @param wires 전선 정보
@@ -72,33 +74,34 @@ class Solution86971 {
         return cnt;
     }
 
+    // 1_v2, 2_v1
     public static int solution2(int n, int[][] wires) {
         int min = 100;
-        // 1. 인접행렬
-        boolean[][] graph = new boolean[n + 1][n + 1];
+        // 1. 인접행렬 초기화
+        boolean[][] map = new boolean[n + 1][n + 1];
         for(int[] wire : wires) {
-            graph[wire[0]][wire[1]] = true;
-            graph[wire[1]][wire[0]] = true;
+            map[wire[0]][wire[1]] = true;
+            map[wire[1]][wire[0]] = true;
         }
         // 2. 각 전선을 끊었을 때 경우의 수 모두 비교
         for(int[] wire: wires) {
-            // 해당 전선을 끊었을 때 각 트리의 탑 개수의 차 구하기
-            // 간선 끊기
-            graph[wire[0]][wire[1]] = false;
-            graph[wire[1]][wire[0]] = false;
-            int v1 = bfs2(wire[0], n, graph);
-            int v2 = bfs2(wire[1], n, graph);
-            // 간선 원상복귀
-            graph[wire[0]][wire[1]] = true;
-            graph[wire[1]][wire[0]] = true;
-            min = Math.min(min, Math.abs(v1 - v2));
+            // 3. 간선 끊기
+            map[wire[0]][wire[1]] = false;
+            map[wire[1]][wire[0]] = false;
+            // 4. 해당 전선을 끊었을 때 각 트리의 탑 개수의 차 구하기
+            boolean[] visited = new boolean[n + 1]; // 탑 방문 여부
+            min = Math.min(min, Math.abs(bfs2(wire[0], n, map, visited) - bfs2(wire[1], n, map, visited)));
+            // 5. 간선 다시 연결(원상복구)
+            map[wire[0]][wire[1]] = true;
+            map[wire[1]][wire[0]] = true;
         }
         return min;
     }
 
-    public static int bfs2(int start, int n, boolean[][] graph) {
+    // start 노드에서 갈 수 있는 모든 노드의 개수 구하기
+    public static int bfs2(int start, int n, boolean[][] map, boolean[] visited) {
         int cnt = 0;    // 탑개수
-        boolean[] visited = new boolean[n + 1]; // 탑 방문 여부
+        // 시작노드 방문 처리
         Queue<Integer> q = new LinkedList<>();
         q.add(start);
         visited[start] = true;
@@ -107,7 +110,7 @@ class Solution86971 {
             cnt++;
             // 인접노드
             for(int i = 1; i < n + 1; i++) {
-                if(graph[v][i] && !visited[i]) {
+                if(map[v][i] && !visited[i]) {
                     q.add(i);
                     visited[i] = true;
                 }
