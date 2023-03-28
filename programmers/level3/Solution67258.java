ae2acbed25675829a10d7f584ffccc977081f729
ahah525 <ahah525@naver.com>
2023-03-28 14:20:51 +0900

    67258-[카카오 인턴] 보석 쇼핑(2_v1.성공)

--- programmers/level3/Solution67258.java
+++ programmers/level3/Solution67258.java
@@ -1,85 +1,37 @@
 package programmers.level3;
 
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * [문제명] [카카오 인턴] 보석 쇼핑
- * [풀이시간] 1시간 / 18분
- * [한줄평] 구현을 하면서도 시간초과가 날 것 같다고 생각했는데, 결국 시간초과가 났다. 결국 힌트를 얻어 투포인터로 해결했는데 투포인터는 무조건 복습이 필요하다!!
- * 투포인터에서 잊지밀아야할 점은 end = n 이 되기전까지 반복한다는것(종료조건)이고, start++ 와 end++ 시점이 중요하다.
+ * [풀이시간] 1시간 / 1시간 8분(18분 + 50분)
+ * [한줄평] 구현을 하면서도 시간초과가 날 것 같다고 생각했는데, 결국 시간초과가 났다. 결국 힌트를 얻어 투포인터로 해결했는데 투포인터는 무조건 복습이 필요하다!! / 결국 스스로 풀기는 했지만 투포인터 문제라는 것을 알고 풀어도 조건을 처리하기가 어려웠다.
  * 1_v1. Set, Map, 2중 for 문 - 완전탐색(실패 - 효율성 테스트 1~5, 7~15 실패)
  * - 시작 인덱스, 끝 인덱스를 바꿔가며 완전 탐색 수행
  * 1_v2. Set, Map, 투포인터(성공)
+ * [해결법] 투포인터에서 잊지밀아야할 점은 end = n 이 되기전까지 반복한다는것(종료조건)이고, start++ 와 end++ 시점이 중요하다.
  * 2_v1. HashSet, HashMap, 투포인터(실패 - 정확성 2,3,7~9,12,13,15, 효율성 1~3,5~11,13~14)
+ * 2_v2. HashSet, HashMap, 투포인터(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/15008/lessons/67258">문제</a>
  * @See <a href="https://velog.io/@fantastik/48">풀이 참고</a>
  */
 class Solution67258 {
     public static void main(String[] args) {
         // [3, 7]
-        String[] gems1 = {"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"};
-        System.out.println(solution2(gems1));
+        System.out.println(Arrays.toString(solution3(new String[]{"DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"})));
 
         // [1, 3]
-        String[] gems2 = {"AA", "AB", "AC", "AA", "AC"};
-        int[] course2 = {2,3,5};
-        System.out.println(solution2(gems2));
+        System.out.println(Arrays.toString(solution3(new String[]{"AA", "AB", "AC", "AA", "AC"})));
 
         // [1, 1]
-        String[] gems3 = {"XYZ", "XYZ", "XYZ"};
-        System.out.println(solution2(gems3));
+        System.out.println(Arrays.toString(solution3(new String[]{"XYZ", "XYZ", "XYZ"})));
 
         // [1, 5]
-        String[] gems4 = {"ZZZ", "YYY", "NNNN", "YYY", "BBB"};
-        System.out.println(solution2(gems4));
+        System.out.println(Arrays.toString(solution3(new String[]{"ZZZ", "YYY", "NNNN", "YYY", "BBB"})));
     }
 
-    public static int[] solution1(String[] gems) {
-        int[] answer = {1, 1};
-        int n = gems.length;   // 보석 개수
-        int minLen = n;     // 최소 구간 길이
-        int minIdx = -1;    // 최소 구간 시작 인덱스
-        // 1. 보석 종류 구하기
-        Set set = new HashSet();
-        for(String s : gems) {
-            set.add(s);
-        }
-        int kind = set.size(); // 보석 종류수
-        if(kind == 1) return answer;
-        // 2. 시작점
-        for(int i = 0; i < n; i++) {
-            // 현재 범위 = start ~ (end - 1)
-            String start = gems[i]; // 시작값
-            Map<String, Integer> map = new HashMap<>(); // (보석명, 개수)
-            for(int j = i; j < n; j++) {
-                String name = gems[j];  // 보석명
-                map.put(name, map.getOrDefault(name, 0) + 1);
-                // 모든 보석이 속해있고 현재 최소 구간보다 더 짧으면 업데이트
-                if(map.size() == kind) {
-                    if(minLen > j - i) {
-                        minLen = j - i;
-                        minIdx = i;
-                    }
-                    break;
-                }
-            }
-            // 맨 앞 인덱스 개수 빼기
-            int cnt = map.get(start);
-            if(cnt == 1) {
-                map.remove(start);
-            } else {
-                map.put(start, cnt - 1);
-            }
-        }
-        answer[0] = minIdx + 1;
-        answer[1] = answer[0] + minLen;
-        return answer;
-    }
-
+    // 1_v2
     /**
      * @param gems 진열대 번호 순서대로 보석들의 이름이 저장된 배열
      * @return 모든 보석을 하나 이상 포함하는 가장 짧은 구간의 시작 진열대 번호와 끝 진열대 번호를 담은 배열
@@ -130,29 +82,49 @@ class Solution67258 {
         return answer;
     }
 
-    public int[] solution(String[] gems) {
+    // 2_v2
+    public static int[] solution3(String[] gems) {
+        // 1. 보석 종류수 구하기
+        Set<String> set = new HashSet<>();
+        for(String gem : gems)
+            set.add(gem);
+        int n = set.size();
         // (보석, 개수)
         Map<String, Integer> map = new HashMap<>();
-        Set<String> set = new HashSet<>();
-        int s = 0;
-        int e = 0;
-        for(String gem : gems) {
-            set.add(gem);
-        }
-        int n = set.size(); // 보석 개수
-        // n 개를 모두 모을때까지 e 를 오른쪽으로
+        int min = Integer.MAX_VALUE;    // 가장 짧은 구간의 길이
+        int s = 0;  // 시작번호
+        int e = 0;  // 끝번호
+        int[] answer = new int[2];  // (시작 진열대 번호, 끝 진열대 번호)
+        //
         while(true) {
-            map.put(gems[e], map.getOrDefault(gems[e], 0) + 1);
-            if(map.size() == n) break;
-            e++;
+            if(map.size() < n) {
+                // 1. 현재 구간의 보석 종류가 n개 미만이면
+                // 1.1. 끝 번호가 진열대 번호를 넘어갔으면 break
+                if(e == gems.length) break;
+                // 1.2. 끝 번호가 아직 진열대 번호내에 있으면 e 를 오른쪽으로 1칸 이동
+                map.put(gems[e], map.getOrDefault(gems[e], 0) + 1);
+                e++;
+            } else {
+                // 2. 현재 구간의 보석 종류가 n개 이상이면
+                // 2.1. 가장 짧은 구간의 길이 > 현재 구간의 길이 이면, 가장 짧은 구간 갱신
+                if(min > e - s) {
+                    answer[0] = s + 1;
+                    answer[1] = e;
+                    min = e - s;
+                }
+                // 3. s 를 오른쪽으로 1칸 이동
+                if(map.get(gems[s]) == 1) {
+                    // 3.1. s 가 구간에 1개 있으면 삭제
+                    map.remove(gems[s]);
+                } else {
+                    // 3.2. s 가 구간에 2개 이상이면 개수 하나 줄이기
+                    map.put(gems[s], map.get(gems[s]) - 1);
+                }
+                s++;
+            }
+//            System.out.println("(" + s + "," + e + ")");
+//            System.out.println(map);
         }
-        // 최소 길이로 줄이기위해서 s 를 오른쪽으로
-        while(true) {
-            if(map.get(gems[s]) == 1) break;
-            map.put(gems[s], map.get(gems[s]) - 1);
-            s++;
-        }
-
-        return new int[]{s + 1, e + 1};
+        return answer;
     }
 }
