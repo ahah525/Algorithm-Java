2d840a56cc9e9f2bdc02354eac35c5cf1d9157e4
ahah525 <ahah525@naver.com>
2023-04-11 15:36:12 +0900

    67257-수식 최대화(2_v1.성공)

--- programmers/level2/Solution67257.java
+++ programmers/level2/Solution67257.java
@@ -6,29 +6,28 @@ import java.util.List;
 
 /**
  * [문제명] [카카오 인턴] 수식 최대화
- * [풀이시간] 1시간 45분(1시간 15분 + 30분)
+ * [풀이시간] 1시간 45분(1시간 15분 + 30분) / 1시간 5분
  * [한줄평] 순열로 각 경우의 수에 따라 연산 결과를 모두 구하고 최댓값을 구해야겠다고 생각은 했는데 구현하는게 생각보다 조금 복잡했던 것 같다.
  * 로직은 맞는 것 같은데 계속 실패가 떠서 결국 반례를 봤는데 조건을 생각안하고 자료형을 잘못사용해서 통과하지 못했다는게 정말 허무했다. 다시 한번 조건을 잘 읽자..
- * v1. 순열로 완전탐색 + 구현(실패 - 정확성 테스트: 11~15, 24, 27~29 실패)
- * v2. 순열로 완전탐색 + 구현(성공)
+ * 1_v1. 순열로 완전탐색 + 구현(실패 - 정확성 테스트: 11~15, 24, 27~29 실패)
+ * 1_v2. 순열로 완전탐색 + 구현(성공)
  * - "expression의 중간 계산값과 최종 결괏값은 절댓값이 2^63 - 1 이하" 조건 -> int 대신 long 으로 변경
+ * 2_v1. 완전탐색(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/62757">문제</a>
  */
-class Solution62757 {
+class Solution67257 {
     public static void main(String[] args) {
         // 60420
-        String expression1 = "100-200*300-500+20";
-        System.out.println(solution(expression1));
+        System.out.println(solution2("100-200*300-500+20"));
 
         // 300
-        String expression2 = "50*6-3*2";
-        System.out.println(solution(expression2));
+        System.out.println(solution2("50*6-3*2"));
 
         // 52
-        String expression3 = "50+2";
-        System.out.println(solution(expression3));
+        System.out.println(solution2("50+2"));
     }
 
+    // 1_v1
     static List<Long> nums;  // 피연산자 리스트
     static String cmds;         // 연산자 리스트
     static long max;             // 연산 결과값(절댓값)의 최댓값
@@ -136,5 +135,68 @@ class Solution62757 {
             }
         }
         return 0;
+    }
+
+    // 2_v1
+    public static long solution2(String expression) {
+        long answer = 0;
+        // 1. 연산자, 피연산자 분리
+        List<Long> nums = new ArrayList<>();
+        List<Character> cmds = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        for(char c : expression.toCharArray()) {
+            if('0' <= c && c <= '9') {
+                sb.append(c);
+            } else {
+                nums.add(Long.parseLong(sb.toString()));
+                sb.setLength(0);
+                cmds.add(c);
+            }
+        }
+        nums.add(Long.parseLong(sb.toString()));    // 마지막 숫자 처리
+        // 2. 연산자 우선순위 경우의 수 = 6개
+        String[] paths = {"+-*", "+*-", "-+*", "-*+", "*+-", "*-+"};
+        // 3. 연산자 우선순위를 적용한 계산 결괏값을 비교해 최댓값 갱신
+        for(String path : paths) {
+            answer = Math.max(answer, calc(path, nums, cmds));
+        }
+        return answer;
+    }
+
+    // 연산자 우선순위를 적용한 계산 결과의 절댓값 리턴
+    public static long calc(String path, List<Long> num, List<Character> cmd) {
+        // 1. 리스트 값 복사(원본을 유지하기 위함)
+        List<Long> nums = new ArrayList<>();
+        List<Character> cmds = new ArrayList<>();
+        for(long n : num) {
+            nums.add(n);
+        }
+        for(char c : cmd) {
+            cmds.add(c);
+        }
+        // 2. 각 연산자 우선순위대로 계산
+        for(char c : path.toCharArray()) {
+            int i = 0;
+            while(i < cmds.size()) {
+                if(cmds.get(i) != c) {
+                    i++;
+                    continue;
+                }
+                long res;
+                if(c == '+') {
+                    res = nums.get(i) + nums.get(i + 1);
+                } else if(c == '-') {
+                    res = nums.get(i) - nums.get(i + 1);
+                } else {
+                    res = nums.get(i) * nums.get(i + 1);
+                }
+                nums.remove(i);
+                nums.remove(i);
+                nums.add(i, res);
+                cmds.remove(i);
+            }
+        }
+        // 3. 절댓값 리턴
+        return Math.abs(nums.get(0));
     }
 }
