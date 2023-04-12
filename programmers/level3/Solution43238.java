51dc168391ebfe31d3e80497b5d9018e83f1ef35
ahah525 <ahah525@naver.com>
2023-04-12 17:35:47 +0900

    43238-입국심사(2_v1.성공)

--- programmers/level3/Solution43238.java
+++ programmers/level3/Solution43238.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 
 /**
  * [문제명] 입국심사
- * [풀이시간] 1시간(30분 + 30분)
+ * [풀이시간] 1시간(30분 + 30분) / 50분
  * [한줄평] 이분탐색 문제라는 것을 알아도 어떻게 풀어야할지 감이 안잡혀서 결국 풀이를 보고 이해했던 문제다.
+ * / 처음 풀 때랑 똑같이 처음에는 PriorityQueue 를 이용해서 풀려고 했다. 이분탐색 문제인 걸 알고도 결국 풀지 못해 답을 보고 풀었다.
  * 1_v1. PriorityQueue(실패 - 6~9 시간초과)
- * 2_v2. BinarySearch(성공)
+ * 1_v2. BinarySearch(성공)
+ * - end 값 설정을 위해 times 를 정렬함
+ * 2_v1. BinarySearch(성공) -> 더 빠름
+ * - end 값 설정을 위해 굳이 times 를 정렬하지 않고, 입력범위의 최댓값을 계산함
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43238">문제</a>
  * @See <a href="https://school.programmers.co.kr/questions/7919">풀이 힌트</a>
  * @See <a href="https://swycha.tistory.com/122">풀이 참고</a>
@@ -19,6 +23,7 @@ class Solution43238 {
         System.out.println();
     }
 
+    // 1_v2
     /**
      * @param n 입국심사를 기다리는 사람 수
      * @param times 각 심사관이 한 명을 심사하는데 걸리는 시간이 담긴 배열
@@ -28,7 +33,6 @@ class Solution43238 {
         long answer = 0;
         // 1. 심사 소요 시간 오름차순 정렬
         Arrays.sort(times);
-        int m = times.length;   // 심사위원 수
         // 2. 모든 사람이 심사를 받는데 걸리는 시간의 최솟값, 최댓값 초기화
         long start = 1;
         long end = n * (long) times[times.length - 1]; // 최댓값 = 심사 소요 시간이 가장 긴 심사위원이 n명을 심사하는 경우
@@ -48,6 +52,28 @@ class Solution43238 {
                 // 5. mid 시간에서 n명 이상을 심사할 수 있으면, 왼쪽 범위 탐색
                 end = mid - 1;
                 answer = mid;   // mid 값 기록해두기
+            }
+        }
+        return answer;
+    }
+
+    // 2_v1
+    public long solution2(int n, int[] times) {
+        long answer = 0;
+        long start = 0;
+        // n = 1,000,000,000명 / 모든 times[i] = 1,000,000,000 일 경우
+        long end = (long) Math.pow(10, 18);
+        while(start <= end) {
+            long mid = (start + end) / 2;
+            long cnt = 0;
+            for(int t : times) {
+                cnt += mid / t;
+            }
+            if(cnt >= n) {
+                answer = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
         return answer;
