05208b7dac11cacb4328b87fdb1d9f44864d632b
ahah525 <ahah525@naver.com>
2023-04-13 16:52:15 +0900

    42627-디스크 컨트롤러(2_v2.실패)

--- programmers/level3/Solution42627.java
+++ programmers/level3/Solution42627.java
@@ -7,18 +7,18 @@ import java.util.Queue;
 
 /**
  * [문제명] 디스크 컨트롤러
- * [풀이시간] 1시간 30분 / (1시간)
+ * [풀이시간] 1시간 30분 / (1시간 + 50분)
  * [한줄평] 저번에 비슷한 문제를 풀어봤음에도 생각보다 구현하는게 너무 어려웠던 문제였다. 결국 답을 보고 풀이를 할 수 밖에 없없다..
- * 1_v1. (성공)
- * 2_v1. (실패 - 1~16,18 실패)
+ * 1_v1. PriorityQueue(성공)
+ * 2_v1. PriorityQueue(실패 - 1~16,18 실패)
+ * 2_v2. PriorityQueue(실패 - 3,6~15,17~19 런타임 에러)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/">문제</a>
  * @See <a href="https://codevang.tistory.com/316">풀이 참고</a>
  */
 class Solution42627 {
     public static void main(String[] args) {
         //
-        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
-        System.out.println(solution(jobs));
+        System.out.println(solution2(new int[][]{{0, 3}, {1, 9}, {2, 6}}));
     }
 
     public static int solution(int[][] jobs) {
@@ -59,8 +59,8 @@ class Solution42627 {
         return times / jobs.length;
     }
 
-    // 2_v1
-    class Job {
+    // 2_v2
+    static class Job {
         int request;
         int play;
 
@@ -69,25 +69,35 @@ class Solution42627 {
             this.play = play;
         }
     }
-    public int solution2(int[][] jobs) {
+    public static int solution2(int[][] jobs) {
         int answer = 0;
-        int end = 0;    // 이전 작업 종료 시점
-        Queue<Job> wait = new PriorityQueue<>((o1, o2) -> {
-            return (o1.request + o1.play) - (o2.request + o2.play);
+        // 1. 요청 시점 오름차순 정렬
+        Arrays.sort(jobs, (o1, o2) -> {
+            return o1[0] - o2[0];
         });
-        for(int[] job : jobs) {
-            wait.add(new Job(job[0], job[1]));
-        }
-        while(!wait.isEmpty()) {
-            Job job = wait.poll();
-            // 이전 작업이 이미 끝났으면, 현재 작업은 대기시간 없음
-            if(end <= job.request) {
-                answer += job.play;
-                end = job.request + job.play;
+        // 2. 실행 시간, 요청 시점 오름차순 정렬
+        Queue<Job> wait = new PriorityQueue<>((o1, o2) -> {
+            if(o1.play == o2.play) return o1.request - o2.request;
+            return o1.play - o2.play;
+        });
+        int i = 0;     // 예약 배열 인덱스
+        int end = 0;   // 현재 작업 종료 시점
+        int cnt = 0;   // 종료한 작업의 개수
+        // 예약 배열 -> 대기큐 -> 실행(현재 작업)
+        while(cnt < jobs.length) {
+            if(i < jobs.length && jobs[i][0] <= end) {
+                // 3. 현재 작업이 끝나기 전에 요청된 작업이 있음: 예약 배열에서 꺼낸 1개의 작업 -> 대기큐로
+                wait.add(new Job(jobs[i][0], jobs[i][1]));
+                i++;
             } else {
-                // 이전 작업이 아직 끝나지 않았으면, 현재 작업은 대기시간 추가
+                // 4. 현재 작업이 끝나기 전에 요청된 작업이 없음: 대기큐에서 꺼낸 1개의 작업 -> 실행 상태로
+                // 4-1. 대기큐에서 실행 시간이 가장 짧은 작업 1개 꺼내기
+                Job job = wait.poll();
+                // 4-2. 실행 상태가 된 작업의 요청~종료 시간 = 대기시간 + 실행시간
                 answer += (end - job.request) + job.play;
+                // 4-3. 현재 작업의 종료 시점 = 이전 작업의 종료 시점 + 현재 작업 실행 시간
                 end += job.play;
+                cnt++;
             }
         }
         return answer / jobs.length;
