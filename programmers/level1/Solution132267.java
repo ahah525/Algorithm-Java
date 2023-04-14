7694f6460382df39d868b82cf381a0d224c21c5f
ahah525 <ahah525@naver.com>
2023-04-14 11:04:15 +0900

    132267-콜라 문제(2_v1.성공)

--- programmers/level1/Solution132267.java
+++ programmers/level1/Solution132267.java
@@ -3,9 +3,11 @@ package programmers.level1;
 
 /**
  * [문제명] 콜라 문제
- * [풀이시간] 25분
+ * [풀이시간] 25분 / 5분
  * [한줄평] 진짜 쉬운 문제였는데, 입력예제만 생각하고 빈병 a개를 주면 콜라 1개를 주는 걸로 식을 잘못설정했다. 오류를 알아차리기까지 너무 오래걸렸다..
- * v1. (성공)
+ * / 수식만 세우면 되는 쉬운 문제였다.
+ * 1_v1. (성공)
+ * 2_v1. (성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/132267">문제</a>
  */
 class Solution132267 {
@@ -31,6 +33,18 @@ class Solution132267 {
             int bottle = n % a;        // 교환하고 남은 빈병개수
             answer += coke;
             n = coke + bottle;
+        }
+        return answer;
+    }
+
+    // 2_v1
+    public int solution2(int a, int b, int n) {
+        int answer = 0;// 받는 병 수
+        while(n >= a) {
+            int coke = (n / a) * b;
+            answer += coke;
+            // 교환한 결과 = 기존 빈병 개수 + 새로 받은 콜라 개수 - 교환을 위해 제출한 빈병 개수
+            n = n + coke - (n / a) * a;
         }
         return answer;
     }
