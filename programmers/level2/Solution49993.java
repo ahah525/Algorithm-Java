6641c9c7cdc65e759f92ea188be7065003a53b79
ahah525 <ahah525@naver.com>
2023-04-21 11:21:05 +0900

    49993-스킬트리(2_v1.성공)

--- programmers/level2/Solution49993.java
+++ programmers/level2/Solution49993.java
@@ -1,23 +1,23 @@
 package programmers.level2;
 
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * [문제명] 스킬트리
- * [풀이시간] 30분
- * [한줄평] 문제를 잘못 이해해서 구현하느데 생각보다 시간이 오래걸렸던 문제였다.
- * v1. (성공)
+ * [풀이시간] 30분 / 13분
+ * [한줄평] 문제를 잘못 이해해서 구현하느데 생각보다 시간이 오래걸렸던 문제였다. / 그냥 구현으로 풀었는데, 다른 풀이를 보니 정규 표현식으로 많이 푼 것 같다.
+ * 1_v1. 문자열, 구현(성공)
+ * 2_v1. 문자열, 구현(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/49993">문제</a>
  */
 class Solution49993 {
     public static void main(String[] args) {
         // 2
-        String[] skillTrees1 = {"BACDE", "CBADF", "AECB", "BDA"};
-        System.out.println(solution("CBD", skillTrees1));
+        System.out.println(solution2("CBD", new String[] {"BACDE", "CBADF", "AECB", "BDA"}));
     }
 
+    // 1_v1
     /**
      * @param skill 선행 스킬 순서
      * @param skillTrees 유저들이 만든 스킬트리1를 담은 배열
@@ -45,6 +45,39 @@ class Solution49993 {
                 if(c != skills.get(i)) return false;
                 i++;
             }
+        }
+        return true;
+    }
+
+    // 2_v1
+    static Set<Character> set;
+    static char[] arr;
+    public static int solution2(String skill, String[] skillTrees) {
+        int answer = 0;
+        set = new HashSet<>(); // 선행 스킬 집합
+        arr = skill.toCharArray(); // 선행 스킬 배열
+        for(char c : arr) {
+            set.add(c);
+        }
+        for(String skillTree : skillTrees) {
+            if(isPossible(skillTree)) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static boolean isPossible(String skillTree) {
+        int i = 0;
+        for(char c : skillTree.toCharArray()) {
+            // 1. 선행 스킬에 없는 문자면 패스
+            if(!set.contains(c)) continue;
+            // 2. 이미 선행 스킬 문자가 모두 나왔으면 종료
+            if(i == arr.length) break;
+            // 3. 선행 스킬 순서가 맞지 않으면 false 리턴
+            if(c != arr[i]) return false;
+            // 4. 선행 스킬 순서 다음으로
+            i++;
         }
         return true;
     }
