d45ff5a36dc4785b198f46ebc2c3a0852b2e2685
ahah525 <ahah525@naver.com>
2023-04-26 16:15:45 +0900

    121690-[PCCP 모의고사 #2] 보물 지도(2_v2.성공)

--- programmers/pccp2/Solution121690.java
+++ programmers/pccp2/Solution121690.java
@@ -6,10 +6,14 @@ import java.util.Queue;
 
 /**
  * [문제명] [PCCP 모의고사 #2] 보물 지도
- * [풀이시간] 1시간 45분 / (55분)
+ * [풀이시간] 1시간 45분 / 1시간(55분 + 5분)
  * [한줄평] 예전에 비슷한 문제를 풀어본적이 있어서 3차원 배열로 접근해야한다는 건 알았는데 생각보다 푸는데 너무 오래걸렸고 결국 문제 풀이를 봤다.
+ * / 3차원 배열로 접근해서 풀어야한다는 것은 알았는데, 막상 구현하는데 시간이 좀 걸렸다.
  * 1_v1. BFS(성공)
  * 2_v1. DP, BFS(실패 - 13, 15, 16 시간초과)
+ * 2_v2. DP, BFS(성공)
+ * [점화식] dp[j][x][y]: 점프를 j번 해서 (x, y)에 왔을 때 최소 이동 시간
+ * [해결법] 가로, 세로가 최대 1000이길래 dp[j][x][y] 의 값을 최댓값을 2001 로 세팅했었는데, Integer.MAX_VALUE 로 초기화해서 해결함
  * @See <a href="https://school.programmers.co.kr/learn/courses/15009/lessons/121690">문제</a>
  * @See <a href="https://www.acmicpc.net/problem/1600">유사 문제</a>
  */
@@ -96,17 +100,15 @@ class Solution121690 {
     int[] dy = {0, 0, 1, -1};
     int[] jx = {2, -2, 0, 0};
     int[] jy = {0, 0, 2, -2};
-    int MAX_VALUE = 2001;
+    int MAX_VALUE = Integer.MAX_VALUE;
     public int solution2(int n, int m, int[][] hole) {
         int answer = 0;
-        // 초기화 0 : 길, 1 : 구멍
+        // 1. map 초기화 0 : 길, 1 : 구멍
         int[][] map = new int[m][n];
         for(int[] h : hole) {
             map[m - h[1]][h[0] - 1] = 1;
         }
-        // for(int[] arr : map) {
-        //     System.out.println(Arrays.toString(arr));
-        // }
+        // 2. dp 초기화
         int[][][] dp = new int[2][m][n];
         for(int i = 0; i < 2; i++) {
             for(int j = 0; j < m; j++) {
@@ -115,16 +117,10 @@ class Solution121690 {
                 }
             }
         }
-
+        // 3.
         bfs(m - 1, 0, map, dp, m, n);
-        // for(int[][] arr : dp) {
-        //     for(int[] a : arr) {
-        //         System.out.println(Arrays.toString(a));
-        //     }
-        //     System.out.println();
-        // }
-        if(dp[1][0][n - 1] == MAX_VALUE) return -1;
-        return dp[1][0][n - 1];
+        // 4. (0, n - 1)에 도달할 수 없으면 -1 리턴, 도달 가능하면 최소 이동횟수 리턴
+        return (dp[1][0][n - 1] == MAX_VALUE) ? -1 : dp[1][0][n - 1];
     }
 
     public void bfs(int startX, int startY, int[][] map, int[][][] dp, int m, int n) {
@@ -133,16 +129,12 @@ class Solution121690 {
         dp[0][startX][startY] = 0;
         dp[1][startX][startY] = 0;
         while(!q.isEmpty()) {
-            // for(int[] a : dp[0]) {
-            //     System.out.println(Arrays.toString(a));
-            // }
-            // System.out.println();
             P p = q.poll();
             if(p.x == 0 && p.y == n - 1) continue;
-            //
             int x = p.x;
             int y = p.y;
             int j = p.jump;
+            // 1칸 이동
             for(int i = 0; i < 4; i++) {
                 int nx = p.x + dx[i];
                 int ny = p.y + dy[i];
@@ -152,7 +144,7 @@ class Solution121690 {
                     dp[j][nx][ny] = Math.min(dp[j][nx][ny], dp[j][x][y] + 1);
                 }
             }
-            //
+            // 점프
             if(p.jump == 0) {
                 for(int i = 0; i < 4; i++) {
                     int nx = p.x + jx[i];
