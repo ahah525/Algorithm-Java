8416384334513de8527811ce3eef85091cec6b37
ahah525 <ahah525@naver.com>
2023-04-26 14:08:00 +0900

    121689-[PCCP 모의고사 #2] 카페 확장(2_v1.성공)

--- programmers/pccp2/Solution121689.java
+++ programmers/pccp2/Solution121689.java
@@ -6,9 +6,10 @@ import java.util.Queue;
 
 /**
  * [문제명] [PCCP 모의고사 #2] 카페 확장
- * [풀이시간] 38분
- * [한줄평] CPU 스케줄링과 비슷한 문제였는데 쉬운듯 약간 복잡했던 문제였다. 다시 풀어보면 좋을 것 같은 문제다.
- * 1_v1. Queue(성공)
+ * [풀이시간] 38분 / 20분
+ * [한줄평] CPU 스케줄링과 비슷한 문제였는데 쉬운듯 약간 복잡했던 문제였다. 다시 풀어보면 좋을 것 같은 문제다. / 다시 풀어봐도 좋을 문제다.
+ * 1_v1. LinkedList(성공)
+ * 2_v1. LinkedList(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/15009/lessons/121689">문제</a>
  */
 class Solution121689 {
@@ -17,6 +18,7 @@ class Solution121689 {
         System.out.println();
     }
 
+    // 1_v1, 2_v1
     /**
      * @param menu 각 음료들의 제조 시간을 담은 정수 배열
      * @param order 오늘 손님들이 주문한 음료가 순서대로 적힌 배열
@@ -29,29 +31,28 @@ class Solution121689 {
         Queue<Integer> q = new LinkedList<>();
         int t = 0;      // 현재 시각
         int end = 0;    // 큐의 마지막값(현재 대기중인 음료중 가장 마지막에 끝나는 시각)
+        // 1. 음료를 k초 마다 큐에 넣기
         for(int i : order) {
-            // 1. 제조 완료된 음료들 큐에서 먼저 빼기
+            // 2. t초 기준 큐에 제조 완료된 음료들이 있으면 빼기
             while(!q.isEmpty()) {
-                // 큐에 담긴 음료의 제조 완료시각 > 현재 시각(t)이면 -> 큐에서 뺄 음료 없음
-                if(q.peek() > t)
-                    break;
-                // 큐에 담긴 음료의 제조 완료시각 <= 현재 시각(t)이면 -> 큐에서 음료 빼기
+                // 큐에 담긴 음료의 제조 완료시각 > 현재 시각(t)이면 -> 아직 제조 중인 음료는 큐에서 뺄 수 없으므로 break
+                if(q.peek() > t) break;
+                // 큐에 담긴 음료의 제조 완료시각 <= 현재 시각(t)이면 -> 이미 제조된 음료이므로 큐에서 빼기
                 q.poll();
             }
-            // 2. 현재 음료의 제조 완료시각을 계산해 큐에 넣기
+            // 3. 현재 음료의 제조 완료시각을 계산해 큐에 넣기
             if(q.isEmpty()) {
                 // t초 에 이전 음료들이 모두 제조 완료된 상태라면 -> 현재 음료 제조완료시각 = t + 음료 제조시간
-                q.add(t + menu[i]);
                 end = t + menu[i];
             } else {
                 // t초 에 아직 제조 중인 음료가 있다면 -> 현재 음료제조 완료시각 = 마지막 음료 제조완료시각 + 음료제조시간
-                q.add(end + menu[i]);
-                end = end + menu[i];
+                end += menu[i];
             }
+            q.add(end);
+            // 4. 큐에 넣고나서 대기중인 음료(사람) 개수로 최댓값 갱신
+            max = Math.max(max, q.size());
             // System.out.println("t=" + t);
             // System.out.println(q);
-            // 3. 큐에 넣고나서 대기중인 음료(사람) 개수로 최댓값 갱신
-            max = Math.max(max, q.size());
             t += k;
         }
         return max;
