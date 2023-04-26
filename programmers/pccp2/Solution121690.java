a593856100f249a22172376ec0c93342dd96aa5b
ahah525 <ahah525@naver.com>
2023-04-26 16:04:27 +0900

    121690-[PCCP 모의고사 #2] 보물 지도(2_v1.실패)

--- programmers/pccp2/Solution121690.java
+++ programmers/pccp2/Solution121690.java
@@ -6,9 +6,10 @@ import java.util.Queue;
 
 /**
  * [문제명] [PCCP 모의고사 #2] 보물 지도
- * [풀이시간] 1시간 45분
+ * [풀이시간] 1시간 45분 / (55분)
  * [한줄평] 예전에 비슷한 문제를 풀어본적이 있어서 3차원 배열로 접근해야한다는 건 알았는데 생각보다 푸는데 너무 오래걸렸고 결국 문제 풀이를 봤다.
  * 1_v1. BFS(성공)
+ * 2_v1. DP, BFS(실패 - 13, 15, 16 시간초과)
  * @See <a href="https://school.programmers.co.kr/learn/courses/15009/lessons/121690">문제</a>
  * @See <a href="https://www.acmicpc.net/problem/1600">유사 문제</a>
  */
@@ -23,6 +24,7 @@ class Solution121690 {
         System.out.println(solution(5, 4, hole2));
     }
 
+    // 1_v1
     static class P {
         int x;
         int y;
@@ -87,5 +89,81 @@ class Solution121690 {
             return visited[1][0][n - 1] - 1;
         }
         return Math.min(visited[0][0][n - 1], visited[1][0][n - 1]) - 1;
+    }
+
+    // 2_v1
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+    int[] jx = {2, -2, 0, 0};
+    int[] jy = {0, 0, 2, -2};
+    int MAX_VALUE = 2001;
+    public int solution2(int n, int m, int[][] hole) {
+        int answer = 0;
+        // 초기화 0 : 길, 1 : 구멍
+        int[][] map = new int[m][n];
+        for(int[] h : hole) {
+            map[m - h[1]][h[0] - 1] = 1;
+        }
+        // for(int[] arr : map) {
+        //     System.out.println(Arrays.toString(arr));
+        // }
+        int[][][] dp = new int[2][m][n];
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < m; j++) {
+                for(int k = 0; k < n; k++) {
+                    dp[i][j][k] = MAX_VALUE;
+                }
+            }
+        }
+
+        bfs(m - 1, 0, map, dp, m, n);
+        // for(int[][] arr : dp) {
+        //     for(int[] a : arr) {
+        //         System.out.println(Arrays.toString(a));
+        //     }
+        //     System.out.println();
+        // }
+        if(dp[1][0][n - 1] == MAX_VALUE) return -1;
+        return dp[1][0][n - 1];
+    }
+
+    public void bfs(int startX, int startY, int[][] map, int[][][] dp, int m, int n) {
+        Queue<P> q = new LinkedList<>();
+        q.add(new P(startX, startY, 0));
+        dp[0][startX][startY] = 0;
+        dp[1][startX][startY] = 0;
+        while(!q.isEmpty()) {
+            // for(int[] a : dp[0]) {
+            //     System.out.println(Arrays.toString(a));
+            // }
+            // System.out.println();
+            P p = q.poll();
+            if(p.x == 0 && p.y == n - 1) continue;
+            //
+            int x = p.x;
+            int y = p.y;
+            int j = p.jump;
+            for(int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if(0 <= nx && nx < m && 0 <= ny && ny < n
+                        && map[nx][ny] == 0 && dp[j][nx][ny] == MAX_VALUE) {
+                    q.add(new P(nx, ny, j));
+                    dp[j][nx][ny] = Math.min(dp[j][nx][ny], dp[j][x][y] + 1);
+                }
+            }
+            //
+            if(p.jump == 0) {
+                for(int i = 0; i < 4; i++) {
+                    int nx = p.x + jx[i];
+                    int ny = p.y + jy[i];
+                    if(0 <= nx && nx < m && 0 <= ny && ny < n
+                            && map[nx][ny] == 0 && dp[1][nx][ny] == MAX_VALUE) {
+                        q.add(new P(nx, ny, 1));
+                        dp[1][nx][ny] = Math.min(dp[1][nx][ny], dp[0][x][y] + 1);
+                    }
+                }
+            }
+        }
     }
 }
