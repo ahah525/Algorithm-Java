b5bb3b352718bd17b4e5d898e2c890943465e5c1
ahah525 <ahah525@naver.com>
2023-04-27 22:10:58 +0900

    12913-땅따먹기(2_v1.성공)

--- programmers/level2/Solution12913.java
+++ programmers/level2/Solution12913.java
@@ -3,11 +3,13 @@ package programmers.level2;
 
 /**
  * [문제명] 땅따먹기
- * [풀이시간] 30분
- * [한줄평] 보자마자 DP 로 풀어야겠다고 생각이 들었던 문제였는데, 알수없는 런타임에러 문제로 시간을 많이 허비했다..
+ * [풀이시간] 30분 / 7분
+ * [한줄평] 보자마자 DP 로 풀어야겠다고 생각이 들었던 문제였는데, 알수없는 런타임에러 문제로 시간을 많이 허비했다.. / 기초 DP 문제였다.
  * 1_v1. DP(성공)
  * [점화식] d[i][j] = land[i][j] + Math.max(d[i - 1][k]) (단 k != j)
  * - d[i][j]: i행에서 j열을 선택했을 때 점수의 최댓값
+ * 2_v1. DP(성공)
+ * [점화식] land[i][j] += Math.max(d[i-1][k]) (k!=j)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12913">문제</a>
  */
 class Solution12913 {
@@ -16,6 +18,7 @@ class Solution12913 {
         System.out.println();
     }
 
+    // 1_v1
     /**
      * @param land 땅따먹기 게임의 땅
      * @return 마지막 행까지 모두 내려왔을 때, 얻을 수 있는 점수의 최대값
@@ -43,6 +46,25 @@ class Solution12913 {
         int answer = 0;
         for(int i = 0; i < 4; i++) {
             answer = Math.max(answer, d[n - 1][i]);
+        }
+        return answer;
+    }
+
+    // 2_v1
+    public int solution2(int[][] land) {
+        int answer = 0;
+        for(int i = 1; i < land.length; i++) {
+            for(int j = 0; j < 4; j++) {
+                int max = 0;
+                for(int k = 0; k < 4; k++) {
+                    if(j == k) continue;
+                    max = Math.max(max, land[i - 1][k]);
+                }
+                land[i][j] += max;
+            }
+        }
+        for(int i = 0; i < 4; i++) {
+            answer = Math.max(answer, land[land.length - 1][i]);
         }
         return answer;
     }
