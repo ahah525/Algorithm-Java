6ee5daa0076f66f5ce49897f24ba02ec311105ed
ahah525 <ahah525@naver.com>
2023-05-01 10:36:05 +0900

    42860-조이스틱(1_v2.성공)

--- programmers/level2/Solution42860.java
+++ programmers/level2/Solution42860.java
@@ -3,10 +3,12 @@ package programmers.level2;
 
 /**
  * [문제명] 조이스틱
- * [풀이시간] (50분)
- * [한줄평]
+ * [풀이시간] 1시간 50분(50분 + 1시간)
+ * [한줄평] 문제 풀이를 이해하는데에도 너무 어려웠던 문제였다.
  * 1_v1. (실패 - 11,13~14,18,20,22~27 실패)
+ * 1_v2. (성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42860">문제</a>
+ * @See <a href="https://born2bedeveloper.tistory.com/26">풀이 참고</a>
  */
 class Solution42860 {
     public static void main(String[] args) {
@@ -14,54 +16,30 @@ class Solution42860 {
         System.out.println();
     }
 
-    // 1_v1
+    // 1_v2
+    /**
+     * @param name 만들고자 하는 이름
+     * @return 이름에 대해 조이스틱 조작 횟수의 최솟값
+     */
     public int solution(String name) {
         int answer = 0;
-        boolean isFind = false;
-
-        // 왼쪽으로 갈 때
-        int a = 0;  // 제일 왼쪽에 있는 'A'가 아닌 문자
-        // 오른쪽으로 갈 때
-        int b = 0;  // 제일 오른쪽에 있는 'A'가 아닌 문자
-        for(int i = 1; i < name.length(); i++) {
-            if(name.charAt(i) == 'A') continue;
-            if(!isFind) {
-                isFind = true;
-                a = i;
+        int len = name.length();
+        int move = len - 1; // 커서 이동 횟수(처음부터 끝까지 계속 오른쪽으로 가는 경우 = 최댓값)
+        for(int i = 0; i < len; i++) {
+            char c = name.charAt(i);
+            // 1. 상, 하 선택
+            answer += Math.min(c - 'A', 'Z' - c + 1);
+            // 2. 좌, 우 선택
+            // 다음 문자가 A일 때 연속된 A의 다음 문자 인덱스 구하기
+            int idx = i + 1;
+            while(idx < len && name.charAt(idx) == 'A') {
+                idx++;
             }
-            b = i;
+            // 2-1. 현재 위치에서 오른쪽으로 갔다가 왼쪽으로 가는 경우
+            move = Math.min(move, 2 * i + (len - idx));
+            // 2-2. 현재 위치에서 왼쪽으로 갔다가 오른쪽으로 가는 경우
+            move = Math.min(move, 2 * (len - idx) + i);
         }
-        boolean right = (b <= name.length() - a) ? true : false;
-        int cursor = 0;
-        // System.out.println(a);
-        // System.out.println(b);
-        if(right) {
-            while(true) {
-                int c = name.charAt(cursor) - 'A';
-                int d = 'Z' - name.charAt(cursor) + 1;
-                answer += Math.min(c, d);
-                // System.out.println(name.charAt(cursor));
-                // System.out.println(c);
-                // System.out.println(d);
-                if(cursor == b) break;
-                cursor++;
-                answer++;
-            }
-        } else {
-            while(true) {
-                int c = name.charAt(cursor) - 'A';
-                int d = 'Z' - name.charAt(cursor) + 1;
-                answer += Math.min(c, d);
-                // System.out.println(name.charAt(cursor));
-                // System.out.println(c);
-                // System.out.println(d);
-                if(cursor == a) break;
-                if(cursor == 0) cursor = name.length() - 1;
-                else cursor--;
-                answer++;
-            }
-        }
-
-        return answer;
+        return answer + move;
     }
 }
