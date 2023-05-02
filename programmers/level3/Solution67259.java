36ea0d3cca56de434b09ba5e0ef48f750c072810
ahah525 <ahah525@naver.com>
2023-05-02 23:53:41 +0900

    67259-[카카오 인턴] 경주로 건설(3_v1.실패)

--- programmers/level3/Solution67259.java
+++ programmers/level3/Solution67259.java
@@ -6,11 +6,12 @@ import java.util.Queue;
 
 /**
  * [문제명] [카카오 인턴] 경주로 건설
- * [풀이시간] 1시간 50분/27분
+ * [풀이시간] 1시간 50분 / 27분 / (37분)
  * [한줄평] 생각보다 너무 어려웠던 문제였고 결국 다른 풀이를 보고 해결했다.. 비슷한 문제를 몇번 풀어본 기억이 있는데 여전히 어렵다ㅠㅠ
  * 1_v1. DP + BFS(성공)
  * - dp[d][x][y]: (x, y)좌표용 d 방향일 때 최소 비용
  * 2_v1. DP + BFS(성공)
+ * 3_v1. BFS(실패 - 3~4,6~11,13~23,25 실패)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/67259">문제</a>
  * @See <a href="https://girawhale.tistory.com/86">다른 풀이</a>
  */
@@ -90,5 +91,53 @@ class Solution67259 {
             }
         }
         return min;
+    }
+
+    // 2_v1
+    public int solution2(int[][] board) {
+        int n = board.length;
+        int[][][] visited = new int[4][n][n];
+        int MAX = Integer.MAX_VALUE;
+        int answer = MAX;
+        for(int i = 0; i < 4; i++) {
+            for(int j = 0; j < n; j++) {
+                for(int k = 0; k < n; k++) {
+                    visited[i][j][k] = MAX;
+                }
+            }
+        }
+        // 상하좌우
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        Queue<P> q = new LinkedList<>();
+        q.add(new P(1, 0, 0));
+        q.add(new P(3, 0, 0));
+        visited[1][0][0] = 0;
+        visited[3][0][0] = 0;
+        while(!q.isEmpty()) {
+            P p = q.poll();
+            int x = p.x;
+            int y = p.y;
+            int d = p.d;
+            int dd = (d % 2 == 0) ? d + 1 : d - 1;
+            for(int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if(i == dd) continue;
+                if(0 <= nx && nx < n && 0 <= ny && ny < n) {
+                    if(board[nx][ny] == 0 && visited[i][nx][ny] == MAX) {
+                        // System.out.println(i + "," + nx + "," + ny);
+                        q.add(new P(i, nx, ny));
+                        int cost = (d == i) ? 100 : 600;
+                        visited[i][nx][ny] = visited[d][x][y] + cost;
+                    }
+                }
+            }
+        }
+        for(int i = 0; i < 4; i++) {
+            System.out.println(visited[i][n-1][n-1]);
+            answer = Math.min(answer, visited[i][n-1][n-1]);
+        }
+        return answer;
     }
 }
