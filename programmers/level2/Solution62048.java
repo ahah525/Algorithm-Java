56e8ce87395109057bdd94845ab1a276de7b9c3f
ahah525 <ahah525@naver.com>
2023-05-02 18:07:39 +0900

    62048-멀쩡한 사각형(2_v1.성공)

--- programmers/level2/Solution62048.java
+++ programmers/level2/Solution62048.java
@@ -3,10 +3,12 @@ package programmers.level2;
 
 /**
  * [문제명] 멀쩡한 사각형
- * [풀이시간] 23분
- * [한줄평] 그냥 수학문제라고 생각하고 풀었던 문제였다.
+ * [풀이시간] 23분 / 8분
+ * [한줄평] 그냥 수학문제라고 생각하고 풀었던 문제였다. / 수학으로 푸는 쉬운 문제였다.
  * 1_v1. 수학(성공)
  * - 밑변의 길이를 1, 2, ... w 로
+ * 2_v1. 수학(성공)
+ * [접근법] i번째 행의 정사각형 개수 = Math.floor(w / h * i)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/62048">문제</a>
  */
 class Solution62048 {
@@ -15,6 +17,7 @@ class Solution62048 {
         System.out.println();
     }
 
+    // 1_v1
     /**
      * @param w 가로의 길이
      * @param h 세로의 길이
@@ -26,6 +29,16 @@ class Solution62048 {
         for(int i = 1; i <= w; i++) {
             double j = (double) h / w * i;
             answer += Math.floor(h - j);
+        }
+        return answer * 2;
+    }
+
+    // 2_v1
+    public long solution2(int w, int h) {
+        long answer = 0;
+        double n = (double) w / h;
+        for(int i = 0; i < h; i++) {
+            answer += Math.floor(n * i);
         }
         return answer * 2;
     }
