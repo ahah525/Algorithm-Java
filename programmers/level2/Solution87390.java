1c7d4cc49c955ea5cbfcd32f34997e84d3c26f9a
ahah525 <ahah525@naver.com>
2023-05-04 00:28:19 +0900

    87390-n^2 배열 자르기(3_v2.성공)

--- programmers/level2/Solution87390.java
+++ programmers/level2/Solution87390.java
@@ -5,14 +5,15 @@ import java.util.List;
 
 /**
  * [문제명] n^2 배열 자르기
- * [풀이시간] / 20분(15분 + 5분) / (17분)
+ * [풀이시간] / 20분(15분 + 5분) / 20분(17분 + 3분)
  * [한줄평] 두번째 풀 때는 바로 어떻게 풀어야겠다고 접근 방법을 떠올리긴했으나 반례를 잡지 못해 한번에 못풀었다. 1번 방법이 더 간결한 풀이다.
- * /
+ * / 역시나 2번째 풀었을 때와 동일한 반례때문에 런타임 에러 실패를 했던 문제다.
  * 1_v1. (성공)
  * 2_v1. (실패 - 2, 3, 15~20 실패)
  * 2_v2. (성공)
  * [반례] a == c 일 경우, 범위 밖의 값들이 추가되는 문제 발생
  * 3_v1. (실패 - 2~3,15~20 런타임 에러)
+ * 3_v2. (성공) -> 빠름
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/87390">문제</a>
  */
 class Solution87390 {
@@ -26,6 +27,7 @@ class Solution87390 {
         System.out.println(answer);
     }
 
+    // 1_v1
     /**
      * @param n 1 ≤ n ≤ 107
      * @param left 0 ≤ left ≤ right < n2
@@ -43,6 +45,7 @@ class Solution87390 {
         return answer;
     }
 
+    // 2_v2
     public List<Integer> solution2(int n, long left, long right) {
         List<Integer> answer = new ArrayList<>();
         int a = (int) (left / n);
@@ -69,6 +72,39 @@ class Solution87390 {
             for(int i = 0; i <= d; i++) {
                 answer.add(Math.max(c, i) + 1);
             }
+        }
+        return answer;
+    }
+
+    // 3_v2
+    public int[] solution3(int n, long left, long right) {
+        int len = (int) (right - left + 1);
+        int[] answer = new int[len];
+        int lx = (int) (left / n);
+        int ly = (int) (left % n);
+        int rx = (int) (right / n);
+        int ry = (int) (right % n);
+        int idx = 0;
+        // 같은 행인 경우
+        if(lx == rx) {
+            for(int i = ly; i <= ry; i++) {
+                answer[idx++] = Math.max(lx, i) + 1;
+            }
+            return answer;
+        }
+        // left 행
+        for(int i = ly; i < n; i++) {
+            answer[idx++] = Math.max(lx, i) + 1;
+        }
+        // left 행과 right 행 사이에 있는 행들
+        for(int i = lx + 1; i < rx; i++) {
+            for(int j = 0; j < n; j++) {
+                answer[idx++] = Math.max(i, j) + 1;
+            }
+        }
+        // right 행
+        for(int i = 0; i <= ry; i++) {
+            answer[idx++] = Math.max(rx, i) + 1;
         }
         return answer;
     }
