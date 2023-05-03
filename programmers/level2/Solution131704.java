c845416a4cb3c50d71c76612f52cfd43b68d224e
ahah525 <ahah525@naver.com>
2023-05-03 15:00:44 +0900

    131704-택배상자(2_v1.실패)

--- programmers/level2/Solution131704.java
+++ programmers/level2/Solution131704.java
@@ -5,10 +5,11 @@ import java.util.Stack;
 
 /**
  * [문제명] 택배상자
- * [풀이시간] 50분(25분 + 25분)
+ * [풀이시간] 50분(25분 + 25분) / (30분)
  * [한줄평] 문제 이해하기가 조금 까다로웠고 생각보다 푸는데 더 어려웠던 문제였다.. 다음에 한번 더 풀어볼만한 문제다.
  * 1_v1. Stack(실패 - 6, 7, 9, 10 시간초과)
  * 1_v2. Stack(성공)
+ * 2_v1. Stack(실패 - 6~10 실패)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/131704">문제</a>
  * @See <a href="https://velog.io/@biny22/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%83%9D%EB%B0%B0%EC%83%81%EC%9E%90">문제 설명</a>
  */
@@ -44,5 +45,31 @@ class Solution131704 {
             }
         }
         return idx;
+    }
+
+    // 2_v1
+    public int solution2(int[] order) {
+        int answer = 0;
+        // 보조 컨베이어
+        Stack<Integer> stack = new Stack<>();
+        int container = 1;
+        for(int n : order) {
+            // 컨테이너에서 가져올 수 있으면
+            while(container < n) {
+                stack.push(container);
+                container++;
+            }
+            if(n == container) {
+                answer++;
+            } else {
+                if(n == stack.peek()) {
+                    stack.pop();
+                    answer++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return answer;
     }
 }
