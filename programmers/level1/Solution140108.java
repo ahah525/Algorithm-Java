92c8603095aedb4c592364dc5b6d4b7b494748c4
ahah525 <ahah525@naver.com>
2023-05-03 14:21:10 +0900

    140108-문자열 나누기(2_v1.성공)

--- programmers/level1/Solution140108.java
+++ programmers/level1/Solution140108.java
@@ -3,10 +3,11 @@ package programmers.level1;
 
 /**
  * [문제명] 문자열 나누기
- * [풀이시간] 15분
- * [한줄평] 쉬운 문제이긴 했으나 처음에 반례를 잡지 못해 틀렸던 문제였다.
- * 1_v1. (성공)
+ * [풀이시간] 15분 / 10분
+ * [한줄평] 쉬운 문제이긴 했으나 처음에 반례를 잡지 못해 틀렸던 문제였다. / 쉽게 풀 수 있는 문자열 문제다.
+ * 1_v1. 문자열(성공)
  * [반례] 마지막에 분해되고 남은 글자는 카운팅 안되는 문제
+ * 2_v1. 문자열(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/140108">문제</a>
  */
 class Solution140108 {
@@ -15,6 +16,7 @@ class Solution140108 {
         System.out.println();
     }
 
+    // 1_v1
     /**
      * @param s 문자열
      * @return 문자열들로 분해하고, 분해한 문자열의 개수
@@ -47,6 +49,30 @@ class Solution140108 {
         }
         // 분해하고 남은 문자가 있으면 1개 추가로 카운트
         if(!split) answer++;
+        return answer;
+    }
+
+    // 2_v1
+    public int solution2(String s) {
+        int answer = 0;
+        char x = ' ';
+        int a = 0;  // x의 개수
+        int b = 0;  // x가 아닌 문자의 개수
+        for(char c : s.toCharArray()) {
+            // 첫번째 문자 갱신
+            if(a == 0) x = c;
+            // x 개수, x가 아닌 문자의 개수 세기
+            if(c == x) a++;
+            else b++;
+            // 두 개가 같으면 분리
+            if(a == b) {
+                a = 0;
+                b = 0;
+                answer++;
+            }
+        }
+        // 남은 문자가 있으면 1개 추가
+        if(a != 0) answer++;
         return answer;
     }
 }
