d0b1b2285e4ede1e80ead17c1b6b677e32765b4b
ahah525 <ahah525@naver.com>
2023-05-04 14:54:05 +0900

    12905-가장 큰 정사각형 찾기(2_v1.성공)

--- programmers/level2/Solution12905.java
+++ programmers/level2/Solution12905.java
@@ -3,15 +3,18 @@ package programmers.level2;
 
 /**
  * [문제명] 가장 큰 정사각형 찾기
- * [풀이시간] 1시간(15분 + 5분 + 40분)
+ * [풀이시간] 1시간(15분 + 5분 + 40분) / 26분
  * [한줄평] DP 로 풀어야함을 알아도 못풀어서 결국 문제 풀이를 보고 이해했던 문제였다.
  * 1_v1. (실패 - 정확성 테스트 8, 14 실패, 효율성 테스트 모두 실패)
- * 2_v1. (실패 - 효율성 테스트 모두 실패)
+ * 1_v2. (실패 - 효율성 테스트 모두 실패)
  * - 오직 1로 이루어진 가장 큰 정사각형을 구해야 함!!
  * [반례] 모두 0으로 이루어진 경우
- * 3_v1. DP(성공)
+ * 1_v3. DP(성공)
  * - d[i][j]: (i, j)가 포함될 수 있는 가장 큰 정사각형 한 변의 길이
  * [점화식] d[i][j] = Math.min(Math.min(d[i][j - 1], d[i - 1][j - 1]), d[i - 1][j]) + 1
+ * 2_v1. DP(실패 - 정확성 테스트 1 실패)
+ * 2_v2. DP(성공)
+ * [반례] 행/열의 길이가 1인 경우 최댓값 갱신 안되는 문제 해결
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12905">문제</a>
  * @See <a href="https://school.programmers.co.kr/questions/12304">반례</a>
  * @See <a href="https://small-stap.tistory.com/40">풀이 참고</a>
@@ -22,6 +25,7 @@ class Solution12905 {
         System.out.println();
     }
 
+    // 1_v3
     /**
      * @param board 1와 0로 채워진 표
      * @return 표에서 1로 이루어진 가장 큰 정사각형을 찾아 넓이를 return
@@ -38,6 +42,27 @@ class Solution12905 {
                 // 2. 1인 경우 (←, ↖, ︎↑) 좌표 중 최솟값 + 1
                 d[i][j] = Math.min(Math.min(d[i][j - 1], d[i - 1][j - 1]), d[i - 1][j]) + 1;
                 answer = Math.max(answer, d[i][j]);
+            }
+        }
+        return answer * answer;
+    }
+
+    // 2_v1
+    public int solution2(int[][] board) {
+        int answer = 0;
+        int n = board.length;
+        int m = board[0].length;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(board[i][j] != 1) {
+                    board[i][j] = 0;
+                    continue;
+                }
+                if(i != 0 && j != 0) {
+                    int min = Math.min(board[i][j - 1], Math.min(board[i - 1][j - 1], board[i - 1][j]));
+                    board[i][j] += min;
+                }
+                answer = Math.max(answer, board[i][j]);
             }
         }
         return answer * answer;
