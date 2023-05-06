185c3e7cf9913d953973298fcb3733d87d17bf87
ahah525 <ahah525@naver.com>
2023-05-06 22:10:43 +0900

    12978-배달(3_v1.성공)

--- programmers/level2/Solution12978.java
+++ programmers/level2/Solution12978.java
@@ -1,15 +1,19 @@
 package programmers.level2;
 
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
  * [문제명] 배달
- * [풀이시간] 3시간 / 40분
- * [한줄평] 이해하는데 너무 오래걸렸다... 나중에 복습 무조건 해야할 문제다.
+ * [풀이시간] 3시간 / 40분 / 55분
+ * [한줄평] 이해하는데 너무 오래걸렸다... 나중에 복습 무조건 해야할 문제다. / 유형에 대한 문제 풀이 복습이 꼭 필요하다.
  * 1_v1. 다익스트라(성공)
+ * [접근법] 최단 거리가 가장 짧은 노드를 선택하기 위해 for문 으로 직접 탐색
  * 2_v1. 다익스트라(성공)
+ * [접근법] 최단 거리가 가장 짧은 노드를 선택하기 위해 PriorityQueue 사용
+ * 3_v1. 다익스트라(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12978">문제</a>
  * @See <a href="https://velog.io/@yanghl98/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%B0%B0%EB%8B%AC-JAVA%EC%9E%90%EB%B0%94">풀이 참고</a>
  */
@@ -19,6 +23,7 @@ class Solution12978 {
         System.out.println();
     }
 
+    // 1_v1
     /**
      * @param N 마을의 개수
      * @param road 각 마을을 연결하는 도로의 정보
@@ -80,6 +85,7 @@ class Solution12978 {
         return answer;
     }
 
+    // 2_v1
     public int solution2(int N, int[][] road, int K) {
         int answer = 0;
         int INF = 500001;   // 거리 최댓값
@@ -126,6 +132,52 @@ class Solution12978 {
         for(int i = 1; i <= N; i++) {
             if(d[i] <= K)
                 answer++;
+        }
+        return answer;
+    }
+
+    // 3_v1
+    public int solution3(int N, int[][] road, int K) {
+        int answer = 0;
+        boolean[] visited = new boolean[N + 1];
+        // 1. 인접행렬 초기화
+        int INF = 490001;
+        int[][] map = new int[N + 1][N + 1];
+        for(int i = 1; i <= N; i++) {
+            for(int j = 1; j <= N; j++) {
+                if(i == j) continue;
+                map[i][j] = INF;
+            }
+        }
+        for(int[] r : road) {
+            if(map[r[0]][r[1]] > r[2]) {
+                map[r[0]][r[1]] = r[2];
+                map[r[1]][r[0]] = r[2];
+            }
+        }
+        // 2. 최단 거리 배열 초기화
+        int[] d = new int[N + 1];
+        Arrays.fill(d, INF);
+        d[1] = 0;
+        // N개의 노드에 대해 반복하여 d[i] 구하기
+        for(int i = 1; i <= N; i++) {
+            // 3. 방문하지 않은 노드 중 최단 거리가 가장 짧은 노드 선택하기
+            int min = INF;
+            int idx = 0;
+            for(int j = 1; j <= N; j++) {
+                if(!visited[j] && d[j] < min) {
+                    min = d[j];
+                    idx = j;
+                }
+            }
+            visited[idx] = true;
+            if(d[idx] <= K) answer++;
+            // 4. idx를 거쳐갈 때 더 최단거리가 나오면 갱신
+            for(int j = 1; j <= N; j++) {
+                if(d[j] > d[idx] + map[idx][j]) {
+                    d[j] = d[idx] + map[idx][j];
+                }
+            }
         }
         return answer;
     }
