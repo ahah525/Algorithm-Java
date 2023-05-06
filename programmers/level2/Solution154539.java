1ff8de41caab60e74df9372613b27d7eaef9033a
ahah525 <ahah525@naver.com>
2023-05-06 11:04:45 +0900

    154539-뒤에 있는 큰 수 찾기(3_v1.성공)

--- programmers/level2/Solution154539.java
+++ programmers/level2/Solution154539.java
@@ -6,11 +6,13 @@ import java.util.Stack;
 
 /**
  * [문제명] 뒤에 있는 큰 수 찾기
- * [풀이시간] 1시간 10분(4분 + 1시간 6분) / 6분
+ * [풀이시간] 1시간 10분(4분 + 1시간 6분) / 6분/ 7분
  * [한줄평] 2중 for문으로 풀면 시간초과가 난다는 것을 알았으나 어떻게 접근해야할지 몰라 결국 풀이를 봤는데, 답을 보고도 이해하기 어려웠던 문제였다. / 유사 문제를 풀고 풀었더니 빨리 풀 수 있었다.
+ * / 문제 유형만 알면 쉽게 풀 수 있는 문제다.
  * 1_v1. 2중 for문(실패-20, 21, 22, 23 시간초과)
  * 1_v2. Stack(성공)
  * 2_v1. Stack(성공)
+ * 3_v1. Stack(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/154539">문제</a>
  * @See <a href="https://velog.io/@ujone/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%92%A4%EC%97%90-%EC%9E%88%EB%8A%94-%ED%81%B0-%EC%88%98-%EC%B0%BE%EA%B8%B0-JAVA">풀이 참고</a>
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42584">유사 문제</a>
@@ -21,6 +23,7 @@ class Solution154539 {
         System.out.println();
     }
 
+    // 1_v2, 2_v1
     /**
      * @param numbers 정수로 이루어진 배열(4 ≤ numbers의 길이 ≤ 1,000,000)
      * @return 모든 원소에 대한 뒷 큰수들을 차례로 담은 배열
@@ -40,6 +43,23 @@ class Solution154539 {
                 stack.pop();
             }
             stack.push(i);
+        }
+        return answer;
+    }
+
+    // 3_v1
+    public int[] solution2(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0; i < numbers.length; i++) {
+            while(!stack.isEmpty() && numbers[stack.peek()] < numbers[i]) {
+                answer[stack.peek()] = numbers[i];
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
         }
         return answer;
     }
