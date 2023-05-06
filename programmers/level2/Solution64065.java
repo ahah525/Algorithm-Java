b84092ffac9b402da98cbe2435fef1d06d6a9343
ahah525 <ahah525@naver.com>
2023-05-06 11:35:23 +0900

    64065-튜플(3_v1.성공)

--- programmers/level2/Solution64065.java
+++ programmers/level2/Solution64065.java
@@ -1,15 +1,16 @@
 package programmers.level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * [문제명] 튜플
- * [풀이시간] / 35분
+ * [풀이시간] / 35분 / 15분
  * [한줄평] / 치환까지는 쉽게 했는데, 그 이후에 숫자 개수 오름차순 정렬을 어떻게 해야할지 고민하다가 결국 힌트를 조금 보고 다시 풀었다.
  * 1_v1. (성공)
- * 2_v1. (성공)
+ * 2_v1. 문자열(성공)
+ * [접근법] List.contains() 사용
+ * 3_v1. 문자열(성공)
+ * [접근법] Set.contains() 사용
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/64065">문제</a>
  */
 class Solution64065 {
@@ -17,6 +18,7 @@ class Solution64065 {
         System.out.println(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"));
     }
 
+    // 2_v1
     /**
      * @param s 특정 튜플을 표현하는 집합이 담긴 문자열
      * @return s가 표현하는 튜플을 배열에 담아 return
@@ -40,6 +42,28 @@ class Solution64065 {
                     answer.add(n);
                     break;
                 }
+            }
+        }
+        return answer;
+    }
+
+    // 3_v1
+    public int[] solution2(String s) {
+        String[] arr = s.replace("{{", "")
+                .replace("}}", "")
+                .split("\\},\\{");
+
+        Arrays.sort(arr, (o1, o2) -> o1.length() - o2.length());
+
+        int[] answer = new int[arr.length];
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < arr.length; i++) {
+            for(String num : arr[i].split(",")) {
+                int n = Integer.parseInt(num);
+                // 이미 set에 있는 값이면(값 추가 실패시) 종료
+                if(!set.add(n)) continue;
+                answer[i] = n;
+                break;
             }
         }
         return answer;
