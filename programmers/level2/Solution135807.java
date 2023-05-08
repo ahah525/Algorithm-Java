d7812c81c0ac4a32033c6edce4d860ab8e0b4f67
ahah525 <ahah525@naver.com>
2023-05-08 17:02:16 +0900

    135807-숫자 카드 나누기(1_v2.성공)

--- programmers/level2/Solution135807.java
+++ programmers/level2/Solution135807.java
@@ -3,10 +3,12 @@ package programmers.level2;
 
 /**
  * [문제명] 숫자 카드 나누기
- * [풀이시간] 20분
+ * [풀이시간] 20분, 20분
  * [한줄평] 최대 공약수 구하는 것을 응용해서 풀 수 있는 문제였다.
  * 1_v1. 수학(성공)
- * [접근법] 최대공약수 -
+ * [접근법] 일반적인 방법
+ * 1_v2. 수학(성공)
+ * [접근법] 유클리드 호제법
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/135807">문제</a>
  */
 class Solution135807 {
@@ -15,21 +17,33 @@ class Solution135807 {
         System.out.println();
     }
 
+    // 1_v2
+    /**
+     * @param arrayA 철수가 가진 카드에 적힌 숫자들을 나타내는 정수 배열
+     * @param arrayB 영희가 가진 카드에 적힌 숫자들을 나타내는 정수 배열
+     * @return 주어진 조건을 만족하는 가장 큰 양의 정수 a
+     */
     public int solution(int[] arrayA, int[] arrayB) {
-        int a = gcd(arrayA);
-        int b = gcd(arrayB);
-        if(a == b) return 0;
-        if(a > b) {
-            for(int n : arrayB) {
-                if(n % a == 0) return 0;
-            }
+        int gcdA = gcd(arrayA);
+        int gcdB = gcd(arrayB);
+        if(gcdA == gcdB) return 0;
+        if(gcdA > gcdB) {
+            if(canDivide(gcdA, arrayB)) return 0;
         } else {
-            for(int n : arrayA) {
-                if(n % b == 0) return 0;
-            }
+            if(canDivide(gcdB, arrayA)) return 0;
         }
-        return Math.max(a, b);
+        return Math.max(gcdA, gcdB);
     }
+
+    // arr의 원소가 하나라도 gcd로 나누어지는지
+    public boolean canDivide(int gcd, int[] arr) {
+        for(int n : arr) {
+            if(n % gcd == 0) return true;
+        }
+        return false;
+    }
+
+    // 배열의 모든 원소의 최대공약수
     public int gcd(int[] arr) {
         int a = arr[0];
         for(int i = 1; i < arr.length; i++) {
@@ -38,11 +52,16 @@ class Solution135807 {
         return a;
     }
 
-    // 최대 공약수
+    // 두 수의 최대 공약수
     public int gcd(int a, int b) {
-        for(int i = Math.min(a, b); i >= 2; i--) {
-            if(a % i == 0 && b % i == 0) return i;
+        int min = Math.min(a, b);
+        int max = Math.max(a, b);
+        int tmp;
+        while(min != 0) {
+            tmp = max % min;
+            max = min;
+            min = tmp;
         }
-        return 1;
+        return max;
     }
 }
