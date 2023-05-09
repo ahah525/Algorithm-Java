70099982e77007d1bb6fc8e2c5272bfdbb9ad527
ahah525 <ahah525@naver.com>
2023-05-09 15:07:38 +0900

    17681-[1차] 비밀지도(2_v1.성공)

--- programmers/level1/Solution17681.java
+++ programmers/level1/Solution17681.java
@@ -3,9 +3,10 @@ package programmers.level1;
 
 /**
  * [문제명] [1차] 비밀지도
- * [풀이 시간] 35분
+ * [풀이 시간] 35분 / 11분
  * [한줄평] 비트 연산을 활용해야겠다는 것은 빨리 떠올렸는데, 진수 변환/앞에 0 채우기 등 문법 공부를 더 해야겠다고 느꼈다. 참고로 2번 방식이 약 7~8초 이상 느리다.
- * v1. StringBuilder 와 for 문, if 문으로 0 채우기와 변환 수행(성공-빠름)
+ * / 비트 연산만 떠올리면 쉽게 풀 수 있는 문제였다.
+ * 1_v1. StringBuilder 와 for 문, if 문으로 0 채우기와 변환 수행(성공) -> 빠름
  * [로직]
  * 1. 비트 OR 연산
  * '#' == 1, ' ' = 0
@@ -14,7 +15,8 @@ package programmers.level1;
  * 2. 연산 결과값을 10진수(int) -> 2진수(String) 변환
  * 3. 자리수를 맞추기 위해 앞에 0(' ') 넣기
  * 4. 변환(0 -> ' ', 1 -> '#') 후 String 으로 저장
- * v2. String.format(), replaceAll() 으로 0 채우기와 변환 수행(성공-느림)
+ * 1_v2. String.format(), replaceAll() 으로 0 채우기와 변환 수행(성공)
+ * 2_v1. 문자열, 비트(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/17681">문제</a>
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12938/solution_groups?language=java">다른 풀이</a>
  */
@@ -33,6 +35,7 @@ class Solution17681 {
         System.out.println(solution(n2, arr3, arr4));
     }
 
+    // 1_v1
     /**
      * @param n 지도의 한 변 크기
      * @param arr1 지도1
@@ -61,6 +64,7 @@ class Solution17681 {
         return answer;
     }
 
+    // 1_v2, 2_v1
     public static String[] solution2(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i = 0; i < n; i++) {
