85d811fca116329e8ac3dbf288f4b30e66a0f2dc
ahah525 <ahah525@naver.com>
2023-05-09 17:51:50 +0900

    12938-최고의 집합(2_v2.성공)

--- programmers/level3/Solution12938.java
+++ programmers/level3/Solution12938.java
@@ -6,16 +6,21 @@ import java.util.List;
 
 /**
  * [문제명] 최고의 집합
- * [풀이시간] (16분)
+ * [풀이시간] / 43분(16분 + 27분)
  * [한줄평] 처음에는 재귀로 완전탐색을 하는 방식으로 접근했다가 할 때마다 배열 값을 저장하는게 부담이 커져서 규칙을 찾아서 아예 다른 방식으로 접근해서 풀었던 문제였다.
+ * / 처음에 완전탐색으로 풀다가 시간초과가 났고 결국에는 풀이를 참고해서 풀었다.
  * 1_v1. 원소의 개수 = n, 합 = s 일때, 곱이 최대가 되는 중복집합은 (n // s) 값을 반복해서 구함(성공)
  * (가정) n = 3, s = 8
  * n = 3, s = 8         -> 8 // 3 = 2
  * n = 2, s = 8 - 2 = 6 -> 6 // 2 = 3
  * n = 1, s = 6 - 3 = 3 -> 3 // 1 = 3
  * 2_v1. DFS, 완전탐색(실패 - 1~14 시간초과)
+ * [접근법] 중복조합
+ * 2_v2. (성공)
+ * [접근법] 최대한 수를 공평하게 분배하기
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12938">문제</a>
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12938/solution_groups?language=java">다른 풀이</a>
+ * @See <a href="https://escapefromcoding.tistory.com/181">풀이 참고</a>
  */
 class Solution12938 {
     public static void main(String[] args) {
@@ -35,6 +40,7 @@ class Solution12938 {
         System.out.println(solution(n3, s3));
     }
 
+    // 1_v1
     /**
      * @param n 집합의 원소의 개수
      * @param s 모든 원소들의 합
@@ -63,38 +69,19 @@ class Solution12938 {
         return answer;
     }
 
-    // 2_v1
-    int max;
-    int[] answer;
-    int[] path;
+    // 2_v2
     public int[] solution2(int n, int s) {
-        max = 0;
-        path = new int[n];
-        answer = new int[n];
-
-        comb(0, 1, 0, 1, n, s);
-        if(max == 0) return new int[] {-1};
+        // 1. n > s 이면 답이 존재하지 않음
+        if(n > s) return new int[] {-1};
+        int[] answer = new int[n];
+        int cnt = n; // 뽑아야하는 원소 수
+        // 2. n개 숫자 뽑기
+        for(int i = 0; i < n; i++) {
+            int res = s / cnt; // cnt개의 숫자로 s를 만들기위한 가장 작은 수
+            answer[i] = res;
+            s -= res;
+            cnt--;
+        }
         return answer;
-    }
-
-    public void comb(int depth, int start, int sum, int mul, int n, int s) {
-        if(depth == n) {
-            // System.out.println(sum + "," + mul);
-            // System.out.println(Arrays.toString(path));
-
-            if(sum == s && max < mul) {
-                // System.out.println("예스");
-                max = mul;
-                for(int i = 0; i < n; i++) {
-                    answer[i] = path[i];
-                }
-            }
-            return;
-        }
-        //
-        for(int i = start; i <= s; i++) {
-            path[depth] = i;
-            comb(depth + 1, start, sum + i, mul * i, n, s);
-        }
     }
 }
