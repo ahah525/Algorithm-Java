5b6a4bf7d036028fe9e5e421b91bf7696d724141
ahah525 <ahah525@naver.com>
2023-05-11 10:04:57 +0900

    12980-점프와 순간 이동(2_v1.실패)

--- programmers/level2/Solution12980.java
+++ programmers/level2/Solution12980.java
@@ -1,13 +1,16 @@
 package programmers.level2;
 
 
+import java.util.Arrays;
+
 /**
  * [문제명] 점프와 순간 이동
- * [풀이시간] 12분
+ * [풀이시간] 12분 / (6분)
  * [한줄평] 처음에는 DP로 푸는 문제인가 했는데, N 이 최대 10억이라 O(N) 으로 풀 수 있는 문제가 아니라는 것을 알아차렸다. 코드는 굉장히 단순했던 문제였다.
- * v1. (성공)
+ * 1_v1. (성공)
  * - 건전지 사용량의 최솟값 = 2로 나눌 수 있다면 무조건 2로 나누기
- * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/">문제</a>
+ * 2_v1. (실패 - 효율성 테스트 1~10 시간초과)
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12980">문제</a>
  */
 class Solution12980 {
     public static void main(String[] args) {
@@ -38,5 +41,19 @@ class Solution12980 {
             }
         }
         return ans;
+    }
+
+    // 2_v1
+    public int solution2(int n) {
+        int[] d = new int[n + 1];
+        int INF = Integer.MAX_VALUE;
+        Arrays.fill(d, INF);
+        d[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            d[i] = Math.min(d[i], d[i - 1] + 1);
+            if(i % 2 == 0)
+                d[i] = Math.min(d[i], d[i / 2]);
+        }
+        return d[n];
     }
 }
