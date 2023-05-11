7f7c7e14d4bcbeee7b95d48df33265d4a176472c
ahah525 <ahah525@naver.com>
2023-05-11 10:10:32 +0900

    12980-점프와 순간 이동(2_v2.성공)

--- programmers/level2/Solution12980.java
+++ programmers/level2/Solution12980.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 
 /**
  * [문제명] 점프와 순간 이동
- * [풀이시간] 12분 / (6분)
+ * [풀이시간] 12분 / 9분(6분 + 3분)
  * [한줄평] 처음에는 DP로 푸는 문제인가 했는데, N 이 최대 10억이라 O(N) 으로 풀 수 있는 문제가 아니라는 것을 알아차렸다. 코드는 굉장히 단순했던 문제였다.
- * 1_v1. (성공)
- * - 건전지 사용량의 최솟값 = 2로 나눌 수 있다면 무조건 2로 나누기
- * 2_v1. (실패 - 효율성 테스트 1~10 시간초과)
+ * / 처음에 DP로 풀었다가 시간초과로 실패했고 결국엔 그리디로 풀 수 있는 문제였다.
+ * 1_v1. 그리디(성공)
+ * [접근법] 건전지 사용량의 최솟값 = 2로 나눌 수 있다면 무조건 2로 나누기
+ * 2_v1. DP(실패 - 효율성 테스트 1~10 시간초과)
+ * 2_v2. 그리디(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12980">문제</a>
  */
 class Solution12980 {
@@ -24,23 +26,24 @@ class Solution12980 {
         System.out.println(solution(5000));
     }
 
+    // 1_v1, 2_v1
     /**
      * @param n 아이언 슈트 구매자가 이동하려는 거리
      * @return 사용해야 하는 건전지 사용량의 최솟값
      */
     public static int solution(int n) {
-        int ans = 0;
+        int cnt = 0;
         while(n != 0) {
             if(n % 2 == 0) {
                 // 1. 짝수이면 2로 나누기(순간이동)
                 n /= 2;
             } else {
                 // 2. 홀수이면 1 빼기(점프)
-                n -= 1;
-                ans++;
+                n--;
+                cnt++;
             }
         }
-        return ans;
+        return cnt;
     }
 
     // 2_v1
