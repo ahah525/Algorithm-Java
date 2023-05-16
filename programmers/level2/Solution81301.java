41d264c63aac8122fb9664d90aaee274623939de
ahah525 <ahah525@naver.com>
2023-05-16 23:14:19 +0900

    81301-숫자 문자열과 영단어(2_v1.성공)

--- programmers/level2/Solution81301.java
+++ programmers/level2/Solution81301.java
@@ -3,9 +3,10 @@ package programmers.level2;
 
 /**
  * [문제명] 숫자 문자열과 영단어
- * [풀이시간] 17분
- * [한줄평] replaceAll() 을 떠올리기만 한다면 로직은 굉장히 짧은 쉬운 문제였다.
- * v1. (성공)
+ * [풀이시간] 17분 / 4분
+ * [한줄평] replaceAll() 을 떠올리기만 한다면 로직은 굉장히 짧은 쉬운 문제였다. / 쉽게 풀 수 있는 문제였다.
+ * 1_v1. 문자열(성공)
+ * 2_v1. 문자열(성공) -> 빠름
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/81301">문제</a>
  */
 class Solution81301 {
@@ -27,6 +28,7 @@ class Solution81301 {
         System.out.println(solution(s4));
     }
 
+    // 1_v1
     /**
      * @param s 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열
      * @return s가 의미하는 원래 숫자
@@ -36,6 +38,21 @@ class Solution81301 {
         for(int i = 0; i < 10; i++) {
             s = s.replaceAll(arr[i], Integer.toString(i));
         }
+        return Integer.parseInt(s);
+    }
+
+    // 2_v1
+    public int solution2(String s) {
+        s = s.replace("zero", "0")
+                .replace("one", "1")
+                .replace("two", "2")
+                .replace("three", "3")
+                .replace("four", "4")
+                .replace("five", "5")
+                .replace("six", "6")
+                .replace("seven", "7")
+                .replace("eight", "8")
+                .replace("nine", "9");
         return Integer.parseInt(s);
     }
 }
