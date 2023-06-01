162f19312fcfea579cf84d4fdfa8485f54cb7f7b
ahah525 <ahah525@naver.com>
2023-06-01 14:57:05 +0900

    150370-개인정보 수집 유효기간(1_v1.성공)

---
+++ programmers/level1/Solution150370.java
@@ -0,0 +1,62 @@
+package programmers.level1;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * [문제명] 개인정보 수집 유효기간
+ * [풀이시간] 19분
+ * [한줄평] 년월일을 일로 변환해서 푸는 것이 구현의 핵심이었다.
+ * 1_v1. 구현(성공)
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/150370">문제</a>
+ */
+class Solution150370 {
+    public static void main(String[] args) {
+        //
+        System.out.println();
+    }
+
+    /**
+     * @param today 오늘 날짜를 의미하는 문자열
+     * @param terms 약관의 유효기간을 담은 1차원 문자열 배열
+     * @param privacies 수집된 개인정보의 정보를 담은 1차원 문자열 배열
+     * @return 파기해야 할 개인정보의 번호를 오름차순으로 1차원 정수 배열에 담아 return
+     */
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        // 1. (약관 종류, 유효기간) 초기화
+        Map<String, Integer> map = new HashMap<>();
+        for(String term : terms) {
+            String[] t = term.split(" ");
+            map.put(t[0], Integer.parseInt(t[1]));
+        }
+        // 2. 오늘 날짜를 일로 변환
+        int now = getDay(today);
+        // 3. 각 약관의 유효기간이 지났는지 검사
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < privacies.length; i++) {
+            String[] p = privacies[i].split(" ");
+            int day = getDay(p[0]);
+            int term = map.get(p[1]);
+            if(day + 28 * term <= now) {
+                list.add(i + 1);
+            }
+        }
+        // 4. 리스트를 배열로 변환
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    // 년월일을 일로 변환
+    public int getDay(String date) {
+        // \\. 으로 사용해야 . 기준으로 분리됨
+        String[] d = date.split("\\.");
+        return (Integer.parseInt(d[0]) * 12 * 28)
+                + (Integer.parseInt(d[1]) * 28) + Integer.parseInt(d[2]);
+    }
+}
