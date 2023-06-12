3c5a8e5431c341de24816d837924f63735d848ce
ahah525 <ahah525@naver.com>
2023-06-12 10:57:53 +0900

    12900-2 x n 타일링(2_v1.성공)

--- programmers/level2/Solution12900.java
+++ programmers/level2/Solution12900.java
@@ -3,11 +3,12 @@ package programmers.level2;
 
 /**
  * [문제명] 2 x n 타일링
- * [한줄평] 유명한 DP 문제인 타일링 문제였기 때문에 쉽게 풀 수 있었다.
- * v1. DP(성공)
- * [점화식]
+ * [풀이시간] / 4분
+ * [한줄평] 유명한 DP 문제인 타일링 문제였기 때문에 쉽게 풀 수 있었다. / 아주 쉬운 타일링 문제였고 더이상 풀어볼 필요는 없다.
+ * 1_v1. DP(성공)
+ * [점화식] d[i] = d[i - 1] + d[i - 2]
  * d[i] = 2 * 1 타일로 2 * n 직사각형을 채우는 경우의 수
- * d[i] = d[i - 1] + d[i - 2]
+ * 2_v1. DP(성공)
  */
 class Solution12900 {
     public static void main(String[] args) {
@@ -16,6 +17,7 @@ class Solution12900 {
         System.out.println(solution(n));
     }
 
+    // 1_v1, 2_v1
     /**
      * @param n 가로의 길이(60,000이하의 자연수)
      * @return 경우의 수를 1,000,000,007으로 나눈 나머지를 return
