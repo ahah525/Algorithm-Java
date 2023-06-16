3f5d45592c2fb0978f4e72681a16d46c518e75de
ahah525 <ahah525@naver.com>
2023-06-16 14:30:26 +0900

    181187-두 원 사이의 정수 쌍(1_v3.성공)

--- programmers/level2/Solution181187.java
+++ programmers/level2/Solution181187.java
@@ -9,7 +9,10 @@ package programmers.level2;
  * [풀이] r1, r2의 제곱값을 담은 변수를 int 로 선언함
  * 1_v2. 수학(성공)
  * [반례] r1, r2 제곱값은 최대 10^12 이므로 변수를 long 으로 선언함
+ * 1_v3. 수학(성공) -> 추천
+ * [풀이] 두 원 사이의 점 개수 = 1사분면 점 개수 * 4
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/181187">문제</a>
+ * @See <a href="https://zangsu.tistory.com/12">다른 풀이</a>
  */
 class Solution181187 {
     public static void main(String[] args) {
@@ -18,6 +21,11 @@ class Solution181187 {
     }
 
     // 1_v2
+    /**
+     * @param r1 작은 반지름
+     * @param r2 큰 반지름
+     * @return 두 원 사이의 공간에 x좌표와 y좌표가 모두 정수인 점의 개수
+     */
     public long solution(int r1, int r2) {
         long rr1 = (long) r1 * r1;
         long rr2 = (long) r2 * r2;
@@ -32,5 +40,18 @@ class Solution181187 {
             }
         }
         return answer;
+    }
+
+    // 1_v3
+    public long solution2(int r1, int r2) {
+        long rr1 = (long) r1 * r1;
+        long rr2 = (long) r2 * r2;
+        long answer = 0;
+        for(int x = 1; x <= r2; x++) {
+            int y1 = (int) Math.ceil(Math.sqrt(rr1 - (long) x * x));
+            int y2 = (int) Math.floor(Math.sqrt(rr2 - (long) x * x));
+            answer += (y2 - y1 + 1);
+        }
+        return answer * 4;
     }
 }
