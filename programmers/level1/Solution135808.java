c2b9dc1fb23211eddea8a21eaed7114024648044
ahah525 <ahah525@naver.com>
2023-06-19 21:56:38 +0900

    135808-과일 장수(2_v1.성공)

--- programmers/level1/Solution135808.java
+++ programmers/level1/Solution135808.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 
 /**
  * [문제명] 과일 장수
- * [풀이시간] 12분
- * [한줄평] 정렬하는 것만 떠올린다면 쉽게 풀 수 있는 문제였다.
- * v1. 정렬(성공)
+ * [풀이시간] 12분 / 6분
+ * [한줄평] 정렬하는 것만 떠올린다면 쉽게 풀 수 있는 문제였다. / 정렬을 하면 쉽게 풀 수 있는 문제였다.
+ * 1_v1. 정렬(성공)
+ * 2_v1. 정렬(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/135808">문제</a>
  */
 class Solution135808 {
@@ -21,6 +22,7 @@ class Solution135808 {
         System.out.println(solution(4, 3, score2));
     }
 
+    // 1_v1
     /**
      * @param k 사과의 최대 점수
      * @param m 한 상자에 들어가는 사과의 수
@@ -34,6 +36,18 @@ class Solution135808 {
 //        Arrays.sort(score, Collections.reverseOrder()); // 불가능
         for(int i = score.length - m; i >= 0; i-= m) {
             answer += score[i] * m;
+        }
+        return answer;
+    }
+
+    // 2_v1
+    public int solution2(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        int i = score.length - m;
+        while(i >= 0) {
+            answer += score[i] * m;
+            i -= m;
         }
         return answer;
     }
