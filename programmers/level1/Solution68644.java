7237b43eb21d5720a3506b9aa791ee8719491b6b
ahah525 <ahah525@naver.com>
2023-06-19 21:47:47 +0900

    68644-두 개 뽑아서 더하기(2_v1.성공)

--- programmers/level1/Solution68644.java
+++ programmers/level1/Solution68644.java
@@ -5,9 +5,10 @@ import java.util.*;
 
 /**
  * [문제명] 두 개 뽑아서 더하기
- * [풀이시간] 3분
- * [한줄평] 조합으로 풀 수 있는 대표적인 문제였고 너무 쉬워서 금방 풀 수 있었다.
- * v1. 조합(성공)
+ * [풀이시간] 3분 / 4분
+ * [한줄평] 조합으로 풀 수 있는 대표적인 문제였고 너무 쉬워서 금방 풀 수 있었다. / 더 이상 풀어볼 필요없는 쉬운 문제였다.
+ * 1_v1. 완전탐색(성공)
+ * 2_v1. 완전탐색(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/68644">문제</a>
  */
 class Solution68644 {
@@ -16,22 +17,26 @@ class Solution68644 {
         System.out.println();
     }
 
+    // 1_v1, 2_v1
     /**
      * @param numbers 정수 배열
      * @return 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return
      */
-    public static List<Integer> solution(int[] numbers) {
-        // 1. n 개에서 2개 뽑은 수의 합들의 모든 경우의 수 구하기
+    public int[] solution(int[] numbers) {
+        // 1. 서로 다른 두 수의 합 집합 구하기
         Set<Integer> set = new HashSet<>();
-        int n = numbers.length;
-        for(int i = 0; i < n - 1; i++) {
-            for(int j = i + 1; j < n; j++) {
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
                 set.add(numbers[i] + numbers[j]);
             }
         }
-        // set 을 list 로 변환하고 오름차순 정렬 후 반환
-        List<Integer> answer = new ArrayList<>(set);
-        Collections.sort(answer);
+        // 2. 집합을 배열로 변환, 오름차순 정렬
+        int[] answer = new int[set.size()];
+        int i = 0;
+        for(int n : set) {
+            answer[i++]  = n;
+        }
+        Arrays.sort(answer);
         return answer;
     }
 }
