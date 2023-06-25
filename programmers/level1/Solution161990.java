2c47c555db294be2d9779a78cf6e12c96169748b
ahah525 <ahah525@naver.com>
2023-06-25 21:49:31 +0900

    161990-바탕화면 정리(2_v1.성공)

--- programmers/level1/Solution161990.java
+++ programmers/level1/Solution161990.java
@@ -3,9 +3,10 @@ package programmers.level1;
 
 /**
  * [문제명] 바탕화면 정리
- * [풀이시간] 10분
- * [한줄평] 배열을 모두 탐색해서 풀 수 있는 쉬운 구현 문제였다.
- * 1_v1. 구현(성공)
+ * [풀이시간] 10분 / 7분
+ * [한줄평] 배열을 모두 탐색해서 풀 수 있는 쉬운 구현 문제였다. / 더 이상 풀어볼 필요는 없는 문제다.
+ * 1_v1. 완전탐색(성공)
+ * 2_v1. 완전탐색(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/161990">문제</a>
  */
 class Solution161990 {
@@ -14,6 +15,7 @@ class Solution161990 {
         System.out.println();
     }
 
+    // 1_v1, 2_v1
     /**
      * @param wallpaper 머쓱이의 컴퓨터 바탕화면의 상태를 나타내는 문자열 배열
      * @return 바탕화면의 파일들을 한 번에 삭제하기 위해 최소한의 이동거리를 갖는 드래그의 시작점과 끝점을 담은 정수 배열
