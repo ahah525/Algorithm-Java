befbab1660f25703d231c7906dbed6d3ccecc439
ahah525 <ahah525@naver.com>
2023-06-26 10:30:38 +0900

    152996-시소 짝꿍(2_v2.성공)

--- programmers/level2/Solution152996.java
+++ programmers/level2/Solution152996.java
@@ -8,13 +8,15 @@ import java.util.Map;
 
 /**
  * [문제명] 시소 짝꿍
- * [풀이시간] 34분(24분+10분) / 22분
+ * [풀이시간] 34분(24분+10분) / 24분(22분+2분)
  * [한줄평] 처음에는 전체에서 2명씩 뽑아서 경우의 수를 구하려다가 시간 초과가 날 것 같아서 Map으로 풀었던 문제다.
  * 1_v1. HashMap(실패 - 2~15 실패)
- * - 몸무게가 같은 사람들의 수를 생각하지 않고 몸무게 쌍으로만 경우의 수를 셈
  * 1_v2. HashMap(성공)
  * [반례] [100, 100, 100] >> 3
- * 2_v1. (실패 - 12~15 실패)
+ * [해결] 몸무게가 같은 사람들의 집단에서도 2명을 뽑는 경우의 수를 센다.
+ * 2_v1. HashMap(실패 - 12~15 실패)
+ * 2_v2. HashMap(성공)
+ * [해결] 전체 경우의 수(answer)은 long 타입이기 때문에 더해줄 때 long 으로 형변환 해주어야 한다.
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/152996">문제</a>
  */
 class Solution152996 {
@@ -23,33 +25,33 @@ class Solution152996 {
         System.out.println();
     }
 
-    // 1_v1
+    // 1_v2, 2_v2
     /**
      * @param weights 사람들의 몸무게 목록
      * @return 시소 짝꿍이 몇 쌍 존재하는지
      */
     public long solution(int[] weights) {
-        // 1. (몸무게, 사람수) 구하기
+        long answer = 0;
+        // 1. (몸무게, 사람수) 초기화
         Map<Integer, Integer> map = new HashMap<>();
-        for(int w: weights) {
+        for(int w : weights) {
             map.put(w, map.getOrDefault(w, 0) + 1);
         }
-        // 2. 몸무게 목록에서 2명씩 뽑는 경우의 수 계산
-        List<Integer> keys = new ArrayList<>(map.keySet());
-        int n = keys.size();
-        long answer = 0;
-        for(int i = 0; i < n; i++) {
-            int w1 = keys.get(i);
-            int cnt = map.get(w1);
-            // 3. 몸무게가 w1 인 그룹에서 2명 뽑기
-            if(cnt > 1 && isPossible(w1, w1)) {
+        // 2. 몸무게 리스트 초기화(중복X)
+        List<Integer> list = new ArrayList<>(map.keySet());
+        // 3. n개의 몸무게 리스트에서 순서 상관없이 2개 뽑기
+        for(int i = 0; i < list.size(); i++) {
+            int a = list.get(i);
+            // 4. 몸무게가 a인 집단에서 2개 뽑기
+            int cnt = map.get(a);
+            if(cnt > 1) {
                 answer += (long) cnt * (cnt - 1) / 2;
             }
-            // 4. 몸무게가 w1, w2인 그룹에서 각 1명씩 뽑기
-            for(int j = i + 1; j < n; j++) {
-                int w2 = keys.get(j);
-                if(isPossible(w1, w2)) {
-                    answer += (long) map.get(w1) * map.get(w2);
+            // 5. 몸무게가 a, b인 집단에서 1명씩 뽑기
+            for(int j = i + 1; j < list.size(); j++) {
+                int b = list.get(j);
+                if(isSame(a, b)) {
+                    answer += (long) map.get(a) * map.get(b);
                 }
             }
         }
@@ -57,46 +59,6 @@ class Solution152996 {
     }
 
     // 몸무게 a, b 가 짝꿍이 될 수 있는지 여부
-    public boolean isPossible(int a, int b) {
-        if(a == b) return true;
-        int min = Math.min(a, b);
-        int max = Math.max(a, b);
-        if(max * 2 == min * 3) return true;
-        if(max * 2 == min * 4) return true;
-        if(max * 3 == min * 4) return true;
-        return false;
-    }
-
-    // 2_v1
-    public long solution2(int[] weights) {
-        long answer = 0;
-        // (몸무게, 사람수)
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int w : weights) {
-            map.put(w, map.getOrDefault(w, 0) + 1);
-        }
-        // System.out.println(map);
-        List<Integer> list = new ArrayList<>(map.keySet());
-        // System.out.println(list);
-        for(int i = 0; i < list.size(); i++) {
-            int a = list.get(i);
-            for(int j = i + 1; j < list.size(); j++) {
-                int b = list.get(j);
-                if(isSame(a, b)) {
-                    answer += map.get(a) * map.get(b);
-                }
-            }
-        }
-        //
-        for(int w : list) {
-            int cnt = map.get(w);
-            if(cnt == 1) continue;
-            answer += cnt * (cnt - 1) / 2;
-        }
-
-        return answer;
-    }
-
     public boolean isSame(int a, int b) {
         int min = Math.min(a, b);
         int max = Math.max(a, b);
