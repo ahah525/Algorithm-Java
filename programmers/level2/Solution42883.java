16257ac65d430e511f43696015908746e1834f79
ahah525 <ahah525@naver.com>
2023-06-27 18:02:17 +0900

    42883-큰 수 만들기(3_v2.성공)

--- programmers/level2/Solution42883.java
+++ programmers/level2/Solution42883.java
@@ -5,10 +5,11 @@ import java.util.Stack;
 
 /**
  * [문제명] 큰 수 만들기
- * [풀이시간] 2시간 / 32분(30분 + 2분) / (13분)
+ * [풀이시간] 2시간 / 32분(30분 + 2분) / 18분(13분 + 5분)
  * [한줄평] stack 을 사용해야겠다는 것은 떠올렸는데 구현하는 과정에서 경우의 수를 처리하는 부분이 어려웠다.
  * 결국 풀이를 보고 해결했는데, 생각보다 너무 코드가 짧아서 놀랐고 꼭 다시 한번 풀어봐야할 문제인 것 같다. stack 이 아닌 for 문으로 해결한 풀이법도 있었다.
  * / 결국 stack 이라는 힌트를 보고 해결했던 문제였다.
+ * / 두 번째 풀 때와 마찬가지로 같은 반례를 해결하지 못해서 시간이 좀 걸렸다.
  * 1_v1. stack(실패)
  * - 경우에 따라서 스택에 선택적으로 넣고 빼는 방식으로 구현했다가 실패했다.
  * 1_v2. stack(성공)
@@ -23,8 +24,11 @@ import java.util.Stack;
  * 4. 스택에 있는 값들을 string 으로 변환
  * 2_v1. stack(실패 - 12 실패)
  * 2_v2. stack(성공)
- * [반례] "4321" 인 경우, stack 에 모두 push 되기때문에 k개 만큼 pop 해야한다.
+ * [반례] "4321" , 2 >> "43"
+ * [해결] k번 pop 되지 않은 경우 남은 횟수 만큼 pop 해야한다.
  * 3_v1. stack(실패 - 12 실패)
+ * 3_v2. stack(성공)
+ * [해결] 2_v2 반례와 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42883">문제</a>
  * @See <a href="https://school.programmers.co.kr/questions/25858">반례</a>
  * @See <a href="https://velog.io/@soo5717/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%ED%81%B0-%EC%88%98-%EB%A7%8C%EB%93%A4%EA%B8%B0-%ED%8C%8C%EC%9D%B4%EC%8D%AC">문제 힌트</a>
@@ -49,7 +53,7 @@ class Solution42883 {
         System.out.println(solution(number3, k3));
     }
 
-    // 1_v1
+    // 1_v2, 2_v2, 3_v2
     /**
      * @param number 2자리 이상, 1,000,000자리 이하인 숫자
      * @param k 제거할 수의 개수
@@ -71,35 +75,6 @@ class Solution42883 {
             stack.pop();
         }
         // 4. 스택에 있는 값들을 string 으로 변환
-        StringBuilder sb = new StringBuilder();
-        for(char c : stack) {
-            sb.append(c);
-        }
-        return sb.toString();
-    }
-
-    // 2_v1
-    public String solution2(String number, int k) {
-        Stack<Character> stack = new Stack<>();
-        for(char c : number.toCharArray()) {
-            if(stack.isEmpty() || k == 0) {
-                stack.push(c);
-            } else {
-                // c보다 작은 숫자 모두 제거
-                while(!stack.isEmpty() && k > 0) {
-                    if(stack.peek() < c) {
-                        stack.pop();
-                        k--;
-                    } else {
-                        break;
-                    }
-                }
-                stack.push(c);
-            }
-        }
-        for(int i = 0; i < k; i++) {
-            stack.pop();
-        }
         StringBuilder sb = new StringBuilder();
         for(char c : stack) {
             sb.append(c);
