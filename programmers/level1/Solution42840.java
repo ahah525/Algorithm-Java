c8371a422e9a3d104e48805def469ca69a15fe77
ahah525 <ahah525@naver.com>
2023-06-27 16:03:56 +0900

    42840-모의고사(3_v1.성공)

--- programmers/level1/Solution42840.java
+++ programmers/level1/Solution42840.java
@@ -6,10 +6,13 @@ import java.util.List;
 
 /**
  * [문제명] 모의고사
- * [풀이시간] 15분 / 20분
- * [한줄평] 있는 그대로 구현하면 쉽게 풀 수 있는 문제였다. / for문으로 모든 경우의 수를 탐색하면 해결되는 문제였다.
+ * [풀이시간] 15분 / 20분 / 11분
+ * [한줄평] 있는 그대로 구현하면 쉽게 풀 수 있는 문제였다.
+ * / for문으로 모든 경우의 수를 탐색하면 해결되는 문제였다.
+ * / 더 이상 풀어볼 필요가 없는 쉬운 문제였다.
  * 1_v1. 구현(성공)
  * 2_v1. 완전탐색(성공)
+ * 3_v1. 완전탐색(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42840">문제</a>
  */
 class Solution42840 {
@@ -23,7 +26,7 @@ class Solution42840 {
         System.out.println(solution(answers2));
     }
 
-    // 1_v1, 2_v1
+    // 1_v1, 2_v1, 3_v1
     /**
      * @param answers 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열
      * @return 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return(가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬)
