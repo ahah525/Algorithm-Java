6bc6cb54b678760cd4e907ba51113eb082340c82
ahah525 <ahah525@naver.com>
2023-06-29 10:31:04 +0900

    12934-정수 제곱근 판별(2_v1.성공)

--- programmers/level1/Solution12934.java
+++ programmers/level1/Solution12934.java
@@ -1,8 +1,13 @@
 package programmers.level1;
 
 /**
- * 정수 제곱근 판별
- * v1. Math.sqrt(), Math.pow() 사용
+ * [문제명] 정수 제곱근 판별
+ * [풀이시간] / 3분
+ * [한줄평] / 너무 기초적인 수학 문제였고 더 풀어볼 필요 없는 문제다.
+ * 1_v1. 수학(성공)
+ * [풀이] sqrt = (int) Math.sqrt(n), Math.pow(sqrt, 2) == n
+ * 2_v1. 수학(성공)
+ * [풀이] Math.sqrt(n) == (int) Math.sqrt(n)
  */
 class Solution12934 {
     public static void main(String[] args) {
@@ -17,6 +22,7 @@ class Solution12934 {
         System.out.println(answer2);
     }
 
+    // 1_v1
     /**
      * @param n 1이상, 50000000000000 이하인 양의 정수
      * @return
