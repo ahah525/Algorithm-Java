de7db5cfe0460b89b0efdf7ba3102874641b7cef
ahah525 <ahah525@naver.com>
2023-06-29 10:43:12 +0900

    12912-두 정수 사이의 합(2_v1.성공)

--- programmers/level1/Solution12912.java
+++ programmers/level1/Solution12912.java
@@ -1,11 +1,15 @@
 package programmers.level1;
 
 /**
- * 두 정수 사이의 합
- * v1. for 문 이용
- * v2. 등차수열 공식 이용(빠름)
+ * [문제명] 두 정수 사이의 합
+ * [풀이시간] / 2분
+ * [한줄평] / 너무 쉬운 문제로 더 풀어볼 필요는 없지만 등차수열 공식정도는 외워두면 좋을 것 같다.
+ * 1_v1. 구현(성공)
+ * [풀이] for문
+ * 1_v2. 수학(성공) -> 빠름
+ * [풀이] 등차수열 공식 이용
  * Sn = n * (A1 + An) / 2
- *
+ * 2_v1. 구현(성공)
  * @see <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12912/solution_groups?language=java">풀이 참고</a>
  * @See <a href="https://ko.wikihow.com/%EB%93%B1%EC%B0%A8%EC%88%98%EC%97%B4%EC%9D%98-%ED%95%A9%EC%9D%84-%EA%B5%AC%ED%95%98%EB%8A%94-%EB%B0%A9%EB%B2%95">등차수열의 합 구하는 방법</a>
  */
@@ -30,6 +34,7 @@ class Solution12912 {
         System.out.println(answer3);
     }
 
+    // 1_v1, 2_v1
     /**
      * for 문을 이용한 a ~ b 합 구하기
      * @param a -10,000,000 이상 10,000,000 이하인 정수
@@ -40,20 +45,17 @@ class Solution12912 {
         int min = Math.min(a, b);
         int max = Math.max(a, b);
         long answer = 0;
-
         for(int i = min; i <= max; i++) {
             answer += i;
         }
         return answer;
     }
 
-    /**
-     * 등차수열 공식을 이용한 a ~ b 합 구하기
-     */
+    // 2_v2
     public static long solution2(int a, int b) {
         int min = Math.min(a, b);
         int max = Math.max(a, b);
-
+        // 첫항~끝항 총합 = (항의 개수) * (첫항 + 끝항) / 2
         return (long)(max - min + 1) * (min + max) / 2;
     }
 }
