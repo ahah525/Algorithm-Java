f7bf5420bb2824fb289516ea69238e9adaa5659f
ahah525 <ahah525@naver.com>
2023-07-02 21:26:48 +0900

    178870-연속된 부분 수열의 합(3_v1.성공)

--- programmers/level2/Solution178870.java
+++ programmers/level2/Solution178870.java
@@ -3,12 +3,15 @@ package programmers.level2;
 
 /**
  * [문제명] 연속된 부분 수열의 합
- * [풀이시간] 21분 / 10분(9분 + 1분)
- * [한줄평] 투포인터 기초 문제였다. / 쉽게 풀었지만 복습해봐도 좋을 문제다.
+ * [풀이시간] 21분 / 10분(9분 + 1분) / 14분
+ * [한줄평] 투포인터 기초 문제였다.
+ * / 쉽게 풀었지만 복습해봐도 좋을 문제다.
+ * / 투포인터 기초 문제지만 오랜만에 풀어서 조건을 처리하는데 시간이 좀 걸렸다.
  * 1_v1. 투포인터(성공)
  * 2_v1. 투포인터(실패 - 1, 16, 25 실패)
  * 2_v2. 투포인터(성공)
  * [반례] 부분 수열의 최대 길이를 수열의 길이로 초기화해서 부분 수열의 길이와 수열의 길이가 같을 경우 [0, 0]이 반환되는 문제
+ * 3_v1. 투포인터(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/178870">문제</a>
  */
 class Solution178870 {
@@ -17,7 +20,7 @@ class Solution178870 {
         System.out.println();
     }
 
-    // 1_v1, 2_v2
+    // 1_v1, 2_v2, 3_v1
     /**
      * @param sequence 수열을 나타내는 정수 배열
      * @param k 부분 수열의 합을 나타내는 정수
@@ -47,6 +50,36 @@ class Solution178870 {
                 sum += sequence[r++];
             } else {
                 sum -= sequence[l++];
+            }
+        }
+        return answer;
+    }
+
+    // 3_v1
+    public int[] solution2(int[] sequence, int k) {
+        int[] answer = new int[2];
+        int n = sequence.length;
+        int l = 0;
+        int r = 0;
+        int sum = 0;    // 연속된 부분 수열의 합
+        int len = sequence.length + 1;  // 연속된 부분 수열의 길이
+        // 1. left 가 배열의 범위를 벗어나기 전까지 반복
+        while(l <= r) {
+            // System.out.println(left + "," + right);
+            if(sum < k) {
+                if(r == n) break;
+                sum += sequence[r++];
+            } else if(sum > k) {
+                sum -= sequence[l++];
+            } else {
+                // 더 길이가 짧은 수열일 때만 기록
+                if(len > r - l) {
+                    answer[0] = l;
+                    answer[1] = r - 1;
+                    len = r - l;
+                }
+                if(r == n) break;
+                sum += sequence[r++];
             }
         }
         return answer;
