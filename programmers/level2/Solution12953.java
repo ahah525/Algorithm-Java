5e31f23f60c873222ca8681ca984528adb1875af
ahah525 <ahah525@naver.com>
2023-07-03 10:10:19 +0900

    12953-N개의 최소공배수(3_v1.성공)

--- programmers/level2/Solution12953.java
+++ programmers/level2/Solution12953.java
@@ -5,15 +5,17 @@ import java.util.Arrays;
 
 /**
  * [문제명] N개의 최소공배수
- * [풀이시간] 25분 / 16분, 20분
+ * [풀이시간] 25분 / 16분, 20분 / 6분
  * [한줄평] 진짜 간단한 문제였는데, 알고리즘을 요즘 소홀히했던게 느껴졌던 문제였다.. 최대공약수 로직은 복습 좀 하자!!
  * / 완전 1차원적으로 풀었는데 통과가 되어서 놀랐다. 최대공약수 로직 복습하자
+ * / git
  * 1_v1. 수학(성공)
- * [접근법] 최소공배수, 최대공약수
  * 2_v1. 수학(성공)
- * [접근법] 단순 반복문
+ * [풀이] 단순 반복문
  * 2_v2. 수학(성공)
- * [접근법] A, B, C의 최소공배수 = (A, B의 최소공배수)와 C의 최소공배수
+ * [풀이] A, B, C의 최소공배수 = (A, B의 최소공배수)와 C의 최소공배수
+ * 3_v1. 수학(성공)
+ * [풀이] A, B 의 최대공약수 => A, B의 최솟값~1까지 나누는 것을 반복할 때 (A%i == 0 && B%i == 0)를 만족하는 i
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12953">문제</a>
  */
 class Solution12953 {
@@ -74,5 +76,26 @@ class Solution12953 {
             answer++;
         }
         return answer;
+    }
+
+    // 3_v1
+    public int solution3(int[] arr) {
+        int answer = arr[0];
+        for(int i = 1; i < arr.length; i++) {
+            answer = lcm2(answer, arr[i]);
+        }
+        return answer;
+    }
+
+    public int gcd2(int a, int b) {
+        int min = Math.min(a, b);
+        for(int i = min; i > 1; i--) {
+            if(a % i == 0 && b % i == 0) return i;
+        }
+        return 1;
+    }
+
+    public int lcm2(int a, int b) {
+        return a * b / gcd2(a, b);
     }
 }
