58a2bdccfabdd6a2be9149d9c205000bf61abfb4
ahah525 <ahah525@naver.com>
2023-07-04 09:27:59 +0900

    12911-다음 큰 숫자(3_v2.성공)

--- programmers/level2/Solution12911.java
+++ programmers/level2/Solution12911.java
@@ -3,14 +3,20 @@ package programmers.level2;
 
 /**
  * [문제명] 다음 큰 숫자
- * [풀이시간] 40분(30분 + 10분) / 8분, 5분
- * [한줄평] 처음에는 규칙을 찾아서 효율적으로 문제를 풀고자 했지만 결국에는 값을 하나씩 증가시키는 방법을 선택했다. 2진수에서 1의 개수를 셀 때 쓰는 메서드가 있다는 사실을 새롭게 알게 되었다.
- * 1_v1. Integer.toString(n, 2) 사용(실패 - 효율성 테스트 모두 시간초과)
- * [접근법] 1의 개수 = Integer.toString() 으로 2진수로 변환한 문자열의 길이 - replaceAll("1", "")로 변환한 문자열의 길이
- * 1_v2. Integer.bitCount() 사용(성공) -> 추천
- * [접근법] 1의 개수 = Integer.bitCount()
+ * [풀이시간] 40분(30분 + 10분) / 8분, 5분 / 15분(10분, 5분)
+ * [한줄평] / 처음에는 규칙을 찾아서 효율적으로 문제를 풀고자 했지만 결국에는 값을 하나씩 증가시키는 방법을 선택했다. 2진수에서 1의 개수를 셀 때 쓰는 메서드가 있다는 사실을 새롭게 알게 되었다.
+ * / Integer.bitCount() 함수의 존재를 잊고 2진수로 변환하고 replace()를 이용해 직접 1의 개수를 셌다가 시간초과가 됐다.
+ * 1_v1. (실패 - 효율성 테스트 모두 시간초과)
+ * [풀이] 1의 개수 = Integer.toString() 으로 2진수로 변환한 문자열의 길이 - replace("1", "")로 변환한 문자열의 길이
+ * 1_v2. (성공) -> 추천
+ * [풀이] 1의 개수 = Integer.bitCount()
  * 2_v1. (성공)
+ * [풀이] 1_v2 동일
  * 2_v2. 비트마스크(성공)
+ * 3_v1. (실패 - 효율성 5 시간초과)
+ * [풀이] 1_v1 동일
+ * 3_v2. (성공)
+ * [풀이] 1_v2 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12911">문제</a>
  * @See <a href="https://fbtmdwhd33.tistory.com/240">풀이 힌트</a>
  * @See <a href="https://advenoh.tistory.com/18">비트마스크 풀이</a>
@@ -63,5 +69,20 @@ class Solution12911 {
             n >>= 1;
         }
         return cnt;
+    }
+
+    // 3_v1
+    public int solution3(int n) {
+        int num = n + 1;
+        int origin = getOne(n);
+        while(true) {
+            if(origin == getOne(num)) return num;
+            num++;
+        }
+    }
+
+    public int getOne(int num) {
+        String s = Integer.toString(num, 2);
+        return s.length() - s.replace("1", "").length();
     }
 }
