d33ce3cb4de4ade9530b6a76a047856c54742e9a
ahah525 <ahah525@naver.com>
2023-07-12 10:53:20 +0900

    42747-H-Index(3_v2.성공)

--- programmers/level2/Solution42747.java
+++ programmers/level2/Solution42747.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 
 /**
  * [문제명] H-Index
- * [풀이시간] 10분 / 44분(34분 + 10분)
+ * [풀이시간] 10분 / 44분(34분 + 10분) / 40분(35분, 5분)
  * [한줄평] 그냥 구현만 하면 되는 굉장히 쉬운 문제였다. 다만 h 의 최댓값을 구하기 위해 어떤 값부터 탐색할 것인지를 잘 정해야 효율성을 높일 수 있다.
  * / 첫번째 풀이와 달리 정렬을 하고 풀었는데 로직을 짜는데 너무 시간이 오래걸렸다.
+ * / h 값을 citations 의 원소값으로만 설정하도록 로직을 짜서 반례를 찾지 못해 틀렸던 문제로 꼭 복습이 필요하다!
  * 1_v1. 구현(성공)
  * 최대 논문수 = n, 최대 논문 인용 횟수 = 배열의 최댓값
  * "h번 이상 인용된 논문이 h편 이상" 인 조건을 만족하는 h 는 최대 논문수와 최대 논문 인용 횟수의 최솟값 보다 큰 값일 수는 없다.
@@ -15,6 +16,10 @@ import java.util.Arrays;
  * 2_v1. 정렬(실패 - 9 실패)
  * 2_v2. 정렬(성공) -> 실패
  * [풀이] h의 최댓값 = Math.min(논문 개수, 논문 인용 횟수의 최댓값)
+ * 3_v1. 정렬(실패)
+ * [풀이] h의 값을 인용수 값으로만 설정함
+ * 3_v2. 정렬(성공)
+ * [풀이] 2_v2 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42747">문제</a>
  */
 class Solution42747 {
@@ -52,7 +57,7 @@ class Solution42747 {
         return 0;
     }
 
-    // 2_v2
+    // 2_v2, 3_v2
     public int solution2(int[] citations) {
         int n = citations.length;
         // 1. 오름차순 정렬
@@ -63,11 +68,9 @@ class Solution42747 {
         while(h >= 0) {
             // 논문의 인용 횟수가 h 이상인 논문의 수 세기
             int cnt = 0;
-            for(int i = 0; i < n; i++) {
-                if(citations[i] >= h) {
-                    cnt = n - i;
-                    break;
-                }
+            for(int i = n - 1; i >= 0; i--) {
+                if(citations[i] < h) break;
+                cnt++;
             }
             // 논문의 인용 횟수가 h 이상인 논문의 수가 h 이상이면
             if(h <= cnt) return h;
