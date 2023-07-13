6c8547188b09965c4d4d53ec8b7d4d2863fc4293
ahah525 <ahah525@naver.com>
2023-07-13 15:14:06 +0900

    42860-조이스틱(3_v2.성공)

--- programmers/level2/Solution42860.java
+++ programmers/level2/Solution42860.java
@@ -3,13 +3,16 @@ package programmers.level2;
 
 /**
  * [문제명] 조이스틱
- * [풀이시간] 1시간 50분(50분 + 1시간) / 1시간 10분 / (38분)
+ * [풀이시간] 1시간 50분(50분 + 1시간) / 1시간 10분 / 43분(38분 + 5분)
  * [한줄평] 문제 풀이를 이해하는데에도 너무 어려웠던 문제였다.
  * / 결국 또 풀이를 보고 해결했고 다시 풀어도 너무 어려웠다.
  * 1_v1. (실패 - 11,13~14,18,20,22~27 실패)
  * 1_v2. (성공)
  * 2_v1. (성공)
  * 3_v1. (실패 - 17 실패)
+ * [반례] "AAA" >> 0
+ * [해결] 최솟값을 (문자열 길이 - 1)이 아니라 (가장 마지막에 나타난 'A'가 아닌 문자의 인덱스)로 초기화한다.
+ * 3_v2. (성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42860">문제</a>
  * @See <a href="https://born2bedeveloper.tistory.com/26">풀이 참고</a>
  */
@@ -52,16 +55,18 @@ class Solution42860 {
     }
 
     // 3_v1
-    public int solution3(String name) {
+    public int solution2(String name) {
         int answer = 0;
-        // 1.
-        for(char c : name.toCharArray()) {
-            if(c == 'A') continue;
-            answer += moveUpAndDown(c);
+        int n = name.length();
+        // 1. 문자열에서 'A'가 아닌 문자의 가장 마지막 인덱스 구하기
+        int end = 0;
+        for(int i = 0; i < n; i++) {
+            if(name.charAt(i) == 'A') continue;
+            answer += moveUpAndDown(name.charAt(i));
+            end = i;
         }
         //
-        int n = name.length();
-        int min = n - 1;
+        int min = end;
         for(int i = 0; i < n - 1; i++) {
             if(name.charAt(i) != 'A' && name.charAt(i + 1) == 'A') {
                 // 연속된 A의 다음 인덱스
