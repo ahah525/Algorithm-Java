6570e8ce7b5b7989c07542ece90c3083f1c8a016
ahah525 <ahah525@naver.com>
2023-07-14 22:44:39 +0900

    154538-숫자 변환하기(3_v2.성공)

--- programmers/level2/Solution154538.java
+++ programmers/level2/Solution154538.java
@@ -5,14 +5,17 @@ import java.util.Arrays;
 
 /**
  * [문제명] 숫자 변환하기
- * [풀이시간] 16분 / 11분
+ * [풀이시간] 16분 / 11분 / 19분(14분+5분)
  * [한줄평] 전형적인 DP 문제여서 쉽게 풀 수 있었다.
+ * /
+ * / 쉬운 문제였는데 점화식을 잘못 세워서 틀렸다. 다음에 다시 한 번 풀어봐야겠다.
  * 1_v1. DP(성공)
  * - d[i]: x 에서 i 를 만들기 위한 최소 연산 횟수
  * [점화식] d[i] = Math.min(d[i], d[i - n], d[i / 2], d[i / 3])
  * 2_v1. DP(성공) -> 빠름
  * 2_v2. DP(성공)
  * 3_v1. DP(실패 - 1 실패)
+ * 3_v2. DP(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/154538">문제</a>
  */
 class Solution154538 {
@@ -21,7 +24,7 @@ class Solution154538 {
         System.out.println();
     }
 
-    // 1_v1, 2_v1
+    // 1_v1, 2_v1, 3_v1
     /**
      * @param x 1 ≤ x ≤ y ≤ 1,000,000
      * @param y 1 ≤ x ≤ y ≤ 1,000,000
@@ -38,14 +41,11 @@ class Solution154538 {
         // 3. x 에서 (x + 1)...(y) 까지 만드는 최소비용 계산
         for(int i = x + 1; i <= y; i++) {
             // 1. n 더하기
-            if(i - n >= x)
-                d[i] = Math.min(d[i], d[i - n] + 1);
+            if(i - n >= x) d[i] = Math.min(d[i], d[i - n] + 1);
             // 2. 2 곱하기
-            if(i % 2 == 0)
-                d[i] = Math.min(d[i], d[i / 2] + 1);
+            if(i % 2 == 0) d[i] = Math.min(d[i], d[i / 2] + 1);
             // 3. 3 곱하기
-            if(i % 3 == 0)
-                d[i] = Math.min(d[i], d[i / 3] + 1);
+            if(i % 3 == 0) d[i] = Math.min(d[i], d[i / 3] + 1);
         }
         return d[y] == INF ? -1 : d[y];
     }
