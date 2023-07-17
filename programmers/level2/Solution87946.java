e0d48ddd080d8af0f93770ad63e7d18a261379ea
ahah525 <ahah525@naver.com>
2023-07-17 22:33:43 +0900

    87946-피로도(3_v1.성공)

--- programmers/level2/Solution87946.java
+++ programmers/level2/Solution87946.java
@@ -3,12 +3,16 @@ package programmers.level2;
 
 /**
  * [문제명] 피로도
- * [풀이시간] 30분 / 16분
- * [한줄평] 입력예시를 보고 완전탐색으로 풀어야겠다는 생각이 들어서 순열을 재귀로 구현하여 문제를 풀었다. / 쉽게 풀 수 있는 기초 문제였다.
+ * [풀이시간] 30분 / 16분 / 18분
+ * [한줄평] 입력예시를 보고 완전탐색으로 풀어야겠다는 생각이 들어서 순열을 재귀로 구현하여 문제를 풀었다.
+ * / 쉽게 풀 수 있는 기초 문제였다.
+ * / 다음에 한 번 더 풀어보면 좋을 문제다.
  * 1_v1. DFS(성공)
  * - 유저가 탐험할 수 있는 최대 던전수 = n 개에서 r 개를 뽑아 나열할 수 있는 경우의 수들 중 r의 최댓값
  * - r 의 최댓값 = n, 최솟값 = 1 -> 최댓값(n)부터 최솟값(1)까지 for 문으로 탐색하기
  * 2_v1. 완전탐색, DFS(성공) -> 빠름
+ * 3_v1. 완전탐색, DFS(성공)
+ * [풀이] 1_v1 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/87946">문제</a>
  */
 class Solution87946 {
@@ -18,7 +22,9 @@ class Solution87946 {
         System.out.println(solution(80, dungeons));
     }
 
+    // 1_v1, 3_v1
     static boolean isFind = false;  // n 개에서 r 개를 뽑아 나열할 수 있는 경우의 수를 찾았는지 여부를 나타내는 플래그
+    static int n;
 
     /**
      * @param k 유저의 현재 피로도
@@ -26,14 +32,11 @@ class Solution87946 {
      * @return 유저가 탐험할수 있는 최대 던전 수
      */
     public static int solution(int k, int[][] dungeons) {
-        int n = dungeons.length;
+        n = dungeons.length;
         // 1. n 개에서 r 개를 뽑아 나열할 수 있는 경우의 수를 최초로 찾은 r 값 구하기
-        for(int r = n; r >= 1; r--) {
-            boolean[] visited = new boolean[n];
-            perm(0, visited, k, n, r, dungeons);
-            // 플래그값이 true 면 n 개에서 r 개를 뽑아 나열하는 경우의 수를 찾았으므로 바로 r 리턴
-            if(isFind)
-                return r;
+        int r = n;
+        while(!isFind) {
+            perm(0, k, r--, new boolean[n], dungeons);
         }
         return 0;
     }
@@ -42,12 +45,11 @@ class Solution87946 {
      * n 개에서 r 개를 뽑아 나열하는 경우의 수 찾기
      * @param depth 방문한 던전 개수(깊이)
      * @param visited 던전 방문여부
-     * @param k 유저의 현재 피로도
-     * @param n 던전 개수
+     * @param power 유저의 현재 피로도
      * @param r 방문해야하는 던전 개수
      * @param dungeons 각 던전별 "최소 필요 피로도", "소모 피로도"가 담긴 2차원 배열
      */
-    public static void perm(int depth, boolean[] visited, int k, int n, int r, int[][] dungeons) {
+    public static void perm(int depth, int power, int r, boolean[] visited, int[][] dungeons) {
         // 1. 경우의 수를 찾았으면 바로 종료
         if(isFind) return;
         // 2. n 개에서 r 개를 뽑아 나열하는 경우의 수를 찾았으면, 플래그값 true 로 바꾸고 종료
@@ -57,11 +59,10 @@ class Solution87946 {
         }
         // 3. 아직 방문하지 않았고 유저의 현재 피로도가 던전의 최소 필요 피로도이상의 값인 던전 방문처리
         for(int i = 0; i < n; i++) {
-            if(!visited[i] && k >= dungeons[i][0]) {
-                visited[i] = true;
-                perm(depth + 1, visited, k - dungeons[i][1], n, r, dungeons);
-                visited[i] = false;
-            }
+            if(visited[i] || power < dungeons[i][0]) continue;
+            visited[i] = true;
+            perm(depth + 1, power - dungeons[i][1], r, visited, dungeons);
+            visited[i] = false;
         }
     }
 
