1baf965281e2d2379e79984fce2ace138ce6f575
ahah525 <ahah525@naver.com>
2023-07-18 11:07:21 +0900

    42862-체육복(3_v1.성공)

--- programmers/level1/Solution42862.java
+++ programmers/level1/Solution42862.java
@@ -1,21 +1,25 @@
 package programmers.level1;
 
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * [문제명] 체육복
  * [풀이시간] 37분(20분 + 15분 + 2분) / 50분(14분 + 36분)
  * [한줄평] 조건(여벌 체육복을 가져온 학생이 체육복을 도난당했을 수 있습니다)을 잘 신경쓰지 않아서 푸는데 너무 오래걸렸던 문제였다.
  * / 반례를 처리하는데 너무 오랜 시간을 쏟았던 문제다.
+ * /
  * 1_v1. 그리디(실패 - 11, 13, 14 실패)
+ * [해결] 입력이 오름차순 정렬된 상태라는 보장이 없음 -> Arrays.sort() 추가
  * 1_v2. 그리디(실패 - 11 실패)
- * - 입력이 오름차순 정렬된 상태라는 보장이 없음 -> Arrays.sort() 추가
+ * [해결] reserves[] 배열의 마지막 인덱스가 (n - 1) 이 아닌 n 이라는 것을 간과함 -> 마지막
  * 1_v3. 그리디(성공)
- * - reserves[] 배열의 마지막 인덱스가 (n - 1) 이 아닌 n 이라는 것을 간과함 -> 마지막
  * 2_v1. 그리디(실패 - 5, 24 실패)
+ * [해결] 본인 체육복을 도난 당한 사람은 다른 사람에게 빌려줄 수 없음!
  * 2_v2. 그리디(성공)
- * [해결법] 본인 체육복을 도난 당한 사람은 다른 사람에게 빌려줄 수 없음!!
+ * [풀이] boolean[] 에 체육복이 있는지 없는지 여부 저장
+ * 3_v1. 그리디(성공) -> 빠름
+ * [풀이] HashSet 에 체육복이 없는 학생들 저장
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42862">문제</a>
  * @See <a href="https://school.programmers.co.kr/questions/37331">반례</a>
  */
@@ -104,5 +108,38 @@ class Solution42862 {
             }
         }
         return answer;
+    }
+
+    // 3_v1
+    public int solution3(int n, int[] lost, int[] reserve) {
+        // 1. 도난당한 학생 집합 초기화
+        Set<Integer> losts = new HashSet<>();
+        for(int num : lost) {
+            losts.add(num);
+        }
+        // 2. 다른 학생에게 체육복을 빌려줄 수 있는 학생 리스트 구하기
+        List<Integer> reserves = new ArrayList<>();
+        for(int num : reserve) {
+            // 3. 본인의 체육복이 도난 당했으면, 본인 우선 사용(다른 학생에게 빌려줄 수 없음)
+            if(losts.contains(num)) {
+                losts.remove(num);
+                continue;
+            }
+            reserves.add(num);
+        }
+        // 3. 오름차순한 학생들이 다른 학생에게 빌려줄 수 있는지 검사
+        Collections.sort(reserves);
+        for(int num : reserves) {
+            // 4. 앞 사람에게 빌려주기
+            if (num > 1 && losts.contains(num - 1)) {
+                losts.remove(num - 1);
+                continue;
+            }
+            // 5. 뒷 사람에게 빌려주기
+            if (num + 1 <= n && losts.contains(num + 1)) {
+                losts.remove(num + 1);
+            }
+        }
+        return n - losts.size();
     }
 }
