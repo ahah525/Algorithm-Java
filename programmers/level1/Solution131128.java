2437675760a8b60abdb755a8f66fa5f7c6d65dbb
ahah525 <ahah525@naver.com>
2023-07-24 11:11:33 +0900

    131128-숫자 짝꿍(3_v1.성공)

--- programmers/level1/Solution131128.java
+++ programmers/level1/Solution131128.java
@@ -5,13 +5,17 @@ import java.util.*;
 
 /**
  * [문제명] 숫자 짝꿍
- * [풀이시간] 20분 / 17분
- * [한줄평] Map 으로 풀었는데, 다른 풀이를 보니 배열로 풀었어도 쉬웠을 것 같다. / Map 으로 풀까하다가 배열로 풀면 더 간단할 것 같아 배열로 풀었는데, 0 반례를 처리하는데 시간이 좀 걸렸다.
+ * [풀이시간] 20분 / 17분 / 10분
+ * [한줄평] Map 으로 풀었는데, 다른 풀이를 보니 배열로 풀었어도 쉬웠을 것 같다.
+ * / Map 으로 풀까하다가 배열로 풀면 더 간단할 것 같아 배열로 풀었는데, 0 반례를 처리하는데 시간이 좀 걸렸다.
+ * / 쉽게 풀 수 있는 문제로 더 이상 안풀어봐도 될 문제다.
  * 1_v1. (성공)
- * [접근법] Map에 X,Y의 (숫자, 개수) 저장
+ * [풀이] Map에 X,Y의 (숫자, 개수) 저장
  * 2_v1. (성공) -> 빠름
- * [접근법] 배열에 X,Y의 숫자 개수 저장
+ * [풀이] 배열에 X,Y의 숫자 개수 저장
  * [반례] "0000" -> "0" 반환
+ * 3_v1. 문자열(성공)
+ * [풀이] HashMap에 X,Y의 (문자, 개수) 저장, StringBuilder 에 제일 큰 문자부터 추가
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/131128">문제</a>
  */
 class Solution131128 {
@@ -79,6 +83,30 @@ class Solution131128 {
         }
         if(sb.length() == 0) return "-1";
         // 0 여러개 값에 대한 처리
+        if(sb.charAt(0) == '0') return "0";
+        return sb.toString();
+    }
+
+    // 3_v1
+    public String solution3(String X, String Y) {
+        Map<Character, Integer> mapX = new HashMap<>();
+        Map<Character, Integer> mapY = new HashMap<>();
+        for(char c : X.toCharArray()) {
+            mapX.put(c, mapX.getOrDefault(c, 0) + 1);
+        }
+        for(char c : Y.toCharArray()) {
+            mapY.put(c, mapY.getOrDefault(c, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(char c = '9'; c >= '0'; c--) {
+            if(!mapX.containsKey(c) || !mapY.containsKey(c)) continue;
+            int min = Math.min(mapX.get(c), mapY.get(c));
+            for(int i = 0; i < min; i++) {
+                sb.append(c);
+            }
+        }
+        if(sb.length() == 0) return "-1";
         if(sb.charAt(0) == '0') return "0";
         return sb.toString();
     }
