c937d8e7e76cf529c10e6203d190401fda49b954
ahah525 <ahah525@naver.com>
2023-07-26 10:39:13 +0900

    131130-혼자 놀기의 달인(3_v1.성공)

--- programmers/level2/Solution131130.java
+++ programmers/level2/Solution131130.java
@@ -5,15 +5,18 @@ import java.util.*;
 
 /**
  * [문제명] 혼자 놀기의 달인
- * [풀이시간] 18분(16분 + 2분) / 15분
+ * [풀이시간] 18분(16분 + 2분) / 15분 / 16분
  * [한줄평] 이 문제는 숫자를 뽑는 순서는 중요하지 않고 단순히 숫자를 그룹핑하기만 하면 쉽게 풀 수 있었다.
  * / 반복문으로 푸는 것이 빠르긴 했지만 재귀로도 풀 수 있는 문제였다.
+ * / 쉬운 문제였지만 다시 풀어봐도 좋을 문제다.
  * 1_v1. DFS(실패 - 2 런타임 에러)
  * 1_v2. DFS(성공) -> 빠름
  * [풀이] 반복문
  * [반례] 1번 상자 그룹을 제외하고 남는 상자가 없으면 이때 획득하는 점수는 0점 / [2, 1] => 0
  * 2_v1. DFS(성공)
  * [풀이] 재귀
+ * 3_v1. DFS(성공)
+ * [풀이] 2_v1 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/131130">문제</a>
  */
 class Solution131130 {
@@ -70,4 +73,29 @@ class Solution131130 {
         visited[n] = true;
         dfs(cards[n] - 1, cards);
     }
+
+    // 3_v1
+//    boolean[] visited;
+//    Queue<Integer> pq;
+//    public int solution(int[] cards) {
+//        visited = new boolean[cards.length];
+//        pq = new PriorityQueue<>(Collections.reverseOrder());
+//        for(int i = 0; i < cards.length; i++) {
+//            if(visited[i]) continue;
+//            dfs(i, 0, cards);
+//        }
+//        // System.out.println(pq);
+//        if(pq.size() < 2) return 0;
+//        return pq.poll() * pq.poll();
+//    }
+//
+//    public void dfs(int now, int cnt, int[] cards) {
+//        if(visited[now]) {
+//            pq.add(cnt);
+//            return;
+//        }
+//        //
+//        visited[now] = true;
+//        dfs(cards[now] - 1, cnt + 1, cards);
+//    }
 }
