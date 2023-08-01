14ccf5714c6f16f562393a76879201483d179347
ahah525 <ahah525@naver.com>
2023-08-01 17:12:43 +0900

    152995-인사고과(2_v2.성공)

--- programmers/level3/Solution152995.java
+++ programmers/level3/Solution152995.java
@@ -5,12 +5,16 @@ import java.util.*;
 
 /**
  * [문제명] 인사고과
- * [풀이시간] 1시간 45분(33분+1시간 12분) / (50분)
+ * [풀이시간] 1시간 45분(33분+1시간 12분) / 1시간(50분+10분)
  * [한줄평] 결국 풀이를 보고 해결했고 인센티브를 받을 수 있는지 여부를 확인하기 위한 로직을 이해하기가 어려웠기 떄문에 꼭 다시 풀어봐야겠다.
+ * / 정렬을 해야겠다고는 쉽게 떠올렸는데 막상 그것을 구현하기가 조금 어려웠던 문제로 꼭 다시 풀어봐야할 문제다.
  * 1_v1. (실패 - 3,8,10~11,14,16,22)
- * 2_v1. 정렬(성공)
+ * 1_v2. 정렬(성공) -> 빠름
  * [풀이] 근무 태도 점수를 내림차순 정렬, 동료 평가 점수를 오름차순 정렬하여 인센티브를 받을 수 있는지 여부를 확인한다.
- * 3_v1. (실패 - 11 실패)
+ * 2_v1. 정렬(실패 - 11 실패)
+ * [해결] 점수의 합이
+ * 2_v2. 정렬(성공)
+ * [풀이] 정렬은 1_v1과 동일, 등수를 구하기 위해서 PriorityQueue에 유효한 점수를 저장하고 두 점수의 합 내림차순 정렬했다.
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/152995">문제</a>
  * @See <a href="https://velog.io/@qodlstjd12/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EC%9D%B8%EC%82%AC%EA%B3%A0%EA%B3%BC-java">풀이 참고</a>
  */
@@ -54,36 +58,42 @@ class Solution152995 {
         return answer;
     }
 
-    // 3_v1
+    // 2_v2
     public int solution2(int[][] scores) {
-        int answer = 1;
-        int[] target = new int[] {scores[0][0], scores[0][1]};
-        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> (o2[0] + o2[1]) - (o1[0] + o1[1]));
-        //
+        // 1. 완호의 근무태도 점수, 동료평가 점수, 두 점수의 합 초기화
+        int targetA = scores[0][0];
+        int targetB = scores[0][1];
+        int targetSum = targetA + targetB;
+        // 2. 근무태도 내림차순, 동료평가 오름차순 정렬
         Arrays.sort(scores, (o1, o2) -> {
             if(o1[0] == o2[0]) return o1[1] - o2[1];
             return o2[0] - o1[0];
         });
-        // 이전 근무태도 점수
-        // int prevA = score[0][0];
-        boolean start = true;
-        //
-        int maxB = -1;
+        int maxB = -1; // 동료평가 점수의 최댓값
         int i = 1;
+        // 3. 두 점수의 합 내림차순 정렬
+        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> (o2[0] + o2[1]) - (o1[0] + o1[1]));
+        // 4. 첫번째 값은 무조건 넣기
         pq.add(new int[] {scores[0][0], scores[0][1]});
         while(i < scores.length) {
+            // 5. 이전 근무태도 점수와 현재 근무태도 점수가 다르다면, 동료평가 점수 최댓값 갱신
             if(scores[i] != scores[i - 1]) {
                 maxB = Math.max(maxB, scores[i - 1][1]);
             }
             if(maxB <= scores[i][1]) {
+                // 6. 현재 동료평가 점수 >= 동료평가 점수 최댓값, 인센티브를 받을 수 있으므로 큐에 넣기
                 pq.add(new int[] {scores[i][0], scores[i][1]});
+            } else {
+                // 7. 현재 동료평가 점수 < 동료평가 점수 최댓값, 인센티브 못받음 -> 그 사람이 완호라면 -1 리턴
+                if(scores[i][0] == targetA && scores[i][1] == targetB) return -1;
             }
             i++;
         }
-
+        // 8. 완호의 등수 계산
+        int answer = 1;
         while(!pq.isEmpty()) {
             int[] score = pq.poll();
-            if(score[0] == target[0] && score[1] == target[1]) return answer;
+            if(score[0] + score[1] == targetSum) return answer;
             answer++;
         }
         return -1;
