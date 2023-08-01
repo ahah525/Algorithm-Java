20258f4d0494e1e53347fae5e95cd5cd6e76e794
ahah525 <ahah525@naver.com>
2023-08-01 18:21:52 +0900

    390-징검다리(1_v2.실패)

--- softeer/level3/Solution390.java
+++ softeer/level3/Solution390.java
@@ -8,13 +8,16 @@ import java.util.StringTokenizer;
 
 /**
  * [문제명] 징검다리
- * [풀이시간] (10분)
+ * [풀이시간] (10분+20분)
  * [한줄평]
  * 1_v1. (실패)
+ * 1_v2. DFS, 완전탐색(실패 - 시간초과)
  * @See <a href="https://softeer.ai/practice/info.do?idx=1&eid=390">문제</a>
  */
 class Solution390 {
-    // 1_v1
+    // 1_v2
+    static int max;
+    // static int[] path;
     public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -24,17 +27,24 @@ class Solution390 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         //
-        int max = 1;
-        int[] dp = new int[n];
-        dp[n - 1] = 1;
-        for(int i = n - 2; i >= 0; i--) {
-            if(arr[i] < arr[i + 1]) {
-                dp[i] = dp[i + 1] + 1;
-                max = Math.max(max, dp[i]);
-            } else {
-                dp[i] = 1;
+        max = 1;
+        // path = new int[n];
+        // Arrays.fill(path, -1);
+        dfs(0, -1, 0, n, arr);
+
+        System.out.println(max);
+    }
+
+    public static void dfs(int depth, int prevIdx, int prev, int n, int[] arr) {
+        // System.out.println(Arrays.toString(path));
+        max = Math.max(max, depth);
+        if(prevIdx == n - 1) return;
+        for(int i = prevIdx + 1; i < n; i++) {
+            if(prev < arr[i]) {
+                // path[depth] = i;
+                dfs(depth + 1, i, arr[i], n, arr);
+                // path[depth] = -1;
             }
         }
-        System.out.println(max);
     }
 }
