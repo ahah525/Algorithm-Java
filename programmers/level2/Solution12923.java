f438244bd4cb70b84c8a143f520fb94b27ae03f0
ahah525 <ahah525@naver.com>
2023-08-04 14:35:45 +0900

    12923-숫자 블록(2_v1.성공)

--- programmers/level2/Solution12923.java
+++ programmers/level2/Solution12923.java
@@ -6,10 +6,11 @@ import java.util.List;
 
 /**
  * [문제명] 숫자 블록
- * [풀이시간] 1시간 10분(17분 + 53분)
+ * [풀이시간] 1시간 10분(17분 + 53분) / (43분)
  * [한줄평] 반례를 찾기가 너무 어려웠고 결국 풀이를 보고 해결했던 문제다.
  * 1_v1. (실패 - 정확성 1~14 실패, 효율성 1~6 실패)
  * 1_v2. (성공)
+ * 2_v1. 구현(실패 - 정확성 13 실패, 효율성 1~6 실패)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12923">문제</a>
  * @See <a href="https://ondol-diary.tistory.com/43">풀이 참고</a>
  */
@@ -50,5 +51,24 @@ class Solution12923 {
             }
         }
         return list.size() == 0 ? 1 : list.get(list.size() - 1);
+    }
+
+    // 2_v1
+    public int[] solution2(long begin, long end) {
+        int b = (int) begin;
+        int e = (int) end;
+        int n = e - b + 1;
+        int[] answer = new int[n];
+        for(int i = 1; i <= e / 2; i++) {
+            int s = Math.max(2, (int) Math.ceil((double) b / i));
+            // System.out.println(s);
+            while(true) {
+                if(s * i > e) break;
+                answer[s * i - b] = i;
+                s++;
+            }
+            // System.out.println(Arrays.toString(answer));
+        }
+        return answer;
     }
 }
