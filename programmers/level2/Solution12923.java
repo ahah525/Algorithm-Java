6c49efb38c17049d2cce89042ee9a822f8c63e6d
ahah525 <ahah525@naver.com>
2023-08-04 14:57:36 +0900

    12923-숫자 블록(2_v2.성공)

--- programmers/level2/Solution12923.java
+++ programmers/level2/Solution12923.java
@@ -6,11 +6,14 @@ import java.util.List;
 
 /**
  * [문제명] 숫자 블록
- * [풀이시간] 1시간 10분(17분 + 53분) / (43분)
+ * [풀이시간] 1시간 10분(17분 + 53분) / 1시간(43분+17분)
  * [한줄평] 반례를 찾기가 너무 어려웠고 결국 풀이를 보고 해결했던 문제다.
+ * / 반례를 찾지 못해서 결국 풀이를 보고 해결했고 꼭 다시 풀어봐야할 문제다.
  * 1_v1. (실패 - 정확성 1~14 실패, 효율성 1~6 실패)
  * 1_v2. (성공)
  * 2_v1. 구현(실패 - 정확성 13 실패, 효율성 1~6 실패)
+ * [해결] n의 최댓값이 10,000,000이라는 사실을 고려함
+ * 2_v2. 구현(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12923">문제</a>
  * @See <a href="https://ondol-diary.tistory.com/43">풀이 참고</a>
  */
@@ -53,22 +56,28 @@ class Solution12923 {
         return list.size() == 0 ? 1 : list.get(list.size() - 1);
     }
 
-    // 2_v1
+    // 2_v2
     public int[] solution2(long begin, long end) {
         int b = (int) begin;
         int e = (int) end;
         int n = e - b + 1;
         int[] answer = new int[n];
-        for(int i = 1; i <= e / 2; i++) {
-            int s = Math.max(2, (int) Math.ceil((double) b / i));
-            // System.out.println(s);
-            while(true) {
-                if(s * i > e) break;
-                answer[s * i - b] = i;
-                s++;
-            }
-            // System.out.println(Arrays.toString(answer));
+        for(int i = 0; i < n; i++) {
+            answer[i] = calc(b + i);
         }
         return answer;
+    }
+
+    // n의 약수 중 본인을 제외한 최댓값(x) 구하기(x < 10,000,000)
+    public int calc(int n) {
+        if(n == 1) return 0;
+        int max = -1;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                if(n / i <= MAX) return n / i;
+                max = i;
+            }
+        }
+        return (max == -1) ? 1 : max;
     }
 }
