1c3808c5364dbed11faed2a446aa94bc773e5952
ahah525 <ahah525@naver.com>
2023-08-08 10:44:59 +0900

    92344-파괴되지 않은 건물(2_v1.성공)

--- programmers/level3/Solution92344.java
+++ programmers/level3/Solution92344.java
@@ -3,13 +3,16 @@ package programmers.level3;
 
 /**
  * [문제명] 파괴되지 않은 건물
- * [풀이시간] 1시간(9분 + 51분)
+ * [풀이시간] 1시간(9분 + 51분) / 30분
  * [한줄평] 풀이를 이해하는 것도 너무 어려웠던 문제였다.
+ * / 결국 또 풀이를 보고 이해했던 문제로 꼭 복습이 필요하다.
  * 1_v1. 구현(실패 - 효울성 테스트 1~7 실패)
  * [시간복잡도] O(N * M * K) -> 시간초과
  * 1_v2. 누적합(성공)
  * [시간복잡도] O(K + N * M)
- * [접근법] 원본 배열 + 누적합 배열 = 결과 배열 -> 누적합 배열을 구하는 것이 핵심!
+ * [풀이] 원본 배열 + 누적합 배열 = 결과 배열 -> 누적합 배열을 구하는 것이 핵심!
+ * 2_v1. 누적합(성공)
+ * [풀이] 1_v2 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/92344">문제</a>
  * @See <a href="https://tech.kakao.com/2022/01/14/2022-kakao-recruitment-round-1/">풀이 참고</a>
  */
@@ -19,7 +22,7 @@ class Solution92344 {
         System.out.println();
     }
 
-    // 1_v2
+    // 1_v2, 2_v1
     /**
      * @param board 건물의 내구도를 나타내는 2차원 정수 배열
      * @param skill 적의 공격 혹은 아군의 회복 스킬을 나타내는 2차원 정수 배열
@@ -56,7 +59,7 @@ class Solution92344 {
                 sum[i][j] += sum[i - 1][j];
             }
         }
-        // 4.
+        // 4. board[i][j] + sum[i][j] = k번의 공격/회복을 거친 후 결과값
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
                 if(board[i][j] + sum[i][j] >= 1) answer++;
