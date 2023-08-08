b9d35b576f713d2d18ee5e8307752e585a77c39f
ahah525 <ahah525@naver.com>
2023-08-08 17:18:14 +0900

    12921-소수 찾기(2_v1.성공)

--- programmers/level1/Solution12921.java
+++ programmers/level1/Solution12921.java
@@ -3,11 +3,15 @@ package programmers.level1;
 
 /**
  * [문제명] 소수 찾기
- * [풀이시간] 5분
+ * [풀이시간] 5분 / 5분
  * [한줄평] 아주 쉬운 문제였고 나누는 수를 어디까지 탐색하는지가 효율성 테스트를 통과할 수 있는 방법이다.
- * v1. (성공)
- * - 소수를 찾을 때는 (2 ~ 루트 n) 범위까지 탐색해야 효율적이다.
+ * / 에라토스테네스의 체로 소수를 구하는 방법에 대해 복습할 수 있었던 문제다.
+ * 1_v1. 완전탐색(성공)
+ * [풀이] 탐색 범위: 2 ~ Math.sqrt(n)
+ * 2_v1. 완전탐색(성공) -> 빠름
+ * [풀이] 에라토스테네스의 체
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12921">문제</a>
+ * @See <a href="https://firework-ham.tistory.com/8">풀이 참고</a>
  */
 class Solution12921 {
     public static void main(String[] args) {
@@ -18,6 +22,7 @@ class Solution12921 {
         System.out.println(solution(5));
     }
 
+    // 1_v1
     /**
      * @param n 2이상 1000000이하의 자연수
      * @return 1부터 입력받은 숫자 n 사이에 있는 소수의 개수를 반환
@@ -40,5 +45,27 @@ class Solution12921 {
                 return false;
         }
         return true;
+    }
+
+    // 2_v1
+    public int solution2(int n) {
+        int answer = 0;
+        // true: 소수아님, false: 소수
+        boolean[] prime = new boolean[n + 1];
+        prime[0] = true;
+        prime[1] = true;
+        for(int i = 2; i <= n; i++) {
+            // 1. 소수가 아니면 패스
+            if(prime[i]) continue;
+            // 2. 소수의 모든 배수는 소수가 아님
+            for(int j = 2 * i; j <= n; j += i) {
+                prime[j] = true;
+            }
+        }
+        // 3. 소수 개수 세기
+        for(int i = 1; i <= n; i++) {
+            if(!prime[i]) answer++;
+        }
+        return answer;
     }
 }
