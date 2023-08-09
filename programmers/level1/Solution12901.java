37947b83e3213525753933bf519a02e4b1c18ad0
ahah525 <ahah525@naver.com>
2023-08-09 10:17:37 +0900

    12901-2016년(3_v1.성공)

--- programmers/level1/Solution12901.java
+++ programmers/level1/Solution12901.java
@@ -3,10 +3,16 @@ package programmers.level1;
 
 /**
  * [문제명] 2016년
- * [풀이시간] 20분 / 20분
- * [한줄평] 쉬운 문제였는데, 윤년이라는 조건을 생각을 못하고 풀어서 생각보다 오래걸렸던 문제였다. / 2번째 풀 때도 윤년을 28일로 착각해서 오래걸렸던 문제였다.
- * 1_v1. 수학(성공)
+ * [풀이시간] 20분 / 20분 / 11분
+ * [한줄평] 쉬운 문제였는데, 윤년이라는 조건을 생각을 못하고 풀어서 생각보다 오래걸렸던 문제였다.
+ * / 2번째 풀 때도 윤년을 28일로 착각해서 오래걸렸던 문제였다.
+ * / 유사한 문제가 코테에 나와서 복습 차원에서 풀었는데 더 이상 풀어볼 필요는 없을 것 같다.
+ * 1_v1. 수학(성공) -> 빠름
+ * [풀이] 월 별 일 수를 int[]에 저장
  * 2_v1. 수학(성공)
+ * [풀이] 월 별 일 수를 switch문으로 구함
+ * 3_v1. 수학(성공)
+ * [풀이] 1_v1 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/12901">문제</a>
  */
 class Solution12901 {
@@ -17,27 +23,28 @@ class Solution12901 {
         System.out.println(solution(a, b));
     }
 
+    // 1_v1, 3_v1
     /**
-     * 추천
      * @param a 월
      * @param b 일
      * @return 2016년 a월 b일이 무슨 요일인지 리턴
      */
     public static String solution(int a, int b) {
-        // 2016년은 윤년입니다. -> 2월은 29일까지
+        // 1. 월 별 일 수 초기화
         int[] month = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        // 2016년 1월 1일은 금요일입니다. -> (날짜수%7)이 1인 날짜 = 금요일
-        String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
-        // a월 b일 -> 1월 1일기준 ?일인지 환산
-        int days = 0;
+        // 2. 지난 일 수에 대한 요일 초기화
+        String[] days = {"FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"};
+        // 3. a월 b일이 1월 1일을 기준으로 몇 일이 지났는지 일 수 계산
+        int day = 0;
         for(int i = 0; i < a - 1; i++) {
-            days += month[i];
+            day += month[i];
         }
-        days += b;
-
-        return day[days % 7];
+        day += (b - 1);
+        // 4. 지난 일 수의 요일 = days[지난 일 수 % 7]
+        return days[day % 7];
     }
 
+    // 2_v1
     public String solution2(int a, int b) {
         String[] day = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
         int days = b;
