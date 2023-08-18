3b485c8481229a179aee3b71472c7ec3a967238a
ahah525 <ahah525@naver.com>
2023-08-18 14:38:45 +0900

    87377-교점에 별 만들기(1_v1.실패)

---
+++ programmers/level2/Solution87377.java
@@ -0,0 +1,85 @@
+package programmers.level2;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * [문제명] 교점에 별 만들기
+ * [풀이시간] (1시간 3분)
+ * [한줄평]
+ * 1_v1. (실패-정확성 29 실패)
+ * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/87377">문제</a>
+ */
+class Solution87377 {
+    public static void main(String[] args) {
+        //
+        System.out.println();
+    }
+
+    // 1_v1
+    public String[] solution(int[][] line) {
+        List<int[]> list = new ArrayList<>();
+
+        int MAX = Integer.MAX_VALUE;
+        int MIN = Integer.MIN_VALUE;
+        int minX = MAX;
+        int minY = MAX;
+        int maxX = MIN;
+        int maxY = MIN;
+        // ax+by+c=0
+        for(int i = 0; i < line.length; i++) {
+            int a = line[i][0];
+            int b = line[i][1];
+            int e = line[i][2];
+            for(int j = i + 1; j < line.length; j++) {
+                int c = line[j][0];
+                int d = line[j][1];
+                int f = line[j][2];
+                //
+                double x = (double) (b * f - e * d) / (a * d - b * c);
+                double y = (double) (e * c - a * f) / (a * d - b * c);
+                // System.out.println(x + "," + y);
+                // 둘 다 정수이면
+                if(x % 1 == 0 && y % 1 == 0) {
+                    int px = (int) x;
+                    int py = (int) y;
+                    list.add(new int[] {px, py});
+                    minX = Math.min(minX, px);
+                    minY = Math.min(minY, py);
+                    maxX = Math.max(maxX, px);
+                    maxY = Math.max(maxY, py);
+                }
+            }
+            //
+
+        }
+        // System.out.println(list.size());
+        // System.out.println(String.format("%d %d, %d %d", minX, minY, maxX, maxY));
+
+        int h = Math.abs(maxY - minY) + 1;
+        int w = Math.abs(maxX - minX) + 1;
+        String[] answer = new String[h];
+        char[][] arr = new char[h][w];
+        for(char[] row : arr) {
+            Arrays.fill(row, '.');
+        }
+        for(int[] p : list) {
+            // System.out.println(p[0] +"," + p[1]);
+            int px = p[0] - minX;
+            int py = maxY - p[1];
+            // System.out.println(px +"," + py);
+            arr[py][px] = '*';
+        }
+        int i = 0;
+        for(char[] row : arr) {
+            StringBuilder sb = new StringBuilder();
+            for(char c : row) {
+                sb.append(c);
+            }
+            answer[i++] = sb.toString();
+        }
+        return answer;
+    }
+}
