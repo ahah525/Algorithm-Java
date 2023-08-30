aa361e24d16fee11a8a63c944c64ef0cf6707655
ahah525 <ahah525@naver.com>
2023-08-30 11:36:46 +0900

    42891-무지의 먹방 라이브(2_v2.성공)

--- programmers/level4/Solution42891.java
+++ programmers/level4/Solution42891.java
@@ -7,13 +7,15 @@ import java.util.List;
 
 /**
  * [문제명] 무지의 먹방 라이브
- * [풀이시간] 2시간 5분(1시간 33분 + 32분) / (20분)
+ * [풀이시간] 2시간 5분(1시간 33분+32분) / 1시간 20분(20분+1시간)
  * [한줄평] 생각보다 어려웠고 결국 풀이를 보고 이해했던 문제다. 솔직히 답을 안봤으면 정렬을 해야겠다는 생각조차 못했을 것 같다.
- * /
+ * / 너무 어려워서 풀이를 보고 풀었고 꼭 복습이 필요하다.
  * 1_v1. (실패 - 정확성 14.7, 효율성 0)
  * 1_v2. 구현, 정렬(성공)
  * 2_v1. 구현(실패 - 정확성 16,19,20,23 시간초과, 효율성 1~8 시간초과)
  * [풀이] 무식하게 k번 반복
+ * 2_v2. 구현, 정렬(성공)
+ * [풀이] 1_v2 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/42891">문제</a>
  * @See <a href="https://moonsbeen.tistory.com/371">풀이</a>
  * @See <a href="https://www.youtube.com/watch?v=4MWxAt4fx5I">풀이</a>
@@ -24,7 +26,7 @@ class Solution42891 {
         System.out.println();
     }
 
-    // 1_v2
+    // 1_v2, 2_v1
     class Food {
         int idx;
         int time;
@@ -35,53 +37,38 @@ class Solution42891 {
     }
 
     public int solution(int[] foodTimes, long k) {
-        //
-        int len = foodTimes.length;
+        int n = foodTimes.length;
         List<Food> list = new ArrayList<>();
-        for(int i = 0; i < len; i++) {
+        for(int i = 0; i < n; i++) {
             list.add(new Food(i + 1, foodTimes[i]));
         }
-        //
+        // 1. 시간 오름차순 정렬
         Collections.sort(list, (o1, o2) -> o1.time - o2.time);
         //
-        int prev = 0;
-        int idx = 0;
-        for(Food f : list) {
-            int diff = f.time - prev;
-            if(diff != 0) {
-                long cnt = (long) diff * len;
-                if(k - cnt >= 0) {
-                    k -= cnt;
-                    prev = f.time;
-                } else {
-                    k %= len;
-                    List<Food> subList = list.subList(idx, foodTimes.length);
-                    Collections.sort(subList, (o1, o2) -> o1.idx - o2.idx);
-                    return subList.get((int) k).idx;
-                }
+        int prev = 0;   // 이전 음식 소요 시간
+        int i = 0;
+        for(Food cur : list) {
+            int h = cur.time - prev;
+            // 2. 이전 음식과 현재 음식의 소요 시간이 같으면 패스
+            if(h == 0) {
+                i++;
+                continue;
             }
-            idx++;
-            len--;
+            // 3. 한 번에 처리할 수 있는 음식(시간) 개수 계산
+            int w = n - i;  // 남은 음식 개수
+            long cnt = (long) h * w;
+            if(k >= cnt) {
+                k -= cnt;
+                prev = cur.time;
+                i++;
+            } else {
+                // [i, n) 구간의 서브 리스트 추출
+                List<Food> subList = list.subList(i, n);
+                Collections.sort(subList, (o1, o2) -> o1.idx - o2.idx);
+                k %= w;
+                return subList.get((int) k).idx;
+            }
         }
         return -1;
-    }
-
-    // 2_v1
-    public int solution2(int[] foodTimes, long k) {
-        int idx = 0;
-        while(k > 0) {
-            if(foodTimes[idx] != 0) {
-                foodTimes[idx]--;
-                k--;
-                // System.out.println(Arrays.toString(foodTimes));
-            }
-            idx++;
-            if(idx == foodTimes.length) idx = 0;
-        }
-        while(foodTimes[idx] == 0) {
-            idx++;
-            if(idx == foodTimes.length) idx = 0;
-        }
-        return idx + 1;
     }
 }
