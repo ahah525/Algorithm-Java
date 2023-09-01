e0176c5ede41f7133da9d5e94a3ef284667e588b
ahah525 <ahah525@naver.com>
2023-09-01 11:03:36 +0900

    package 이동

--- baekjoon/gold/Boj12851.java
+++ baekjoon/gold/Boj12851.java
@@ -1,4 +1,4 @@
-package baekjoon.dfsbfs;
+package baekjoon.gold;
 
 
 import java.io.BufferedReader;
@@ -9,17 +9,18 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 /**
- * [문제명] 숨바꼭질 3
- * [풀이시간] 21분
- * [한줄평] BFS 문제인 것만 안다면 어렵지 않게 풀 수 있었던 문제였다.
+ * [문제명] 숨바꼭질 2
+ * [풀이시간] 55분
+ * [한줄평] 처음에 DP 문제라고 생각해서 풀다가 결국 풀이 힌트를 보고 이해했던 문제였다.
  * 1_v1. BFS(성공)
  * [풀이]
- * @See <a href="https://www.acmicpc.net/problem/13549">문제</a>
+ * @See <a href="https://www.acmicpc.net/problem/12851">문제</a>
  */
-class Boj13549 {
-    static int minTime; // n->k 최소 이동 시간
+class Boj12851 {
     static int MAX = 100000;
     static int[] visited = new int[MAX + 1];
+    static int minTime;
+    static int cnt;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,8 +28,11 @@ class Boj13549 {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         minTime = Integer.MAX_VALUE;
+        cnt = 0;
+        // n -> k
         bfs(n, k);
         System.out.println(minTime);
+        System.out.println(cnt);
     }
 
     public static void bfs(int start, int target) {
@@ -38,24 +42,18 @@ class Boj13549 {
         //
         while(!q.isEmpty()) {
             int v = q.poll();
-            // 1. v로 이동 시간이 target로 최소 이동 시간보다 더 크면, 패스(다음 노드에 방문할 필요X)
-            if(visited[v] - 1 > minTime) continue;
-            // 2. v가 target이면 최솟값 갱신
             if(v == target) {
                 minTime = visited[v] - 1;
-                continue;
+                cnt++;
             }
+            if(minTime < visited[v] - 1) return;
             int[] next = {v - 1, v + 1, 2 * v};
-            int[] time = {1, 1, 0};
-            for(int i = 0; i < 3; i++) {
-                int nv = next[i];   // 다음 노드
-                int t = time[i];    // 다음 노드로 이동 시간
-                // 3. 범위 벗어나면 패스
+            for(int nv : next) {
                 if(!isRange(nv)) continue;
-                // 4. 아직 방문한 적 없는 경우 or 방문했지만 현재 이동 시간이 더 최소인 경우
-                if(visited[nv] == 0 || visited[nv] > visited[v] + t) {
+                // nv에 처음 방문한 경우 or 이전에 nv에 방문했을 때 걸린 시간과 이번에 nv에 방문할 때 걸린 시간이 같은 경우
+                if(visited[nv] == 0 || visited[nv] == visited[v] + 1) {
                     q.add(nv);
-                    visited[nv] = visited[v] + t;
+                    visited[nv] = visited[v] + 1;
                 }
             }
         }
