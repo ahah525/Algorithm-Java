9d94fcfdfe07c0ef418fcadad8602aba1ce66d67
ahah525 <ahah525@naver.com>
2023-09-07 13:49:35 +0900

    15486-퇴사 2(1_v2.성공)

--- baekjoon/gold/Boj15486.java
+++ baekjoon/gold/Boj15486.java
@@ -4,16 +4,24 @@ package baekjoon.gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  * [문제명] 퇴사 2
- * [풀이시간] (35분)
- * [한줄평]
+ * [풀이시간] 40분(35분+5분)
+ * [한줄평] 역방향으로 값을 기록한다는 힌트를 보고 풀었던 문제다.
  * 1_v1. DP(실패)
- * [풀이]
+ * [반례] 6,7일의 상담 뿐 아니라 경우에 따라 1일의 상담도 기간 내에 수행하지 못할 수 있음
+ * [점화식]
+ * 1) i일 상담이 기간 내에 끝나지 않는 경우, d[i] = 0
+ * 2) i일 상담이 기간 내에 끝나는 경우, d[i] = Math.max(d[i + 1], p[i] + d[i + t[i]])
+ * 1_v2. DP(성공)
+ * [해결] i일 상담을 못하는 경우에 (i+1)일의 최댓값을 저장
+ * [점화식]
+ * 1) i일 상담이 기간 내에 끝나지 않는 경우, d[i] = d[i + 1]
+ * 2) i일 상담이 기간 내에 끝나는 경우, d[i] = Math.max(d[i + 1], p[i] + d[i + t[i]])
  * @See <a href="https://www.acmicpc.net/problem/15486">문제</a>
+ * @See <a href="https://j2wooooo.tistory.com/42">풀이</a>
  */
 class Boj15486 {
     // 1_v1
@@ -28,14 +36,17 @@ class Boj15486 {
             p[i] = Integer.parseInt(st.nextToken());
         }
         //
-        int[] d = new int[n + 2];
+        int[] d = new int[n + 2];   // d[i]: [i, n]일의 범위에서 i일 상담을 선택하거나, 선택하지 않았을 때 최대 이익
         for(int i = n; i >= 1; i--) {
-            // 상담 종료일이 기간을 벗어나면, 해당 상담은 선택X
-            if(i + t[i] - 1 > n) continue;
-            // i일 상담을 선택하는 경우 & i일 상담을 선택하지 않는 경우 중 최댓값
-            d[i] = Math.max(p[i] + d[i + t[i]], d[i + 1]);
+            if(i + t[i] - 1 > n) {
+                // i일 상담이 기간 내에 완료하지 못하면, i일 상담은 무조건 선택X
+                d[i] = d[i + 1];
+            } else {
+                // i일 상담을 선택하는 경우 & i일 상담을 선택하지 않는 경우 중 최댓값
+                d[i] = Math.max(d[i + 1], p[i] + d[i + t[i]]);
+            }
         }
-        System.out.println(Arrays.toString(d));
+//        System.out.println(Arrays.toString(d));
         System.out.println(d[1]);
     }
 }
