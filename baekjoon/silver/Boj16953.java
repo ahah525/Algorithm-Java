0090200d7e626a5ef272049ba03e6917ed2f9961
ahah525 <ahah525@naver.com>
2023-09-11 21:24:44 +0900

    16953-A -> B(2_v2.성공)

--- baekjoon/silver/Boj16953.java
+++ baekjoon/silver/Boj16953.java
@@ -10,13 +10,18 @@ import java.util.StringTokenizer;
 
 /**
  * [문제명] A -> B
- * [풀이시간] (7분)
+ * [풀이시간] / 25분(7분+18분)
  * [한줄평]
- * 1_v1. (성공)
- * 2_v1. BFS(실패-메모리 초과)
+ * / 너무 쉬운 문제라고 생각했는데, 메모리 초과가 난다는 생각을 못해서 힌트를 보고 해결했다.
+ * 1_v1. BFS(성공)
+ * 2_v1. BFS, DP(실패-메모리 초과)
+ * [풀이] visited 배열에 i를 만들기 위한 최소 횟수를 기록했다. A, B가 최대 10^9로 해당 크기 만큼 배열을 만들 수 없어 메모리 초과가 되었다.
+ * 2_v2. BFS(성공)
+ * [풀이] 첫번째 연산은 무조건 짝수만 나오고, 두번째 연산은 무조건 홀수만 나온다. 즉, 두 연산으로 인해 같은 값이 또 나올일은 없으므로 visited 없이 풀 수 있다.
  * @See <a href="https://www.acmicpc.net/problem/16953">문제</a>
  */
 class Boj16953 {
+    // 1_v1, 2_v2
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,22 +30,28 @@ class Boj16953 {
         System.out.println(bfs(a, b));
     }
 
+    static class Node {
+        long value;
+        int depth;
+
+        public Node(long value, int depth) {
+            this.value = value;
+            this.depth = depth;
+        }
+    }
+
     public static int bfs(int start, int end) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] visited = new int[end + 1];
-        q.add(start);
-        visited[start] = 1;
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(start, 0));
         while(!q.isEmpty()) {
-            int v = q.poll();
-            if(v == end) return visited[end];
-            if(2 * v <= end && visited[2 * v] == 0) {
-                q.add(2 * v);
-                visited[2 * v] = visited[v] + 1;
-            }
-            if(v * 10 + 1 <= end && visited[v * 10 + 1] == 0) {
-                q.add(v * 10 + 1);
-                visited[v * 10 + 1] = visited[v] + 1;
-            }
+            Node cur = q.poll();
+            long v = cur.value;
+            int d = cur.depth;
+            if(v == end) return d + 1;
+            // 1. 2 곱하기
+            if(2 * v <= end) q.add(new Node(2 * v, d + 1));
+            // 2. 가장 오른쪽에 1 추가하기
+            if(v * 10 + 1 <= end) q.add(new Node(v * 10 + 1, d + 1));
         }
         return -1;
     }
