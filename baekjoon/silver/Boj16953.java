ac21b0e442f9d6439e51cd6368048a16e2c00367
ahah525 <ahah525@naver.com>
2023-09-11 21:48:20 +0900

    16953-A -> B(2_v3.성공)

--- baekjoon/silver/Boj16953.java
+++ baekjoon/silver/Boj16953.java
@@ -18,7 +18,10 @@ import java.util.StringTokenizer;
  * [풀이] visited 배열에 i를 만들기 위한 최소 횟수를 기록했다. A, B가 최대 10^9로 해당 크기 만큼 배열을 만들 수 없어 메모리 초과가 되었다.
  * 2_v2. BFS(성공)
  * [풀이] 첫번째 연산은 무조건 짝수만 나오고, 두번째 연산은 무조건 홀수만 나온다. 즉, 두 연산으로 인해 같은 값이 또 나올일은 없으므로 visited 없이 풀 수 있다.
+ * 2_v3. 그리디(성공) -> 빠름
+ * [풀이] B->A로 갈 수 있는지 검사한다. 2로 나누는 것은 짝수만 가능하고, 1을 떼는 것은 끝 자리가 1일 때만 가능하기 때문에 그리디로도 풀 수 있다.
  * @See <a href="https://www.acmicpc.net/problem/16953">문제</a>
+ * @See <a href="https://jooona.tistory.com/105">힌트</a>
  */
 class Boj16953 {
     // 1_v1, 2_v2
@@ -54,5 +57,27 @@ class Boj16953 {
             if(v * 10 + 1 <= end) q.add(new Node(v * 10 + 1, d + 1));
         }
         return -1;
+    }
+
+    // 2_v2
+    public static void main2(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 1;
+        while(true) {
+            if(b == a) break;
+            if(b % 2 == 0 && b / 2 >= a) {
+                b /= 2;
+            } else if(b % 10 == 1 && b / 10 >= a) {
+                b /= 10;
+            } else {
+                cnt = -1;
+                break;
+            }
+            cnt++;
+        }
+        System.out.println(cnt);
     }
 }
