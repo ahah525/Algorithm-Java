270e26f9fe15f180e151e18eb75ae517157881e4
ahah525 <ahah525@naver.com>
2023-10-18 15:26:02 +0900

    1406-에디터(1_v3.성공)

--- baekjoon/silver/Boj1406.java
+++ baekjoon/silver/Boj1406.java
@@ -4,24 +4,24 @@ package baekjoon.silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /**
  * [문제명] 에디터
- * [풀이시간] 40분(20분+20분)
- * [한줄평]
+ * [풀이시간] 40분(20분+20분,15분)
+ * [한줄평] 결국 시간초과를 해결하지 못해 풀이를 봤는데, 스택으로 풀 수 있을거라고는 전혀 생각못했다. 꼭 다시 한 번 풀어봐야할 좋은 문제다.
  * 1_v1. LinkedList(실패-시간초과)
  * [풀이] LinkedList의 remove(index, value) 함수를 사용했다.
  * 1_v2. LinkedList(성공)
  * [풀이] ListIterator를 사용하여 커서를 양방향으로 움직여서 값을 삭제할 수 있도록 한다.
+ * 1_v3. Stack(성공)
+ * [풀이] 커서를 기준으로 왼쪽에 있는 문자는 left 스택에서 관리하고, 오른쪽에 있는 문자는 right 스택에서 관리한다.
  * @See <a href="https://www.acmicpc.net/problem/1406">문제</a>
  * @See <a href="https://minhamina.tistory.com/17">풀이</a>
  */
 class Boj1406 {
-    public static void main(String[] args) throws IOException {
+    // 1_v2
+    public static void main1(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int m = Integer.parseInt(br.readLine());
@@ -63,6 +63,53 @@ class Boj1406 {
         StringBuilder sb = new StringBuilder();
         for(String c : list) {
             sb.append(c);
+        }
+        System.out.println(sb);
+    }
+
+    // 1_v3
+    public static void main2(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int m = Integer.parseInt(br.readLine());
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+        for(char c : s.toCharArray()) {
+            left.add(c);
+        }
+        //
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();    // 명령어
+            switch(cmd) {
+                case "L":
+                    if(left.isEmpty()) continue;
+                    // 1. 왼쪽 맨 위에 있는 것을 오른쪽으로
+                    right.push(left.pop());
+                    break;
+                case "D":
+                    if(right.isEmpty()) continue;
+                    // 2. 오른쪽 맨 위에 있는 것을 왼쪽으로
+                    left.push(right.pop());
+                    break;
+                case "B":
+                    if(left.isEmpty()) continue;
+                    // 3. 왼쪽 맨 위 문자 꺼내기
+                    left.pop();
+                    break;
+                case "P":
+                    String c = st.nextToken();
+                    // 4. 왼쪽에 넣기
+                    left.push(c.charAt(0));
+                    break;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(char c : left) {
+            sb.append(c);
+        }
+        while(!right.isEmpty()) {
+            sb.append(right.pop());
         }
         System.out.println(sb);
     }
