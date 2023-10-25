d48350978e5b68e4addd79bf959f7b46d536e458
ahah525 <ahah525@naver.com>
2023-10-25 14:47:02 +0900

    16235-나무 재테크(1_v2.성공)

--- baekjoon/gold/Boj16235.java
+++ baekjoon/gold/Boj16235.java
@@ -8,105 +8,100 @@ import java.util.*;
 
 /**
  * [문제명] 나무 재테크
- * [풀이시간] 1시간 25분
- * [한줄평]
+ * [풀이시간] 2시간(1시간 25분+35분)
+ * [한줄평] 그대로 구현하는 문제였는데 자료구조를 잘못 써서 시간초과가 났고 결국 풀이를 보고 문제를 해결했다.
  * 1_v1. 구현(실패- 43%에서 시간초과)
+ * 1_v2. 구현(성공)
  * [풀이]
+ * - PriorityQueue<Tree> liveQ : 살아있는 나무를 저장하는 큐(나이 오름차순 정렬)
+ * - PriorityQueue<Tree> tmpQ : 살아있는 나무를 임시 저장하는 큐(나이 오름차순 정렬)
+ * - ArrayList<Tree> dies : 죽은 나무 리스트
  * @See <a href="https://www.acmicpc.net/problem/16235">문제</a>
+ * @See <a href="https://excited-hyun.tistory.com/223">풀이</a>
  */
 class Boj16235 {
-    // 1_v1
+    // 1_v2
+    static class Tree {
+        int x;
+        int y;
+        int age;
+
+        public Tree(int x, int y, int age) {
+            this.x = x;
+            this.y = y;
+            this.age = age;
+        }
+    }
     static int n;
     public static void main(String[] args) throws IOException {
         int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());   // 땅 크기
         int m = Integer.parseInt(st.nextToken());   // 나무 개수
         int k = Integer.parseInt(st.nextToken());
+        // 1. 각 칸에 추가되는 양분의 양과 현재 양분의 양 초기화
         int[][] a = new int[n][n];  // 각 칸에 추가되는 양분의 양
         int[][] arr = new int[n][n];  // 각 칸의 양분의 양
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < n; j++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = Integer.parseInt(st.nextToken());
                 arr[i][j] = 5;
             }
         }
-        Map<Integer, Queue<Integer>> map = new HashMap<>(); // (칸 번호, 해당 칸에 있는 나무들)
-        for(int i = 0; i < m; i++) {
+        // 2. 나무 초기화
+        Queue<Tree> liveQ = new PriorityQueue<>((o1, o2) -> o1.age - o2.age); // 나이가 어린 나무부터 양분을 먹기 위해 나이 기준 오름차순 정렬
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken()) - 1;
             int y = Integer.parseInt(st.nextToken()) - 1;
-            int age = Integer.parseInt(st.nextToken());   // 나이
-            int key = x * n + y;
-            if(!map.containsKey(key)) {
-                map.put(key, new PriorityQueue<>());
-            }
-            map.get(key).add(age);
+            int age = Integer.parseInt(st.nextToken());
+            liveQ.add(new Tree(x, y, age));
         }
-        int cnt = m;
-        for(int i = 0; i < k; i++) {
-            Map<Integer, Integer> five = new HashMap<>();
-            // 1. 나무가 자신의 나이만큼 양분을 먹고, 나이 1 증가
-            for(int key : map.keySet()) {
-                Queue<Integer> pq = map.get(key);
-                int x = key / n;
-                int y = key % n;
-                int add = 0;
-                // 한 칸에 여러 개의 나무가 있다면, 나이가 어린 나무부터 양분 먹기
-                Queue<Integer> res = new PriorityQueue<>();
-                while(!pq.isEmpty()) {
-                    int age = pq.poll();   // 가장 나이가 어린 나무의 나이
-                    // 자신의 나이만큼 양분을 먹을 수 없다면 죽음
-                    if(arr[x][y] < age) {
-                        add += age / 2;
-                        cnt--;
-                        continue;
-                    }
-                    res.add(age + 1);
-                    arr[x][y] -= age;
-                    if((age + 1) % 5 == 0) {
-                        five.put(key, five.getOrDefault(key, 0) + 1);
-                    }
+        // 3. k번 반복
+        for (int i = 0; i < k; i++) {
+            // 4. 봄
+            Queue<Tree> tmpQ = new PriorityQueue<>((o1, o2) -> o1.age - o2.age);
+            List<Tree> dies = new ArrayList<>();
+            while (!liveQ.isEmpty()) {
+                Tree tree = liveQ.poll();
+                // 자신의 나이만큼 양분을 먹을 수 없다면 죽음
+                if (arr[tree.x][tree.y] < tree.age) {
+                    dies.add(tree);
+                    continue;
                 }
-                map.put(key, res);
-                // 2. 죽은 나무가 있다면, (죽은 나무 나이 / 2)만큼 해당 칸에 양분 추가
-                arr[x][y] += add;
+                // 자신의 나이만큼 양분을 먹고, 나이 1증가
+                arr[tree.x][tree.y] -= tree.age;
+                tree.age++;
+                tmpQ.add(tree);
             }
-            // 3. 나이가 5의 배수인 나무의 인접 8칸에 나이가 1인 나무 생김
-            for(int key : five.keySet()) {
-                int x = key / n;
-                int y = key % n;
-                for(int j = 0; j < 8; j++) {
-                    int nx = x + dx[j];
-                    int ny = y + dy[j];
-                    int nKey = nx * n + ny;
-                    if(!inRange(nx, ny)) continue;
-                    if(!map.containsKey(nKey)) {
-                        map.put(nKey, new PriorityQueue<>());
-                    }
-                    for(int p = 0; p < five.get(key); p++) {
-                        map.get(nKey).add(1);
-                        cnt++;
-                    }
+            liveQ = new PriorityQueue<>(tmpQ);
+            // 5. 여름
+            for (Tree tree : dies) {
+                // 죽은 나무들의 위치에 (나이 / 2)만큼 양분 추가
+                arr[tree.x][tree.y] += tree.age / 2;
+            }
+            // 6. 가을
+            for (Tree tree : tmpQ) {
+                // 나이가 5의 배수인 나무의 인접 8칸에 나이가 1인 나무 생김
+                if (tree.age % 5 != 0) continue;
+                for (int j = 0; j < 8; j++) {
+                    int nx = tree.x + dx[j];
+                    int ny = tree.y + dy[j];
+                    if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+                    liveQ.add(new Tree(nx, ny, 1));
                 }
             }
-            // 4. 각 칸에 a[x][y]만큼 양분 추가
-            for(int x = 0; x < n; x++) {
-                for(int y = 0; y < n; y++) {
+            // 7. 겨울
+            for (int x = 0; x < n; x++) {
+                for (int y = 0; y < n; y++) {
+                    // 각 칸에 a[x][y]만큼 양분 추가
                     arr[x][y] += a[x][y];
                 }
-
             }
         }
-        System.out.println(cnt);
-    }
-
-    public static boolean inRange(int x, int y) {
-        if(0 <= x && x < n && 0 <= y && y < n) return true;
-        return false;
+        System.out.println(liveQ.size());
     }
 }
