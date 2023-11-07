c647ad4614acd3c5e2e1a10a65a8ea88eee14601
ahah525 <ahah525@naver.com>
2023-11-07 14:33:14 +0900

    42328-입국심사(4_v1.성공)

--- programmers/level3/Solution43238.java
+++ programmers/level3/Solution43238.java
@@ -5,16 +5,18 @@ import java.util.Arrays;
 
 /**
  * [문제명] 입국심사
- * [풀이시간] 1시간(30분 + 30분) / 50분 / 9분
+ * [풀이시간] 1시간(30분 + 30분) / 50분 / 9분 / 15분
  * [한줄평] 이분탐색 문제라는 것을 알아도 어떻게 풀어야할지 감이 안잡혀서 결국 풀이를 보고 이해했던 문제다.
  * / 처음 풀 때랑 똑같이 처음에는 PriorityQueue 를 이용해서 풀려고 했다. 이분탐색 문제인 걸 알고도 결국 풀지 못해 답을 보고 풀었다.
  * / 입력 범위를 보고 바로 이분탐색으로 풀었던 문제다.
+ * / 처음에 end 값을 잘못 설정해서 실패가 떴고 반례를 찾느라 시간이 좀 오래걸렸다. 한번쯤 더 풀어봐도 좋을 문제인것 같다.
  * 1_v1. PriorityQueue(실패 - 6~9 시간초과)
- * 1_v2. BinarySearch(성공)
+ * 1_v2. 이분탐색(성공)
  * - end 값 설정을 위해 times 를 정렬함
- * 2_v1. BinarySearch(성공) -> 빠름
+ * 2_v1. 이분탐색(성공) -> 빠름
  * - end 값 설정을 위해 굳이 times 를 정렬하지 않고, 입력범위의 최댓값을 계산함
- * 3_v1. BinarySearch(성공)
+ * 3_v1. 이분탐색(성공)
+ * 4_v1. 이분탐색(성공)
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/43238">문제</a>
  * @See <a href="https://school.programmers.co.kr/questions/7919">풀이 힌트</a>
  * @See <a href="https://swycha.tistory.com/122">풀이 참고</a>
@@ -59,7 +61,7 @@ class Solution43238 {
         return answer;
     }
 
-    // 2_v1, 3_v1
+    // 2_v1, 3_v1, 4_v1
     public long solution2(int n, int[] times) {
         long answer = 0;
         long start = 0;
@@ -67,11 +69,7 @@ class Solution43238 {
         long end = (long) Math.pow(10, 18);
         while(start <= end) {
             long mid = (start + end) / 2;
-            long cnt = 0;
-            for(int t : times) {
-                cnt += mid / t;
-            }
-            if(cnt >= n) {
+            if(isPossible(mid, n, times)) {
                 answer = mid;
                 end = mid - 1;
             } else {
@@ -79,5 +77,15 @@ class Solution43238 {
             }
         }
         return answer;
+    }
+
+    // t시간 내에 n명을 모두 입국심사할 수 있는지 여부 조회
+    public boolean isPossible(long t, int n, int[] times) {
+        long cnt = 0;
+        for(int time : times) {
+            cnt += t / time;
+        }
+        if(cnt >= n) return true;
+        return false;
     }
 }
