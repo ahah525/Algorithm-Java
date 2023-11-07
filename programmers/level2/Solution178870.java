18c4ac7627021d163d4cec7801bf821c20dc2dd3
ahah525 <ahah525@naver.com>
2023-11-07 14:10:15 +0900

    178870-연속된 부분 수열의 합(4_v1.성공)

--- programmers/level2/Solution178870.java
+++ programmers/level2/Solution178870.java
@@ -3,15 +3,18 @@ package programmers.level2;
 
 /**
  * [문제명] 연속된 부분 수열의 합
- * [풀이시간] 21분 / 10분(9분 + 1분) / 14분
+ * [풀이시간] 21분 / 10분(9분 + 1분) / 14분 / 6분
  * [한줄평] 투포인터 기초 문제였다.
  * / 쉽게 풀었지만 복습해봐도 좋을 문제다.
  * / 투포인터 기초 문제지만 오랜만에 풀어서 조건을 처리하는데 시간이 좀 걸렸다.
+ * / 너무 기초적인 문제라 더 이상 안풀어봐도 될 것 같다.
  * 1_v1. 투포인터(성공)
  * 2_v1. 투포인터(실패 - 1, 16, 25 실패)
  * 2_v2. 투포인터(성공)
  * [반례] 부분 수열의 최대 길이를 수열의 길이로 초기화해서 부분 수열의 길이와 수열의 길이가 같을 경우 [0, 0]이 반환되는 문제
  * 3_v1. 투포인터(성공)
+ * 4_v1. 투포인터(성공)
+ * [풀이] 3_v1 동일
  * @See <a href="https://school.programmers.co.kr/learn/courses/30/lessons/178870">문제</a>
  */
 class Solution178870 {
@@ -20,7 +23,7 @@ class Solution178870 {
         System.out.println();
     }
 
-    // 1_v1, 2_v2, 3_v1
+    // 1_v1, 2_v2
     /**
      * @param sequence 수열을 나타내는 정수 배열
      * @param k 부분 수열의 합을 나타내는 정수
@@ -55,17 +58,16 @@ class Solution178870 {
         return answer;
     }
 
-    // 3_v1
+    // 3_v1, 4_v1
     public int[] solution2(int[] sequence, int k) {
         int[] answer = new int[2];
         int n = sequence.length;
         int l = 0;
         int r = 0;
         int sum = 0;    // 연속된 부분 수열의 합
-        int len = sequence.length + 1;  // 연속된 부분 수열의 길이
+        int minLen = sequence.length + 1;  // 연속된 부분 수열의 길이
         // 1. left 가 배열의 범위를 벗어나기 전까지 반복
         while(l <= r) {
-            // System.out.println(left + "," + right);
             if(sum < k) {
                 if(r == n) break;
                 sum += sequence[r++];
@@ -73,10 +75,10 @@ class Solution178870 {
                 sum -= sequence[l++];
             } else {
                 // 더 길이가 짧은 수열일 때만 기록
-                if(len > r - l) {
+                if(minLen > r - l) {
                     answer[0] = l;
                     answer[1] = r - 1;
-                    len = r - l;
+                    minLen = r - l;
                 }
                 if(r == n) break;
                 sum += sequence[r++];
